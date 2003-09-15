be5e7def49c0fcde745acf4e0747d795a0a076da
jcarreira <jcarreira@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2003-09-15 03:51:40 +0000

    Jalopy reformatting...

--- trunk/src/java/com/opensymphony/webwork/views/jsp/IncludeTag.java
+++ trunk/src/java/com/opensymphony/webwork/views/jsp/IncludeTag.java
@@ -1,4 +1,8 @@
 /*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+/*
  * WebWork, Web Application Framework
  *
  * Distributable under Apache license.
@@ -8,9 +12,19 @@ package com.opensymphony.webwork.views.jsp;
 
 import com.opensymphony.webwork.config.Configuration;
 import com.opensymphony.webwork.util.FastByteArrayOutputStream;
+
 import com.opensymphony.xwork.ActionContext;
 import com.opensymphony.xwork.util.OgnlValueStack;
+
 import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import java.net.URLEncoder;
+
+import java.util.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -23,11 +37,7 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.TagSupport;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.URLEncoder;
-import java.util.*;
+
 
 /**
  * Include a servlets output (e.g. result of servlet, or a JSP page).
@@ -36,17 +46,78 @@ import java.util.*;
  * @author <a href="mailto:scott@atlassian.com">Scott Farquhar</a>
  * @version $Revision$
  */
-public class IncludeTag
-        extends TagSupport
-        implements ParameterizedTag {
+public class IncludeTag extends TagSupport implements ParameterizedTag {
+    //~ Static fields/initializers /////////////////////////////////////////////
+
+    private static String encoding;
+    private static boolean encodingDefined = true;
+
+    //~ Instance fields ////////////////////////////////////////////////////////
+
+    protected Map params;
+
+    // Attributes ----------------------------------------------------
+    protected String pageAttr;
+    protected String valueAttr;
+
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    public static String getContextRelativePath(ServletRequest request, String relativePath) {
+        String returnValue;
+
+        if (relativePath.startsWith("/")) {
+            returnValue = relativePath;
+        } else if (!(request instanceof HttpServletRequest)) {
+            returnValue = relativePath;
+        } else {
+            HttpServletRequest hrequest = (HttpServletRequest) request;
+            String uri = (String) request.getAttribute("javax.servlet.include.servlet_path");
+
+            if (uri == null) {
+                uri = hrequest.getServletPath();
+            }
+
+            returnValue = uri.substring(0, uri.lastIndexOf('/')) + '/' + relativePath;
+        }
+
+        // .. is illegal in an absolute path according to the Servlet Spec and will cause
+        // known problems on Orion application servers.
+        if (returnValue.indexOf("..") != -1) {
+            Stack stack = new Stack();
+            StringTokenizer pathParts = new StringTokenizer(returnValue.replace('\\', '/'), "/");
+
+            while (pathParts.hasMoreTokens()) {
+                String part = pathParts.nextToken();
+
+                if (!part.equals(".")) {
+                    if (part.equals("..")) {
+                        stack.pop();
+                    } else {
+                        stack.push(part);
+                    }
+                }
+            }
+
+            StringBuffer flatPathBuffer = new StringBuffer();
+
+            for (int i = 0; i < stack.size(); i++) {
+                flatPathBuffer.append("/" + stack.elementAt(i));
+            }
+
+            returnValue = flatPathBuffer.toString();
+        }
+
+        return returnValue;
+    }
+
     // Static --------------------------------------------------------
-    public static void include(String aResult, PageContext aContext)
-            throws ServletException, IOException {
+    public static void include(String aResult, PageContext aContext) throws ServletException, IOException {
         String resourcePath = getContextRelativePath(aContext.getRequest(), aResult);
         RequestDispatcher rd = aContext.getRequest().getRequestDispatcher(resourcePath);
 
-        if (rd == null)
+        if (rd == null) {
             throw new ServletException("Not a valid resource path:" + resourcePath);
+        }
 
         PageResponse pageResponse = new PageResponse((HttpServletResponse) aContext.getResponse());
 
@@ -55,6 +126,7 @@ public class IncludeTag
 
         //write the response back to the JspWriter, using the correct encoding.
         String encoding = getEncoding();
+
         if (encoding != null) {
             //use the encoding specified in the property file
             pageResponse.getContent().writeTo(aContext.getOut(), encoding);
@@ -64,8 +136,123 @@ public class IncludeTag
         }
     }
 
-    private static String encoding;
-    private static boolean encodingDefined = true;
+    // Public --------------------------------------------------------
+
+    /**
+     * Name of page/servlet to include.
+     *
+     * @param   aPage
+     * @deprecated use value attribute instead
+     */
+    public void setPage(String aPage) {
+        pageAttr = aPage;
+    }
+
+    /* (non-Javadoc)
+     * @see com.opensymphony.webwork.views.jsp.ParameterizedTag#getParams()
+     */
+    public Map getParams() {
+        return params;
+    }
+
+    /**
+     * Name of property whose value is the name of the page/servlet to include.
+     *
+     * @param   aName
+     */
+    public void setValue(String aName) {
+        valueAttr = aName;
+    }
+
+    // ParamTag.Parametric implementation ----------------------------
+
+    /**
+     * Add a parameter to the URL of the included page/servlet.
+     *
+     * @param   name
+     * @param   value
+     */
+    public void addParam(String name, Object value) {
+        if (value != null) {
+            List currentValues = (List) params.get(name);
+
+            if (currentValues == null) {
+                currentValues = new ArrayList();
+                params.put(name, currentValues);
+            }
+
+            currentValues.add(value);
+        }
+    }
+
+    public int doEndTag() throws JspException {
+        OgnlValueStack stack = ActionContext.getContext().getValueStack();
+
+        String page;
+
+        // If value is set, we resolve it to get the page name
+        if (valueAttr != null) {
+            page = (String) stack.findValue(valueAttr);
+        } else {
+            page = pageAttr;
+        }
+
+        StringBuffer urlBuf = new StringBuffer();
+
+        // Add URL
+        urlBuf.append(page);
+
+        // Add request parameters
+        if (params.size() > 0) {
+            urlBuf.append('?');
+
+            String concat = "";
+
+            // Set parameters
+            Iterator enum = params.entrySet().iterator();
+
+            while (enum.hasNext()) {
+                Map.Entry entry = (Map.Entry) enum.next();
+                Object name = entry.getKey();
+                List values = (List) entry.getValue();
+
+                for (int i = 0; i < values.size(); i++) {
+                    urlBuf.append(concat);
+                    urlBuf.append(name);
+                    urlBuf.append('=');
+
+                    try {
+                        urlBuf.append(URLEncoder.encode(values.get(i).toString()));
+                    } catch (Exception e) {
+                    }
+
+                    concat = "&";
+                }
+            }
+        }
+
+        params = null;
+
+        String result = urlBuf.toString();
+
+        // Include
+        try {
+            include(result, pageContext);
+        } catch (Exception e) {
+            LogFactory.getLog(getClass()).warn("Exception thrown during include of " + result, e);
+            throw new JspTagException(e.toString());
+        }
+
+        return EVAL_PAGE;
+    }
+
+    // BodyTag implementation ----------------------------------------
+    public int doStartTag() throws JspException {
+        // Init parameter map
+        params = new HashMap();
+
+        return super.doStartTag();
+    }
 
     /**
      * Get the encoding specified by the property 'webwork.i18n.encoding' in webwork.properties,
@@ -92,170 +279,11 @@ public class IncludeTag
                 encodingDefined = false;
             }
         }
+
         return encoding;
-
-    }
-
-    public static String getContextRelativePath(ServletRequest request,
-                                                String relativePath) {
-        String returnValue;
-
-        if (relativePath.startsWith("/"))
-            returnValue = relativePath;
-        else if (!(request instanceof HttpServletRequest))
-            returnValue = relativePath;
-        else {
-            HttpServletRequest hrequest = (HttpServletRequest) request;
-            String uri = (String)
-                    request.getAttribute("javax.servlet.include.servlet_path");
-            if (uri == null)
-                uri = hrequest.getServletPath();
-            returnValue = uri.substring(0, uri.lastIndexOf('/')) + '/' + relativePath;
-        }
-
-        // .. is illegal in an absolute path according to the Servlet Spec and will cause
-        // known problems on Orion application servers.
-        if (returnValue.indexOf("..") != -1) {
-            Stack stack = new Stack();
-            StringTokenizer pathParts = new StringTokenizer(returnValue.replace('\\', '/'), "/");
-            while (pathParts.hasMoreTokens()) {
-                String part = pathParts.nextToken();
-                if (!part.equals(".")) {
-                    if (part.equals(".."))
-                        stack.pop();
-                    else
-                        stack.push(part);
-                }
-            }
-
-            StringBuffer flatPathBuffer = new StringBuffer();
-
-            for (int i = 0; i < stack.size(); i++)
-                flatPathBuffer.append("/" + stack.elementAt(i));
-
-            returnValue = flatPathBuffer.toString();
-        }
-
-        return returnValue;
-    }
-
-    // Attributes ----------------------------------------------------
-    protected String pageAttr;
-    protected String valueAttr;
-    protected Map params;
-
-    // Public --------------------------------------------------------
-
-    /**
-     * Name of page/servlet to include.
-     *
-     * @param   aPage
-     * @deprecated use value attribute instead
-     */
-    public void setPage(String aPage) {
-        pageAttr = aPage;
-    }
-
-    /**
-     * Name of property whose value is the name of the page/servlet to include.
-     *
-     * @param   aName
-     */
-    public void setValue(String aName) {
-        valueAttr = aName;
-    }
-
-    // ParamTag.Parametric implementation ----------------------------
-    /**
-     * Add a parameter to the URL of the included page/servlet.
-     *
-     * @param   name
-     * @param   value
-     */
-    public void addParam(String name, Object value) {
-        if (value != null) {
-            List currentValues = (List) params.get(name);
-            if (currentValues == null) {
-                currentValues = new ArrayList();
-                params.put(name, currentValues);
-            }
-            currentValues.add(value);
-        }
-    }
-
-    // BodyTag implementation ----------------------------------------
-    public int doStartTag() throws JspException {
-        // Init parameter map
-        params = new HashMap();
-
-        return super.doStartTag();
-    }
-
-    public int doEndTag() throws JspException {
-        OgnlValueStack stack = ActionContext.getContext().getValueStack();
-
-
-        String page;
-        // If value is set, we resolve it to get the page name
-        if (valueAttr != null) {
-            page = (String) stack.findValue(valueAttr);
-        } else {
-            page = pageAttr;
-        }
-
-        StringBuffer urlBuf = new StringBuffer();
-
-        // Add URL
-        urlBuf.append(page);
-
-        // Add request parameters
-        if (params.size() > 0) {
-
-            urlBuf.append('?');
-            String concat = "";
-
-            // Set parameters
-            Iterator enum = params.entrySet().iterator();
-            while (enum.hasNext()) {
-                Map.Entry entry = (Map.Entry) enum.next();
-                Object name = entry.getKey();
-                List values = (List) entry.getValue();
-                for (int i = 0; i < values.size(); i++) {
-                    urlBuf.append(concat);
-                    urlBuf.append(name);
-                    urlBuf.append('=');
-
-                    try {
-                        urlBuf.append(URLEncoder.encode(values.get(i).toString(), "UTF-8"));
-                    } catch (Exception e) {
-
-                    }
-                    concat = "&";
-                }
-            }
-        }
-        params = null;
-
-        String result = urlBuf.toString();
-
-        // Include
-        try {
-            include(result, pageContext);
-        } catch (Exception e) {
-            LogFactory.getLog(getClass()).warn("Exception thrown during include of " + result, e);
-            throw new JspTagException(e.toString());
-        }
-
-        return EVAL_PAGE;
-    }
-
-    /* (non-Javadoc)
-     * @see com.opensymphony.webwork.views.jsp.ParameterizedTag#getParams()
-     */
-    public Map getParams() {
-        return params;
     }
 }
+
 
 /**
  * Implementation of ServletOutputStream that stores all data written
@@ -265,10 +293,31 @@ public class IncludeTag
  * @author <a href="mailto:scott@atlassian.com">Scott Farquhar</a>
  */
 final class PageOutputStream extends ServletOutputStream {
+    //~ Instance fields ////////////////////////////////////////////////////////
+
     private FastByteArrayOutputStream buffer;
+
+    //~ Constructors ///////////////////////////////////////////////////////////
 
     public PageOutputStream() {
         buffer = new FastByteArrayOutputStream();
+    }
+
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    /** Return all data that has been written to this OutputStream. */
+    public FastByteArrayOutputStream getBuffer() throws IOException {
+        flush();
+
+        return buffer;
+    }
+
+    public void close() throws IOException {
+        buffer.close();
+    }
+
+    public void flush() throws IOException {
+        buffer.flush();
     }
 
     public void write(byte[] b, int o, int l) throws IOException {
@@ -279,24 +328,9 @@ final class PageOutputStream extends ServletOutputStream {
         buffer.write(i);
     }
 
-    public void flush() throws IOException {
-        buffer.flush();
-    }
-
-    public void close() throws IOException {
-        buffer.close();
-    }
-
-    public void write(byte b[]) throws IOException {
+    public void write(byte[] b) throws IOException {
         buffer.write(b);
     }
-
-    /** Return all data that has been written to this OutputStream. */
-    public FastByteArrayOutputStream getBuffer() throws IOException {
-        flush();
-        return buffer;
-    }
-
 }
 
 
@@ -315,32 +349,20 @@ final class PageOutputStream extends ServletOutputStream {
  * @author <a href="mailto:scott@atlassian.com">Scott Farquhar</a>
  */
 final class PageResponse extends HttpServletResponseWrapper {
+    //~ Instance fields ////////////////////////////////////////////////////////
+
     protected PrintWriter pagePrintWriter;
     protected ServletOutputStream outputStream;
     private PageOutputStream pageOutputStream = null;
+
+    //~ Constructors ///////////////////////////////////////////////////////////
 
     /** Create PageResponse wrapped around an existing HttpServletResponse. */
     public PageResponse(HttpServletResponse response) {
         super(response);
     }
 
-    /** Return PrintWriter wrapper around PageOutputStream. */
-    public PrintWriter getWriter() throws IOException {
-        if (pagePrintWriter == null)
-            pagePrintWriter = new PrintWriter(new OutputStreamWriter(getOutputStream(), getCharacterEncoding()));
-        return pagePrintWriter;
-    }
-
-    /**
-     * Return instance of {@link com.opensymphony.webwork.views.jsp.PageOutputStream}
-     * allowing all data written to stream to be stored in temporary buffer.
-     */
-    public ServletOutputStream getOutputStream() throws IOException {
-        if (pageOutputStream == null) {
-            pageOutputStream = new PageOutputStream();
-        }
-        return pageOutputStream;
-    }
+    //~ Methods ////////////////////////////////////////////////////////////////
 
     /**
      * Return the content buffered inside the {@link com.opensymphony.webwork.views.jsp.PageOutputStream}.
@@ -351,9 +373,31 @@ final class PageResponse extends HttpServletResponseWrapper {
         //if we are using a writer, we need to flush the
         //data to the underlying outputstream.
         //most containers do this - but it seems Jetty 4.0.5 doesn't
-        if (pagePrintWriter != null)
+        if (pagePrintWriter != null) {
             pagePrintWriter.flush();
+        }
 
         return ((PageOutputStream) getOutputStream()).getBuffer();
+    }
+
+    /**
+     * Return instance of {@link com.opensymphony.webwork.views.jsp.PageOutputStream}
+     * allowing all data written to stream to be stored in temporary buffer.
+     */
+    public ServletOutputStream getOutputStream() throws IOException {
+        if (pageOutputStream == null) {
+            pageOutputStream = new PageOutputStream();
+        }
+
+        return pageOutputStream;
+    }
+
+    /** Return PrintWriter wrapper around PageOutputStream. */
+    public PrintWriter getWriter() throws IOException {
+        if (pagePrintWriter == null) {
+            pagePrintWriter = new PrintWriter(new OutputStreamWriter(getOutputStream(), getCharacterEncoding()));
+        }
+
+        return pagePrintWriter;
     }
 }
