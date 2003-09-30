e07cde866c83ea184e1b1aa0c3b02cbb700aa02a
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2003-09-30 03:41:43 +0000

    migration jars to help with the migration from 1.3

---
+++ src/migration/webwork/action/ActionSupport.java
@@ -0,0 +1,12 @@
+package webwork.action;
+
+import java.io.Serializable;
+
+/**
+ * Backwards compatible class for WebWork 1.x migration
+ * 
+ * @author $Author$
+ * @version $Revision$
+ */
+public class ActionSupport extends com.opensymphony.xwork.ActionSupport implements Serializable {
+}
