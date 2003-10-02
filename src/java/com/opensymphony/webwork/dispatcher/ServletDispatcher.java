9b5d30e087c095b302acb3b316195748f82e75ae
cameronbraid <cameronbraid@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2003-10-02 09:58:31 +0000

    Issue number:  WW-326 Submitted by:  Jerome Bernard (jerome.bernard@xtremejava.com) Reviewed by:   Cameron Braid (cameron@datacodex.net) Refactoring of ServletDispatcher to allow for extensibility. Added CoolUriServletDispatcher for turbine like URIs Added test case for ServletDispatcher and CoolUriServletDispatcher

--- src/java/com/opensymphony/webwork/dispatcher/ServletDispatcher.java
+++ src/java/com/opensymphony/webwork/dispatcher/ServletDispatcher.java
@@ -24,6 +24,7 @@ import java.io.File;
 import java.io.IOException;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -36,8 +37,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author Rickard Öberg (rickard@middleware-company.com)
  * @author Matt Baldree (matt@smallleap.com)
  * @author jcarreira
- * @author Cameron Braid
- * @version $Revision$
+ * @author Cameron Braid (cameron@datacodex.net)
  */
 public class ServletDispatcher extends HttpServlet implements WebWorkStatics {
     //~ Static fields/initializers /////////////////////////////////////////////
@@ -117,46 +117,37 @@ public class ServletDispatcher extends HttpServlet implements WebWorkStatics {
     }
 
     /**
-     * Service a request.
-     * The request is first checked to see if it is a multi-part. If it is, then the request
-     * is wrapped so WW will be able to work with the multi-part as if it was a normal request.
-     * Then the request is handed to GenericDispatcher and executed.
-     *
-     * @param request
-     * @param response
-     * @exception javax.servlet.ServletException
-     */
+	* Service a request - get the namespace, actionName, paramMap, sessionMap, applicationMap from the providers
+	* and delegate to the service call
+	* 
+	* @param request
+	* @param response
+	* @exception javax.servlet.ServletException
+	*/
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        serviceAction(request, response, getNameSpace(request), getActionName(request), getParameterMap(request), getSessionMap(request), getApplicationMap());
+    }
+
+    /**
+	* The request is first checked to see if it is a multi-part. If it is, then the request
+	* is wrapped so WW will be able to work with the multi-part as if it was a normal request.
+	* Then the request is handed to GenericDispatcher and executed.
+	*/
+    public void serviceAction(HttpServletRequest request, HttpServletResponse response, String namespace, String actionName, Map parameterMap, Map sessionMap, Map applicationMap) {
         //wrap request if needed
         try {
             request = wrapRequest(request);
         } catch (IOException e) {
             String message = "Could not wrap servlet request with MultipartRequestWrapper!";
             log.error(message, e);
-            throw new ServletException(message, e);
+            sendError(request, response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, new ServletException(message, e));
         }
-
-        // Get action
-        String servletPath = (String) request.getAttribute("javax.servlet.include.servlet_path");
-
-        if (servletPath == null) {
-            servletPath = request.getServletPath();
-        }
-
-        // Path is always original path, even if it is included in page with another path
-        String actionPath = request.getServletPath();
-        actionPath = getNamespaceFromServletPath(actionPath);
-
-        String actionName = getActionName(servletPath);
 
         HashMap extraContext = new HashMap();
-        SessionMap sessionMap = new SessionMap(request.getSession());
-        ApplicationMap applicationMap = new ApplicationMap(getServletContext());
-        RequestMap requestMap = new RequestMap(request);
-
-        extraContext.put(ActionContext.PARAMETERS, request.getParameterMap());
+        extraContext.put(ActionContext.PARAMETERS, parameterMap);
         extraContext.put(ActionContext.SESSION, sessionMap);
         extraContext.put(ActionContext.APPLICATION, applicationMap);
+        
         extraContext.put(HTTP_REQUEST, request);
         extraContext.put(HTTP_RESPONSE, response);
         extraContext.put(SERVLET_CONFIG, getServletConfig());
@@ -169,25 +160,95 @@ public class ServletDispatcher extends HttpServlet implements WebWorkStatics {
         extraContext.put("application", applicationMap);
 
         try {
-            ActionProxy proxy = ActionProxyFactory.getFactory().createActionProxy(actionPath, actionName, extraContext);
+            ActionProxy proxy = ActionProxyFactory.getFactory().createActionProxy(namespace, actionName, extraContext);
             request.setAttribute("webwork.valueStack", proxy.getValueStack());
             proxy.execute();
         } catch (ConfigurationException e) {
-            try {
-                response.sendError(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
-                log.error("Could not find action", e);
-            } catch (IOException e1) {
-            }
+            log.error("Could not find action", e);
+            sendError(request, response, HttpServletResponse.SC_NOT_FOUND, e);
         } catch (Exception e) {
             log.error("Could not execute action", e);
-            throw new ServletException("Could not execute action", e);
+            sendError(request, response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e);
         }
     }
 
     /**
-     * Determine action name by extracting last string and removing
-     * extension. (/.../.../Foo.action -> Foo)
-     */
+ * build the name of the action from the request
+ *
+ * override this method to customize a ULR -> action name mapping
+ */
+    protected String getActionName(HttpServletRequest request) {
+        String servletPath = (String) request.getAttribute("javax.servlet.include.servlet_path");
+
+        if (servletPath == null) {
+            servletPath = request.getServletPath();
+        }
+
+        return getActionName(servletPath);
+    }
+
+    protected Map getApplicationMap() {
+        return new ApplicationMap(getServletContext());
+    }
+
+    /**
+ * get the namespace of the action from the request
+ *
+ * override this method to customize a ULR -> action namespace mapping
+ */
+    protected String getNameSpace(HttpServletRequest request) {
+        // Path is always original path, even if it is included in page with another path
+        String servletPath = request.getServletPath();
+
+        return getNamespaceFromServletPath(servletPath);
+    }
+
+    protected Map getParameterMap(HttpServletRequest request) {
+        return request.getParameterMap();
+    }
+
+    protected Map getSessionMap(HttpServletRequest request) {
+        return new SessionMap(request.getSession());
+    }
+
+    /**
+ * send a http error response
+ *
+ * @param request
+ * @param response
+ * @param code the HttpServletResponse error code
+ * @param e the exception that needs to be reported on
+ */
+    protected void sendError(HttpServletRequest request, HttpServletResponse response, int code, Exception e) {
+        try {
+            // send a http error response to use the servlet defined error handler
+            // make the exception availible to the web.xml defined error page
+            request.setAttribute("javax.servlet.error.exception", e);
+
+            // for compatibility 
+            request.setAttribute("javax.servlet.jsp.jspException", e);
+
+            // send the error response
+            response.sendError(code, e.getMessage());
+
+            /*
+         response.setContentType("text/html");
+    response.setLocale(Configuration.getLocale());
+
+    PrintWriter writer = response.getWriter();
+    writer.write("Error executing action: " + e.getMessage());
+    writer.println("<pre>\n");
+    e.printStackTrace(response.getWriter());
+    writer.print("</pre>\n");
+*/
+        } catch (IOException e1) {
+        }
+    }
+
+    /**
+* Determine action name by extracting last string and removing
+* extension. (/.../.../Foo.action -> Foo)
+*/
     String getActionName(String name) {
         // Get action name ("Foo.action" -> "Foo" action)
         int beginIdx = name.lastIndexOf("/");
@@ -197,12 +258,12 @@ public class ServletDispatcher extends HttpServlet implements WebWorkStatics {
     }
 
     /**
-     * Wrap servlet request with the appropriate request. It will check to
-     * see if request is a multipart request and wrap in appropriately.
-     *
-     * @param request
-     * @return wrapped request or original request
-     */
+* Wrap servlet request with the appropriate request. It will check to
+* see if request is a multipart request and wrap in appropriately.
+*
+* @param request
+* @return wrapped request or original request
+*/
     private HttpServletRequest wrapRequest(HttpServletRequest request) throws IOException {
         // don't wrap more than once
         if (request instanceof MultiPartRequestWrapper) {
--- src/test/com/opensymphony/webwork/dispatcher/ServletDispatcherTest.java
+++ src/test/com/opensymphony/webwork/dispatcher/ServletDispatcherTest.java
@@ -2,27 +2,76 @@
  * Copyright (c) 2002-2003 by OpenSymphony
  * All rights reserved.
  */
+/*
+ * Created on 2/10/2003
+ *
+ */
 package com.opensymphony.webwork.dispatcher;
 
-import junit.framework.TestCase;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
- * ServletDispatcherTest
- * @author Jason Carreira
- * Created Mar 25, 2003 11:55:37 PM
+ * @author CameronBraid
+ *
  */
-public class ServletDispatcherTest extends TestCase {
+public class ServletDispatcherTest extends AbstractServletDispatcherTestCase {
+    //~ Constructors ///////////////////////////////////////////////////////////
+
+    /**
+     *
+     */
+    public ServletDispatcherTest(String name) {
+        super(name);
+    }
+
     //~ Methods ////////////////////////////////////////////////////////////////
+
+    /* (non-Javadoc)
+     * @see com.opensymphony.webwork.dispatcher.AbstractServletDispatcherTest#getConfigFilename()
+     */
+    public String getConfigFilename() {
+        return "com/opensymphony/webwork/dispatcher/ServletDispatcherTest-xwork.xml";
+    }
+
+    /* (non-Javadoc)
+     * @see com.opensymphony.webwork.dispatcher.AbstractServletDispatcherTest#getServletDispatcher()
+     */
+    public ServletDispatcher getServletDispatcher() {
+        return new ServletDispatcher();
+    }
+
+    /* (non-Javadoc)
+     * @see com.opensymphony.webwork.dispatcher.AbstractServletDispatcherTest#getServletPath()
+     */
+    public String getServletPath() {
+        return "/Test.action";
+    }
 
     public void testGetActionName() {
         ServletDispatcher dispatcher = new ServletDispatcher();
-        String actionName = dispatcher.getActionName("/foo/bar/Baz.action");
+        String actionName = null;
+
+        actionName = dispatcher.getActionName("/foo/bar/Baz.action");
+        assertEquals("Baz", actionName);
+
+        actionName = dispatcher.getActionName("/foo/bar/Baz");
         assertEquals("Baz", actionName);
 
         // todo do we care if foo.bar.Baz.action returns foo.bar.Baz?
         // We use aliases for everything now, so they had to set this up
         actionName = dispatcher.getActionName("/foo/bar/foo.bar.Baz.action");
         assertEquals("foo.bar.Baz", actionName);
+    }
+
+    /* (non-Javadoc)
+     * @see com.opensymphony.webwork.dispatcher.AbstractServletDispatcherTestCase#getParameterMap()
+     */
+    protected Map getParameterMap() {
+        Map map = new HashMap();
+        map.put("foo", "bar");
+
+        return map;
     }
 }
