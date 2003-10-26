e9296449c602d9dcf532f6b6722f14358b5b5c1a
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2003-10-26 22:24:56 +0000

    resolves WW-336

--- trunk/src/test/com/opensymphony/webwork/views/jsp/IteratorTagTest.java
+++ trunk/src/test/com/opensymphony/webwork/views/jsp/IteratorTagTest.java
@@ -35,7 +35,7 @@ public class IteratorTagTest extends AbstractUITagTest {
 
     public void testArrayIterator() {
         Foo foo = new Foo();
-        foo.setArray(new String[] {"test1", "test2", "test3"});
+        foo.setArray(new String[]{"test1", "test2", "test3"});
 
         stack.push(foo);
 
@@ -43,6 +43,12 @@ public class IteratorTagTest extends AbstractUITagTest {
 
         iterateThreeStrings();
     }
+
+    public void testIteratorWithDefaultValue() {
+        stack.push(new String[]{"test1", "test2", "test3"});
+        iterateThreeStrings();
+    }
+
 
     public void testCollectionIterator() {
         Foo foo = new Foo();
@@ -139,7 +145,7 @@ public class IteratorTagTest extends AbstractUITagTest {
 
     public void testStatus() {
         Foo foo = new Foo();
-        foo.setArray(new String[] {"test1", "test2", "test3"});
+        foo.setArray(new String[]{"test1", "test2", "test3"});
 
         stack.push(foo);
 
