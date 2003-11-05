d655fb9d80f3bf928d47bc4cdcc696b41edb7a54
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2003-11-05 07:58:29 +0000

    alsmost 100% compatible with the old EL!

--- src/java/com/opensymphony/webwork/views/jsp/CompatUtil.java
+++ src/java/com/opensymphony/webwork/views/jsp/CompatUtil.java
@@ -1,16 +1,28 @@
 package com.opensymphony.webwork.views.jsp;
 
+import com.opensymphony.webwork.config.Configuration;
+
 /**
  * User: plightbo
  * Date: Nov 4, 2003
  * Time: 7:32:57 AM
  */
 public class CompatUtil {
+    private static boolean compatMode = false;
+    static {
+        if (Configuration.isSet("compatibility.mode")) {
+            compatMode = "true".equals(Configuration.getString("compatibility.mode"));
+        }
+    }
+
     public static String compat(String expr) {
-        expr = expr.replaceAll("\\.\\.", "[1]");
-        expr = expr.replaceAll("\\.", "top");
-        expr = expr.replaceAll("([^\\d\\/ ]+?)\\/([^\\d\\/ ]+?)", "$1\\.$2");
-        expr = expr.replaceAll("\\@([^\\. \\[]+)", "#request\\.get\\($1\\)");
+        if (compatMode) {
+            expr = expr.replaceAll("\\.\\.", "[1]");
+            expr = expr.replaceAll("\\.", "top");
+            expr = expr.replaceAll("([^\\d\\/ ]+?)\\/([^\\d\\/ ]+?)", "$1\\.$2");
+            expr = expr.replaceAll("\\@([^\\. \\[]+)", "#attr\\.get\\($1\\)");
+            expr = expr.replaceAll("\\$([^\\. \\[]+)", "#parameters\\.get\\($1\\)");
+        }
 
         return expr;
     }
