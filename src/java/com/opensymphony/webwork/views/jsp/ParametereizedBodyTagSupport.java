a56c8a3d97eefef7ba6119cf2a36a7f0ef06ab85
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2003-11-16 23:36:23 +0000

    resolves WW-374 -- URLTag is now almost a carbon-copy of the 1.3 version

---
+++ src/java/com/opensymphony/webwork/views/jsp/ParametereizedBodyTagSupport.java
@@ -0,0 +1,40 @@
+package com.opensymphony.webwork.views.jsp;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * User: plightbo
+ * Date: Nov 16, 2003
+ * Time: 3:25:09 PM
+ */
+public class ParametereizedBodyTagSupport extends WebWorkBodyTagSupport implements ParameterizedTag {
+    //~ Instance fields ////////////////////////////////////////////////////////
+    final protected static Log log = LogFactory.getLog(ParameterizedTagSupport.class);
+
+    Map params;
+
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    public Map getParams() {
+        if (params == null) {
+            params = new HashMap();
+        }
+
+        return params;
+    }
+
+    public void addParam(String key, Object value) {
+        if (key != null) {
+            Map myParams = getParams();
+
+            if (value == null) {
+                myParams.remove(key);
+            } else {
+                params.put(key, value);
+            }
+        }
+    }
