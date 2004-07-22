97757a2587379bba29bfa648d513a3118c282fca
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2004-07-22 17:00:31 +0000

    WW-575: support ports other than 80/443

--- trunk/src/test/com/opensymphony/webwork/config/ConfigurationTest.java
+++ trunk/src/test/com/opensymphony/webwork/config/ConfigurationTest.java
@@ -31,7 +31,7 @@ public class ConfigurationTest extends TestCase {
         assertEquals("de", locale.getLanguage());
 
         int count = getKeyCount();
-        assertEquals(12, count);
+        assertEquals(14, count);
     }
 
     public void testSetConfiguration() {
