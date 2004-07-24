d9e009315f6e462d58f6f1527d6547b48ae38f9b
jcarreira <jcarreira@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2004-07-24 07:01:51 +0000

    WW-531: Making RequestMap be backed by the request so it will pick up changes to the attributes

--- trunk/src/java/com/opensymphony/webwork/dispatcher/RequestMap.java
+++ trunk/src/java/com/opensymphony/webwork/dispatcher/RequestMap.java
@@ -20,74 +20,115 @@ import javax.servlet.http.HttpServletRequest;
 public class RequestMap extends AbstractMap implements Serializable {
     //~ Instance fields ////////////////////////////////////////////////////////
 
-    HashSet entrySet = new HashSet();
+    Set entries;
+    private HttpServletRequest request;
 
     //~ Constructors ///////////////////////////////////////////////////////////
 
     /**
-     * Enumerates over all request attributes and creates a simple Map of attributes.
-     *
-     * @param request the http servlet request.
-     */
+ * Saves the request to use as the backing for getting and setting values
+ *
+ * @param request the http servlet request.
+ */
     public RequestMap(final HttpServletRequest request) {
-        for (Enumeration attrNames = request.getAttributeNames();
-                attrNames.hasMoreElements();) {
-            final String attrName = (String) attrNames.nextElement();
-            entrySet.add(new Map.Entry() {
-                    public Object getKey() {
-                        return attrName;
-                    }
-
-                    public Object getValue() {
-                        return request.getAttribute(attrName);
-                    }
-
-                    public Object setValue(Object value) {
-                        request.setAttribute(attrName, value);
-
-                        return value;
-                    }
-
-                    public int hashCode() {
-                        return ((getKey() == null) ? 0 : getKey().hashCode()) ^ ((getValue() == null) ? 0 : getValue().hashCode());
-                    }
-
-                    public boolean equals(Object obj) {
-                        if ((obj != null) && obj instanceof Entry) {
-                            Entry me = (Entry) obj;
-
-                            Object key = me.getKey();
-                            Object value = me.getValue();
-
-                            if (key == null) {
-                                return false;
-                            }
-
-                            if (value == null) {
-                                if (getValue() != null) {
-                                    return false;
-                                } else {
-                                    return key.equals(getKey());
-                                }
-                            }
-
-                            return key.equals(getKey()) && value.equals(getValue());
-                        }
-
-                        return false;
-                    }
-                });
-        }
+        this.request = request;
     }
 
     //~ Methods ////////////////////////////////////////////////////////////////
 
     /**
-     * Returns a Set of all request attributes.
-     *
-     * @return a Set of all request attributes.
-     */
+ * Removes all attributes from the request as well as clears entries in this map.
+ */
+    public void clear() {
+        Enumeration keys = request.getAttributeNames();
+
+        while (keys.hasMoreElements()) {
+            String key = (String) keys.nextElement();
+            request.removeAttribute(key);
+        }
+    }
+
+    /**
+ * Returns a Set of attributes from the http request.
+ *
+ * @return a Set of attributes from the http request.
+ */
     public Set entrySet() {
-        return entrySet;
+        if (entries == null) {
+            entries = new HashSet();
+
+            Enumeration enumeration = request.getAttributeNames();
+
+            while (enumeration.hasMoreElements()) {
+                final String key = enumeration.nextElement().toString();
+                final Object value = request.getAttribute(key);
+                entries.add(new Entry() {
+                        public boolean equals(Object obj) {
+                            Entry entry = (Entry) obj;
+
+                            return ((key == null) ? (entry.getKey() == null) : key.equals(entry.getKey())) && ((value == null) ? (entry.getValue() == null) : value.equals(entry.getValue()));
+                        }
+
+                        public int hashCode() {
+                            return ((key == null) ? 0 : key.hashCode()) ^ ((value == null) ? 0 : value.hashCode());
+                        }
+
+                        public Object getKey() {
+                            return key;
+                        }
+
+                        public Object getValue() {
+                            return value;
+                        }
+
+                        public Object setValue(Object obj) {
+                            request.setAttribute(key.toString(), obj);
+
+                            return value;
+                        }
+                    });
+            }
+        }
+
+        return entries;
+    }
+
+    /**
+ * Returns the request attribute associated with the given key or <tt>null</tt> if it doesn't exist.
+ *
+ * @param key the name of the request attribute.
+ * @return the request attribute or <tt>null</tt> if it doesn't exist.
+ */
+    public Object get(Object key) {
+        return request.getAttribute(key.toString());
+    }
+
+    /**
+ * Saves an attribute in the request.
+ *
+ * @param key   the name of the request attribute.
+ * @param value the value to set.
+ * @return the object that was just set.
+ */
+    public Object put(Object key, Object value) {
+        entries = null;
+        request.setAttribute(key.toString(), value);
+
+        return get(key);
+    }
+
+    /**
+ * Removes the specified request attribute.
+ *
+ * @param key the name of the attribute to remove.
+ * @return the value that was removed or <tt>null</tt> if the value was not found (and hence, not removed).
+ */
+    public Object remove(Object key) {
+        entries = null;
+
+        Object value = get(key);
+        request.removeAttribute(key.toString());
+
+        return value;
     }
 }
