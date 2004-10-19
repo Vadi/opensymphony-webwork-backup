4c078b89d4bbb9d10ded396497a9281fe6ef674d
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2004-10-19 05:53:14 +0000

    webflow usage helper and manifest entry

--- src/webflow/com/opensymphony/webwork/webFlow/WebFlow.java
+++ src/webflow/com/opensymphony/webwork/webFlow/WebFlow.java
@@ -18,11 +18,19 @@ public class WebFlow {
     public static void main(String[] args) {
         LOG.info("WebFlow starting...");
 
+        if (args.length != 6) {
+            System.out.println("Usage: -config CONFIG_DIR -views VIEWS_DIRS -output OUTPUT");
+            System.out.println("       CONFIG_DIR => a directory containing xwork.xml");
+            System.out.println("       VIEWS_DIRS => comma seperated list of dirs containing JSPs, VMs, etc");
+            System.out.println("       OUPUT      => the directory where the output should go");
+            return;
+        }
+
         String configDir = getArg(args, "config");
         String views = getArg(args, "views");
 
         XWorkConfigRetriever.setConfiguration(configDir, views.split("[, ]+"));
-        Renderer renderer = new DOTRenderer("temp");
+        Renderer renderer = new DOTRenderer(getArg(args, "output"));
         renderer.render();
     }
 
