858a63a7b948e1ad9dcadf4cdb8d1e90307cddf6
matthew_payne <matthew_payne@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-03-07 02:37:09 +0000

    Need to implement additional methods as NOPs(for now) so ww will compile under jdk1.5
    Issue number:  Jira down on sunday
    Obtained from:
    Submitted by:  Matthew Payne
    Reviewed by:
    CVS: ----------------------------------------------------------------------
    CVS: Issue number:
    CVS:   If this change addresses one or more issues,
    CVS:   then enter the issue number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to the project by someone else; i.e.,
    CVS:   they sent us a patch or a set of diffs, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.

--- trunk/src/java/com/opensymphony/webwork/views/xslt/ArrayAdapter.java
+++ trunk/src/java/com/opensymphony/webwork/views/xslt/ArrayAdapter.java
@@ -6,6 +6,11 @@ package com.opensymphony.webwork.views.xslt;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Attr;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Node;
+import org.w3c.dom.TypeInfo;
+import org.w3c.dom.UserDataHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,4 +49,132 @@ public class ArrayAdapter extends DefaultElementAdapter {
 
         return children;
     }
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#getBaseURI()
+	 */
+	public String getBaseURI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#compareDocumentPosition(org.w3c.dom.Node)
+	 */
+	public short compareDocumentPosition(Node other) throws DOMException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#getTextContent()
+	 */
+	public String getTextContent() throws DOMException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#setTextContent(java.lang.String)
+	 */
+	public void setTextContent(String textContent) throws DOMException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#isSameNode(org.w3c.dom.Node)
+	 */
+	public boolean isSameNode(Node other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#lookupPrefix(java.lang.String)
+	 */
+	public String lookupPrefix(String namespaceURI) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#isDefaultNamespace(java.lang.String)
+	 */
+	public boolean isDefaultNamespace(String namespaceURI) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#lookupNamespaceURI(java.lang.String)
+	 */
+	public String lookupNamespaceURI(String prefix) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#isEqualNode(org.w3c.dom.Node)
+	 */
+	public boolean isEqualNode(Node arg) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#getFeature(java.lang.String, java.lang.String)
+	 */
+	public Object getFeature(String feature, String version) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#setUserData(java.lang.String, java.lang.Object, org.w3c.dom.UserDataHandler)
+	 */
+	public Object setUserData(String key, Object data, UserDataHandler handler) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Node#getUserData(java.lang.String)
+	 */
+	public Object getUserData(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Element#getSchemaTypeInfo()
+	 */
+	public TypeInfo getSchemaTypeInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Element#setIdAttribute(java.lang.String, boolean)
+	 */
+	public void setIdAttribute(String name, boolean isId) throws DOMException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Element#setIdAttributeNS(java.lang.String, java.lang.String, boolean)
+	 */
+	public void setIdAttributeNS(String namespaceURI, String localName, boolean isId) throws DOMException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.w3c.dom.Element#setIdAttributeNode(org.w3c.dom.Attr, boolean)
+	 */
+	public void setIdAttributeNode(Attr idAttr, boolean isId) throws DOMException {
+		// TODO Auto-generated method stub
+		
+	}
 }
