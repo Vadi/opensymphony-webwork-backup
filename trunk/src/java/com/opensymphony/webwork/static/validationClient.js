e91d7dfcce6a5cddb594a446c4035f8f703c1f6c
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-05-12 23:36:12 +0000

    replaced validation with DWR (was dojo + home-made marshalling)

--- trunk/src/java/com/opensymphony/webwork/static/validationClient.js
+++ trunk/src/java/com/opensymphony/webwork/static/validationClient.js
@@ -3,76 +3,26 @@
  * Common code to interface with the validationServlet 
  *
  */
-function xmlEncode(s) {
-	return s;
-}
 
 function ValidationClient(servletUrl) {
 
 	this.servletUrl = servletUrl;
 
-	makeValidateXml = function(form, nameSpace, actionName) {
-	    var xml = '<r a="' + xmlEncode(actionName) + '" ns="' + xmlEncode(nameSpace) +'">\n';
+	this.validate = function(input, namespace, actionName) {
+		var vc = this;
+		var form = input.form;
+		var params = "";
 	    for (var i = 0; i < form.elements.length; i++) {
 	        var e = form.elements[i];
-			// TODO get the value of the input dependant on the type of the input
-	        xml = xml + '<p n="' + xmlEncode(e.name) + '">' + xmlEncode(e.value) + '</p>\n';
-	    }
-	    xml = xml + '</r>\n';
-	    return xml;
-	}
-
-	extractErrorMessages = function(node) {
-        var errorMessageXml = node.getElementsByTagName("errorMessage");
-        var ary = new Array();
-        for (var i = 0; i < errorMessageXml.length; i++) {
-
-			// why oh why do firefox and IE have different ways to access text content of xml nodes 
-        	if (errorMessageXml[i].text) {
-	        	ary[i] = errorMessageXml[i].text;
-	        }
-	        else {
-	        	ary[i] = errorMessageXml[i].textContent;
+	        params = params + e.name + "=" + e.value;
+	        if ((i + 1) < form.elements.length) {
+	            params = params + ";";
 	        }
         }
-        return ary;
-	}
 
-
-	
-	this.validate = function(input, nameSpace, actionName) {
-		var vc = this;
-		dojo.io.bind({
-			mimetype: "text/xml",
-			url: this.servletUrl,
-			method: "post",
-			postContent: makeValidateXml(input.form, nameSpace, actionName),
-			load: function(type, data, evt) { 
-
-		        var root = data.getElementsByTagName("errors")[0];
-
-		        // build a javascript object to hold the errors
-		        var errors = new Object();
-		
-				// add the actionErrors
-		        errors.actionErrors = new Array();
-		
-				errors.actionErrors = extractErrorMessages(root.getElementsByTagName("actionErrors")[0]);
-		
-				// add the fieldErrors
-				errors.fieldErrors = new Object();
-				var fieldErrorsXml = root.getElementsByTagName("fieldErrors");
-		        for (var i = 0; i < fieldErrorsXml.length; i++) {
-		        	var fieldName = fieldErrorsXml[i].getAttribute("fieldName");
-		            errors.fieldErrors[fieldName] = extractErrorMessages(fieldErrorsXml[i]);
-				}
-		
-		        // make the callback with the errors
-		        vc.onErrors(input, errors);
-        	},
-        	error : function(a,b) {
-	        }
-		});
+		validator.doPost(function(action) {
+		    vc.onErrors(input, action);
+		}, namespace, actionName, params);
     }
     
 
@@ -83,5 +33,4 @@ function ValidationClient(servletUrl) {
 	}
 	
 	return this;
-
 }
---
+++ trunk/src/java/com/opensymphony/webwork/validators/DWRValidator.java
@@ -0,0 +1,74 @@
+package com.opensymphony.webwork.validators;
+
+import com.opensymphony.webwork.dispatcher.ServletDispatcher;
+import com.opensymphony.xwork.*;
+import com.opensymphony.xwork.config.entities.ActionConfig;
+import uk.ltd.getahead.dwr.ExecutionContext;
+
+import javax.servlet.ServletConfig;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+
+/**
+ * User: plightbo
+ * Date: Dec 11, 2004
+ * Time: 6:17:58 PM
+ */
+public class DWRValidator {
+    public ValidationAwareSupport doPost(String namespace, String action, String params) throws Exception {
+        ServletConfig sc = ExecutionContext.get().getServletConfig();
+        HashMap ctx = new HashMap();
+
+        HashMap paramMap = new HashMap();
+        String[] split = params.split(";");
+        for (int i = 0; i < split.length; i++) {
+            String s = split[i];
+            String[] strings = s.split("=");
+            if (strings.length == 2) {
+                paramMap.put(strings[0], strings[1]);
+            } else {
+                paramMap.put(strings[0], "");
+            }
+        }
+
+        ctx.put(ActionContext.PARAMETERS, paramMap);
+        ctx.put(ServletDispatcher.SERVLET_CONFIG, sc);
+
+        ValidatorActionProxy proxy = new ValidatorActionProxy(namespace, action, ctx);
+        proxy.execute();
+        Action a = proxy.getAction();
+
+        if (a instanceof ValidationAware) {
+            ValidationAware aware = (ValidationAware) a;
+            ValidationAwareSupport vas = new ValidationAwareSupport();
+            vas.setActionErrors(aware.getActionErrors());
+            vas.setActionMessages(aware.getActionMessages());
+            vas.setFieldErrors(aware.getFieldErrors());
+
+            return vas;
+        } else {
+            return null;
+        }
+    }
+
+    public static class ValidatorActionInvocation extends DefaultActionInvocation {
+        protected ValidatorActionInvocation(ActionProxy proxy, Map extraContext) throws Exception {
+            super(proxy, extraContext, true);
+        }
+
+        protected String invokeAction(Action action, ActionConfig actionConfig) throws Exception {
+            return Action.NONE; // don't actually execute the action
+        }
+    }
+
+    public static class ValidatorActionProxy extends DefaultActionProxy {
+        protected ValidatorActionProxy(String namespace, String actionName, Map extraContext) throws Exception {
+            super(namespace, actionName, extraContext, false);
+        }
+
+        protected void prepare() throws Exception {
+            invocation = new ValidatorActionInvocation(this, extraContext);
+        }
+    }
+}
