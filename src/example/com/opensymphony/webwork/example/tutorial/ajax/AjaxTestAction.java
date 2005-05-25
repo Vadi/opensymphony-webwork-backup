ba61d40fe9a54f3f4d65e2bbbbc7e34590caf05d
roughley <roughley@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-05-25 03:27:14 +0000

    moved into tutorial directory

---
+++ src/example/com/opensymphony/webwork/example/tutorial/ajax/AjaxTestAction.java
@@ -0,0 +1,29 @@
+package com.opensymphony.webwork.example.tutorial.ajax;
+
+import com.opensymphony.xwork.Action;
+
+
+/**
+ * @author		Ian Roughley
+ * @version		$Id$
+ */
+public class AjaxTestAction implements Action {
+
+    private static int counter = 0;
+
+    public long getServerTime() {
+        return System.currentTimeMillis();
+    }
+
+    public int getCount() {
+        return ++counter;
+    }
+
+    public int getPeriod() {
+        return 3;
+    }
+
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
