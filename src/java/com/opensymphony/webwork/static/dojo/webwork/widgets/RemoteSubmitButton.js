9412382bc0f6fb84fb280ebc76fcee7eb927f11c
cameronbraid <cameronbraid@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-06-01 16:22:30 +0000

    subclassed RemoteSubmitButton to create a RemoteSubmitButtonAndDiv class that outputs the results of a form post into a div

--- src/java/com/opensymphony/webwork/static/dojo/webwork/widgets/RemoteSubmitButton.js
+++ src/java/com/opensymphony/webwork/static/dojo/webwork/widgets/RemoteSubmitButton.js
@@ -1,5 +1,4 @@
 dojo.hostenv.startPackage("webwork.widgets.RemoteSubmitButton");
-dojo.hostenv.startPackage("webwork.widgets.HTMLRemoteSubmitButton");
 
 dojo.hostenv.loadModule("dojo.io.*");
 
@@ -32,21 +31,34 @@ webwork.widgets.HTMLRemoteSubmitButton = function() {
 	this.isContainer = false;
 	this.widgetType = "RemoteSubmitButton";
 
+	// the id of the form object to bind to
 	this.formId = "";
+
+	// the name of the global javascript variable to associate with this widget instance
+	this.id = "";
+	
+	// javascript code to be evaled when data arrives - arguments are (eventType, data)
 	this.onLoad = "";
+	
+	// the type of the input button - can be image
 	this.type = "submit";
 
+	// the template button instance
 	this.button = null;
 	
 	this.fillInTemplate = function() {
-		webwork.Util.passThroughArgs(_this, _this.button);
+		webwork.Util.passThroughArgs(_this.extraArgs, _this.button);
 		_this.button.type = _this.type;
+		if (_this.id != "") {
+			window[_this.id] = this;
+		}
     }
     
     this.submitForm = function() {
 		dojo.io.bind({
 		    load: _this.load,
-	    	formNode: document.getElementById(_this.formId)
+	    	formNode: document.getElementById(_this.formId),
+			useCache: false
 		});
     }
     
@@ -60,12 +72,42 @@ webwork.widgets.HTMLRemoteSubmitButton = function() {
     }
 
 }
+webwork.widgets.HTMLRemoteSubmitButtonAndDiv = function() {
+
+	webwork.widgets.HTMLRemoteSubmitButton.call(this);
+
+	this.widgetType = 'RemoteSubmitButtonAndDiv';
+
+	this.divId = "";
+	
+	var _this = this;
+	
+    // allow this to be overriden by subclasses - say one that renders the response into a div
+    this.load = function(type, data) {
+	
+    	// by default, eval the onLoad handler
+		if (_this.divId == "") {
+			dj_debug('no divId specified');
+		}else{
+			var d = document.getElementById(_this.divId);
+			if (d) {
+				d.innerHTML = data;
+			}else{
+				dj_debug('div ' + this._divId + ' not found');
+			}
+		}
+
+    }
+
+}
 
 // is this needed as well as dojo.webui.Widget.call(this);
 dj_inherits(webwork.widgets.HTMLRemoteSubmitButton, dojo.webui.DomWidget);
+dj_inherits(webwork.widgets.HTMLRemoteSubmitButtonAndDiv, webwork.widgets.HTMLRemoteSubmitButton);
 
 // allow the markup parser to construct these widgets
 dojo.webui.widgets.tags.addParseTreeHandler("dojo:RemoteSubmitButton");
+dojo.webui.widgets.tags.addParseTreeHandler("dojo:RemoteSubmitButtonAndDiv");
 
 // register this widget constructor with the widget manager
 dojo.webui.widgetManager.registerWidgetPackage('webwork.widgets');
--- src/webapp/sandbox/index.ftl
+++ src/webapp/sandbox/index.ftl
@@ -20,22 +20,21 @@
 			.sampleBox {
 				text-align : center;
 				vertical-align: middle;
-				border : 1px solid;
 				width:250px;
 			}
 		</style>
 	</head>
 	
 	<body>
-	
+
 		<@example heading='RemoteDiv : XHTML Widget Tag'>
 <dojo:remotediv 
 	href="data/date.jsp?sleep=1000" 
 	loadingHtml='Loading...'
 	delay='1000'
-	updateFreq='2000' 
+	refresh='2000' 
 	class='sampleBox'
-	style='border-color:red;'
+	style='border: 1px solid red;'
 	>
 		<b>initial content</b>
 </dojo:remotediv>
@@ -47,22 +46,37 @@
 	href="data/date.jsp" 
 	delay='1000'
 	class='sampleBox'
-	style='border-color:orange;'
+	style='border: 1px solid orange;'
 	>
 		<b>initial content</b>
 </div>
 </@>
 
 <@example heading='RemoteSubmitButton'>
-<form id='theForm' action='data/form.ftl' oncubmit='return false;'>
+<form id='theForm' action='data/form.ftl' onSubmit='return false;' method='post'>
 	<input type='text' name='name' value='WebWork User'>
 </form>
 
 <dojo:RemoteSubmitButton
 	formId="theForm" 
 	onLoad="alert(data)"
+	value='Post'
 />
-</@>
+</@><b>Note : RemoteSubmitButton has issues with IE .. some strange SystemError with http.responseText </b>
+
+
+<@example heading='HTMLRemoteSubmitButtonAndDiv'>
+<form id='theForm2' action='data/form.ftl' onSubmit='return false;' method='post'>
+	<input type='text' name='name' value='WebWork User'>
+</form>
+
+<dojo:RemoteSubmitButtonAndDiv
+	formId='theForm2'
+	divId='theForm2Output'
+	value='Post'
+/>
+<div id='theForm2Output'>form 2 will render to here</div>
+</@><b>Note : RemoteSubmitButtonAndDiv has issues with IE .. some strange SystemError with http.responseText </b>
 
 	</body>
 </html>
