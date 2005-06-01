19e691de17a6c1448dbaacdbdb88383af19eeac6
cameronbraid <cameronbraid@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-06-01 11:00:52 +0000

    added a new build target that builds dojo with out dojo.profile.js file, and coppies the build (from dojo/releases) into src/java/com/os/static/dojo

---
+++ trunk/dojo.profile.js
@@ -0,0 +1,10 @@
+var dependencies = [ 
+	"dojo.io.IO",
+	"dojo.event.*",
+	"dojo.io.BrowserIO",
+	"dojo.xml.Parse",
+	"dojo.webui.widgets.*",
+	"dojo.webui.DomWidget"
+];
+
+load("getDependencyList.js");
