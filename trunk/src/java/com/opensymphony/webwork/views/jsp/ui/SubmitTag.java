e76fc4eaa794708d263d0758ab18d0b9ca250688
roughley <roughley@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-06-14 22:41:53 +0000

    new functionality for remote form submitting via ajax

--- trunk/src/java/com/opensymphony/webwork/views/jsp/ui/SubmitTag.java
+++ trunk/src/java/com/opensymphony/webwork/views/jsp/ui/SubmitTag.java
@@ -6,6 +6,8 @@ package com.opensymphony.webwork.views.jsp.ui;
 
 import com.opensymphony.xwork.util.OgnlValueStack;
 
+import javax.servlet.jsp.JspException;
+
 
 /**
  * @author Matt Ho <a href="mailto:matt@indigoegg.com">&lt;matt@indigoegg.com&gt;</a>
@@ -45,5 +47,62 @@ public class SubmitTag extends AbstractUITag {
         super.evaluateParams(stack);
 
         addParameter("align", findString(alignAttr));
+    }
+
+    /**
+     * @return the 'id' of the enclosing form
+     *
+     * @throws JspException if there is no enclosing form or the id is null
+     */
+    public String getFormId() throws JspException {
+        FormTag form = (FormTag)findAncestorWithClass( this, FormTag.class );
+        if( null!=form && null!=((FormTag)form).getId() )
+            return ((FormTag)form).getId();
+        else
+            throw new JspException( "When using the ajax theme an enclosing form with an 'id' attribute is required" );
+    }
+
+    /**
+     * @return the 'id' of the html element to place the html from submitting the form
+     */
+    public String getFormResultId() {
+        FormTag form = (FormTag)findAncestorWithClass( this, FormTag.class );
+        if( null!=form )
+            return ((FormTag)form).getResultDivId();
+        else
+            return null;
+    }
+
+    /**
+     * @return the JS to evaluate after submitting the form
+     */
+    public String getOnLoadJS() {
+        FormTag form = (FormTag)findAncestorWithClass( this, FormTag.class );
+        if( null!=form )
+            return ((FormTag)form).getOnLoadJS();
+        else
+            return null;
+    }
+
+    /**
+     * @return the topics to listen to and submit the form when a message is recieved
+     */
+    public String getListenTopics() {
+        FormTag form = (FormTag)findAncestorWithClass( this, FormTag.class );
+        if( null!=form )
+            return ((FormTag)form).getListenTopics();
+        else
+            return null;
+    }
+
+    /**
+     * @return the topics to publish to when the form has been submitted
+     */
+    public String getNotifyTopics() {
+        FormTag form = (FormTag)findAncestorWithClass( this, FormTag.class );
+        if( null!=form )
+            return ((FormTag)form).getNotifyTopics();
+        else
+            return null;
     }
 }
