04b2936565c2a1da944ea1285f0047abaa82ad5d
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-06-16 04:24:57 +0000

    moving example app to its own directory and IDEA module

---
+++ webwork-example/src/test/com/opensymphony/webwork/example/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.opensymphony.webwork.example;
+
+import junit.framework.TestCase;
+
+/**
+ * User: plightbo
+ * Date: Jun 15, 2005
+ * Time: 9:19:18 PM
+ */
+public class SimpleTest extends TestCase {
+    public void testFoo() {
+        assertEquals(4, 2 + 2);
+    }
+}
