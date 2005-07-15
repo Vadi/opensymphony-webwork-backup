b903c8d86c44228bd935ec4e02f285dccb40c1b2
roughley <roughley@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-07-15 18:44:15 +0000

    remote div test case

---
+++ trunk/src/test/com/opensymphony/webwork/views/jsp/ui/ajax/RemoteUpdateDivTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.webwork.views.jsp.ui.ajax;
+
+import com.opensymphony.webwork.TestAction;
+import com.opensymphony.webwork.views.jsp.AbstractUITagTest;
+import com.opensymphony.webwork.views.jsp.ui.LabelTag;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+
+/**
+ * @author Ian Roughley<a href="mailto:ian@fdar.com">&lt;ian@fdar.com&gt;</a>
+ * @version $Id$
+ */
+public class RemoteUpdateDivTest extends AbstractUITagTest {
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+
+    public void testSimple() throws Exception {
+        TestAction testAction = (TestAction) action;
+        testAction.setFoo("bar");
+
+        RemoteUpdateDivTag tag = new RemoteUpdateDivTag();
+        tag.setPageContext(pageContext);
+
+        tag.setId("mylabel");
+        tag.setTheme("ajax");
+        tag.addParameter( "href", "a" );
+        tag.addParameter( "loadingText", "b" );
+        tag.addParameter( "errorText", "c" );
+        tag.addParameter( "showErrorTransportText", "d" );
+        tag.addParameter( "delay", "e" );
+        tag.addParameter( "updateFreq", "f" );
+        tag.addParameter( "listenTopics", "g" );
+        tag.addParameter( "afterLoading", "h" );
+
+        assertEquals( tag.doStartTag(), TagSupport.EVAL_BODY_INCLUDE );
+        assertEquals( tag.doEndTag(), TagSupport.EVAL_PAGE );
+
+        verify(RemoteUpdateDivTest.class.getResource("remotediv-1.txt"));
+    }
+
+}
