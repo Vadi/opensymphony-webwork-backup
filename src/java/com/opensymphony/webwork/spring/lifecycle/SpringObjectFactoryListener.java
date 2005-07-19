18e67e952b4900dcf0c467b8d1ee2fb0ea404a21
jcarreira <jcarreira@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-07-19 17:18:32 +0000

    New XWork jar with Spring support and some Spring web support classes. Need to test.

---
+++ src/java/com/opensymphony/webwork/spring/lifecycle/SpringObjectFactoryListener.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2005 Opensymphony. All Rights Reserved.
+ */
+package com.opensymphony.webwork.spring.lifecycle;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.opensymphony.xwork.ObjectFactory;
+import com.opensymphony.xwork.spring.SpringObjectFactory;
+
+/**
+ * ApplicationContextListener that sets up the environment so that XWork and
+ * Webwork can load data and information from Spring. Relies on Spring's
+ * {@link org.springframework.web.context.ContextLoaderListener}having been
+ * called first.
+ * 
+ * @author sms
+ */
+public class SpringObjectFactoryListener implements ServletContextListener
+{
+    /**
+     * Creates a {@link com.opensymphony.xwork.spring.SpringObjectFactory}and sets that as the default
+     * {@link ObjectFactory}to use for XWork.
+     *
+     * @param event
+     *            The ServletContextEvent.
+     */
+    public void contextInitialized(ServletContextEvent event) {
+        ServletContext app = event.getServletContext();
+        ApplicationContext appContext = WebApplicationContextUtils
+                .getWebApplicationContext(app);
+        SpringObjectFactory objFactory = new SpringObjectFactory();
+        objFactory.setApplicationContext(appContext);
+        ObjectFactory.setObjectFactory(objFactory);
+    }
+    public void contextDestroyed(ServletContextEvent arg0) {
+        // Nothing to do.
+    }
+}
