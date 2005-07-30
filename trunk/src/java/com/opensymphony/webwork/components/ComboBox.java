609987fbe6a651fbf118b8fa4e5b361896e61b19
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-07-30 21:51:07 +0000

    combobox working again

--- trunk/src/java/com/opensymphony/webwork/components/ComboBox.java
+++ trunk/src/java/com/opensymphony/webwork/components/ComboBox.java
@@ -19,6 +19,10 @@ public class ComboBox extends TextField {
         super(stack, request, response);
     }
 
+    protected String getDefaultTemplate() {
+        return TEMPLATE;
+    }
+
     public void evaluateExtraParams() {
         super.evaluateExtraParams();
 
