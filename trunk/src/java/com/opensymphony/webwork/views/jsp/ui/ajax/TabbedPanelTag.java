bd570f552a1409f7543f630ab50b55eabe8abc67
roughley <roughley@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-08-02 02:40:36 +0000

    updated so that class compiles using new UI scheme

--- trunk/src/java/com/opensymphony/webwork/views/jsp/ui/ajax/TabbedPanelTag.java
+++ trunk/src/java/com/opensymphony/webwork/views/jsp/ui/ajax/TabbedPanelTag.java
@@ -2,15 +2,15 @@ package com.opensymphony.webwork.views.jsp.ui.ajax;
 
 import com.opensymphony.webwork.views.jsp.ui.AbstractUITag;
 import com.opensymphony.webwork.components.ajax.ContentPane;
-import com.opensymphony.webwork.components.ajax.JavascriptEmitter;
+import com.opensymphony.webwork.components.ajax.Panel;
+import com.opensymphony.webwork.components.ajax.TabbedPanel;
+import com.opensymphony.webwork.components.UIBean;
+import com.opensymphony.xwork.util.OgnlValueStack;
 
 import java.util.List;
-import java.util.ArrayList;
 
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.tagext.BodyTag;
-import javax.servlet.jsp.tagext.BodyContent;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * TabbedPanelTag
@@ -18,41 +18,10 @@ import javax.servlet.jsp.tagext.BodyContent;
  * @author Jason Carreira <jcarreira@eplus.com>
  * @author <a href="ian@fdar.com">Ian Roughley</a>
  */
-public class TabbedPanelTag extends AbstractUITag implements BodyTag {
+public class TabbedPanelTag extends AbstractUITag {
 
-    public static final String TEMPLATE_CLOSE = "tabbedpanel-close";
-    public static final String COMPONENT_JS = "tabbedpanel-js.vm";
-    final private static String COMPONENT_NAME = TabbedPanelTag.class.getName();
-
-    private String innerBodyContent;
-    private JspWriter innerBodyWriter;
-    private BodyContent bodyContent;
-
-    /**
-     * Constructor.
-     */
-    public TabbedPanelTag() {
-        tabs = new ArrayList();
-    }
-
-    public String getDefaultOpenTemplate() {
-        return null;  // the opening tab is not used
-    }
-
-    public void setBodyContent(BodyContent bodyContent) {
-        this.bodyContent = bodyContent;
-    }
-
-    public void doInitBody() throws JspException {
-        // do nothing
-    }
-
-    public int doStartTag() throws JspException {
-        return BodyTag.EVAL_BODY_BUFFERED;
-    }
-
-    protected String getDefaultTemplate() {
-        return TEMPLATE_CLOSE;
+    public UIBean getBean(OgnlValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+        return new Panel(stack, req, res);
     }
 
     /**
@@ -60,7 +29,7 @@ public class TabbedPanelTag extends AbstractUITag implements BodyTag {
      * @return the list of {@link ContentPane} tabs for this tab panel
      */
     public List getTabs() {
-        return tabs;
+        return ((TabbedPanel) bean).getTabs();
     }
 
     /**
@@ -69,7 +38,7 @@ public class TabbedPanelTag extends AbstractUITag implements BodyTag {
      * @param pane a new {@link com.opensymphony.webwork.components.ajax.ContentPane} to be rendered
      */
     public void addTab( ContentPane pane ) {
-        tabs.add(pane);
+        ((TabbedPanel) bean).addTab(pane);
     }
 
     /**
@@ -79,54 +48,6 @@ public class TabbedPanelTag extends AbstractUITag implements BodyTag {
      */
     public String getTopicName() {
         return "topic_tab_" + id + "_selected";
-    }
-
-    /**
-     * Get the body of this tag and store it away to be rendered after the tab headers.
-     *
-     * @see javax.servlet.jsp.tagext.BodyTag#doAfterBody()
-     */
-    public int doAfterBody() throws JspException {
-        BodyContent body = bodyContent;
-        innerBodyWriter = body.getEnclosingWriter();
-        innerBodyContent = body.getString();
-        body.clearBody();
-        return SKIP_BODY;
-    }
-
-    /**
-     * Note: no idea why this.clone() needs to be used, but when it wasn't the same
-     * tag instance was re-used for each tag cause incorrect javascript to be generated.
-     * <p/>
-     *
-     * Write the closing template (for the header tabs), and then write the contents of
-     * the body tag, which will be the div's for each tab's contents.
-     *
-     * @see javax.servlet.jsp.tagext.TagSupport#doEndTag()
-     */
-    public int doEndTag() throws JspException {
-        try {
-            try {
-                ((TopicScopeTag)findAncestorWithClass( this, TopicScopeTag.class )).addEmitter((JavascriptEmitter)this.clone());
-            } catch (CloneNotSupportedException e) {
-                throw new JspException(e);
-            }
-
-            evaluateParams(getStack());
-
-            try {
-                mergeTemplate(getTemplateName());
-                innerBodyWriter.println(innerBodyContent);
-                return BodyTag.EVAL_BODY_AGAIN;
-
-            } catch (Exception e) {
-                throw new JspException("Fatal exception caught in " + this.getClass().getName() + " tag class, doEndTag: " + e.getMessage(), e);
-            }
-        } finally {
-            tabs = new ArrayList();
-            innerBodyContent = null;
-            innerBodyWriter = null;
-        }
     }
 
 }
