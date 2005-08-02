0ebfcd89e1cbff654f982d1cd5453eed178889f7
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-08-02 01:28:07 +0000

    compiles now

--- trunk/src/java/com/opensymphony/webwork/views/jsp/ui/ajax/PanelTag.java
+++ trunk/src/java/com/opensymphony/webwork/views/jsp/ui/ajax/PanelTag.java
@@ -1,6 +1,12 @@
 package com.opensymphony.webwork.views.jsp.ui.ajax;
 
+import com.opensymphony.webwork.components.UIBean;
+import com.opensymphony.webwork.components.ajax.Panel;
 import com.opensymphony.webwork.views.jsp.ui.AbstractUITag;
+import com.opensymphony.xwork.util.OgnlValueStack;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * PanelTag
@@ -9,21 +15,19 @@ import com.opensymphony.webwork.views.jsp.ui.AbstractUITag;
  * @author <a href="ian@fdar.com">Ian Roughley</a>
  */
 public class PanelTag extends AbstractUITag {
+    protected String tabName;
 
-    private String tabName;
+    public UIBean getBean(OgnlValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+        return new Panel(stack, req, res);
+    }
 
+    protected void populateParams() {
+        super.populateParams();
 
+        ((Panel) bean).setTabName(tabName);
+    }
 
     public void setTabName(String tabName) {
         this.tabName = findString(tabName);
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getTabName() {
-        return tabName;
-    }
-
 }
