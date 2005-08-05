75e23a50f3489b4a3a180a219131fb4157ce0cd1
roughley <roughley@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-08-05 17:43:34 +0000

    updates to get example8-1 and example 8-2 working

--- src/java/com/opensymphony/webwork/components/ajax/RemotePanel.java
+++ src/java/com/opensymphony/webwork/components/ajax/RemotePanel.java
@@ -1,6 +1,5 @@
 package com.opensymphony.webwork.components.ajax;
 
-import com.opensymphony.webwork.components.ClosingUIBean;
 import com.opensymphony.xwork.util.OgnlValueStack;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -15,7 +14,7 @@ import java.io.Writer;
  * Date: Jul 20, 2005
  * Time: 7:22:54 AM
  */
-public class RemotePanel extends ClosingUIBean implements ContentPane, JavascriptEmitter {
+public class RemotePanel extends RemoteUpdateDiv implements ContentPane, JavascriptEmitter {
     private static final Log LOG = LogFactory.getLog(RemotePanel.class);
 
     public static final String TEMPLATE = "remotetab";
@@ -41,12 +40,21 @@ public class RemotePanel extends ClosingUIBean implements ContentPane, Javascrip
         TabbedPanel tabbedPanel = ((TabbedPanel) findAncestor(TabbedPanel.class));
         subscribeTopicName = tabbedPanel.getTopicName();
         tabbedPanel.addTab(this);
+        ((TopicScope) findAncestor(TopicScope.class)).addEmitter(this);
 
         super.end(writer);
     }
 
-    protected void evaluateExtraParams() {
+    public void evaluateExtraParams() {
         super.evaluateExtraParams();
+
+        if (tabName != null) {
+            addParameter("tabName", findString(tabName));
+        }
+
+        if (subscribeTopicName != null) {
+            addParameter("subscribeTopicName", findString(subscribeTopicName));
+        }
     }
 
     public String getTabName() {
@@ -64,24 +72,37 @@ public class RemotePanel extends ClosingUIBean implements ContentPane, Javascrip
         StringBuffer sb = new StringBuffer();
 
         // create the remote div object
-        sb.append("var remotediv_").append(id);
-        sb.append(" = new RemoteUpdateComponent( '");
-        sb.append(getParameters().get("href")).append("', 'tab_contents_update_").append(id).append("', '");
+//        sb.append("var remotediv_").append(id);
+//        sb.append(" = new RemoteUpdateComponent( '");
+//        sb.append(getParameters().get("href")).append("', 'tab_contents_update_").append(id).append("', '");
         //sb.append(getReloadingText()).append("', '");
-        sb.append(getParameters().get("errorText")).append("', '").append(getParameters().get("showErrorTransportText")).append("' );\n");
+//        sb.append(getParameters().get("errorText")).append("', '").append(getParameters().get("showErrorTransportText")).append("' );\n");
 
-        // subscribe to the tab selection topics
-        sb.append("var tabpanel_").append(id).append(" = new TabContent( \"").append(id).append("\" );\n");
+
+        sb.append("var tabpanelc_").append(id).append(" = new TabContent( \"").append(id).append("\", true );\n");
         sb.append("dojo.event.topic.subscribe( \"").append(subscribeTopicName).append("\"");
-        sb.append(", tabpanel_").append(id).append(", ");
+        sb.append(", tabpanelc_").append(id).append(", ");
         sb.append("\"updateVisibility\"").append(" );");
         sb.append("\n");
 
+        // subscribe to the tab selection topics
+//        sb.append("var tabpanel_").append(id).append(" = document.getElementById( \"tab_contents_update_").append(id).append("\" );\n");
+//        sb.append("dojo.event.topic.subscribe( \"").append(subscribeTopicName).append("\"");
+//        sb.append(", tabpanel_").append(id).append(", ");
+//        sb.append("\"bind\"").append(" );");
+//        sb.append("\n");
+
+//        sb.append("dojo.event.topic.subscribe( \"").append(subscribeTopicName).append("\"");
+//        sb.append(", tab_contents_update_").append(id).append(", ");
+//        sb.append("\"bind\"").append(" );");
+//        sb.append("\n");
+
         // code to update the panel when it is selected
-        sb.append("dojo.event.topic.subscribe( \"").append(subscribeTopicName).append("\"");
-        sb.append(", remotediv_").append(id).append(", ");
-        sb.append("\"tabbedElementIdRefresh\"").append(" );");
-        sb.append("\n");
+//        tab_contents_update_ryh21
+//        sb.append("dojo.event.topic.subscribe( \"").append(subscribeTopicName).append("\"");
+//        sb.append(", remotediv_").append(id).append(", ");
+//        sb.append("\"tabbedElementIdRefresh\"").append(" );");
+//        sb.append("\n");
 
         try {
             writer.write(sb.toString() + "\n");
--- src/java/template/ajax/tabbedpanel-close.ftl
+++ src/java/template/ajax/tabbedpanel-close.ftl
@@ -1,13 +1,13 @@
 <script language="JavaScript" type="text/javascript">
-    var headerDiv = document.getElementById("tab_header_${parameters.id}");
-    var content = "        <ul class='tab_header_main' id='tab_header_main'>\n";
+    var headerDiv_${parameters.id} = document.getElementById("tab_header_${parameters.id}");
+    var content_${parameters.id} = "        <ul class='tab_header_main' id='tab_header_main'>\n";
     <#list parameters.tabs as tab>
-        content = content + "        <li onmouseover='mouseIn(this)' onmouseout='mouseOut(this)' class ='tab_default tab_unselected'  id='tab_header_${tab.id}'>";
-        content = content + "        <a href='#' onclick='dojo.event.topic.publish(\"${parameters.topicName}\", \"${tab.id}\");return false;'>${tab.tabName}</a>";
-        content = content + "        </li>\n";
+        content_${parameters.id} = content_${parameters.id} + "        <li onmouseover='mouseIn(this)' onmouseout='mouseOut(this)' class ='tab_default tab_unselected'  id='tab_header_${tab.id}'>";
+        content_${parameters.id} = content_${parameters.id} + "        <a href='#' onclick='dojo.event.topic.publish(\"${parameters.topicName}\", \"${tab.id}\");return false;'>${tab.tabName}</a>";
+        content_${parameters.id} = content_${parameters.id} + "        </li>\n";
     </#list>
-    content = content + "        </ul>\n";
-    headerDiv.innerHTML = content;
+    content_${parameters.id} = content_${parameters.id} + "        </ul>\n";
+    headerDiv_${parameters.id}.innerHTML = content_${parameters.id};
 </script>
 
 
