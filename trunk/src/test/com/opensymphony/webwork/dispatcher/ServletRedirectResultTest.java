7cc1647b65e717cae734f461aaa82355a59983ad
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-08-09 15:15:05 +0000

    passing test

--- trunk/src/test/com/opensymphony/webwork/dispatcher/ServletRedirectResultTest.java
+++ trunk/src/test/com/opensymphony/webwork/dispatcher/ServletRedirectResultTest.java
@@ -62,6 +62,7 @@ public class ServletRedirectResultTest extends TestCase implements WebWorkStatic
     public void testRelativeRedirect() throws Exception {
         view.setLocation("foo.jsp");
         requestMock.expectAndReturn("getServletPath", "/namespace/some.action");
+        requestMock.expectAndReturn("getAttribute", C.ANY_ARGS, null);
         responseMock.expectAndReturn("encodeRedirectURL", "/context/namespace/foo.jsp", "/context/namespace/foo.jsp");
         responseMock.expect("sendRedirect", C.args(C.eq("/context/namespace/foo.jsp")));
 
