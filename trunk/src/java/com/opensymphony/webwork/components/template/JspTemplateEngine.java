14d43ab4b4df622d4e60a55b7f07da4820fbe833
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-08-11 04:24:10 +0000

    themes fall back now -- should work for VM and FM now

--- trunk/src/java/com/opensymphony/webwork/components/template/JspTemplateEngine.java
+++ trunk/src/java/com/opensymphony/webwork/components/template/JspTemplateEngine.java
@@ -8,6 +8,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.jsp.PageContext;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * JspTemplateEngine
@@ -19,16 +21,41 @@ public class JspTemplateEngine extends BaseTemplateEngine {
     private static final Log LOG = LogFactory.getLog(JspTemplateEngine.class);
 
     public void renderTemplate(TemplateRenderingContext templateContext) throws Exception {
-        String templateName = getFinalTemplateName(templateContext.getTemplate());
+        Template template = templateContext.getTemplate();
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("Rendering template '" + templateName + "'");
+            LOG.debug("Trying to render template " + template + ", repeating through parents until we succeed");
         }
         UIBean tag = templateContext.getTag();
         OgnlValueStack stack = templateContext.getStack();
         stack.push(tag);
         PageContext pageContext = (PageContext) stack.getContext().get(ServletActionContext.PAGE_CONTEXT);
-        IncludeTag.include(templateName, pageContext);
+        List templates = template.getPossibleTemplates(this);
+        Exception exception = null;
+        boolean success = false;
+        for (Iterator iterator = templates.iterator(); iterator.hasNext();) {
+            Template t = (Template) iterator.next();
+            try {
+                IncludeTag.include(getFinalTemplateName(t), pageContext);
+                success = true;
+                break;
+            } catch (Exception e) {
+                if (exception == null) {
+                    exception = e;
+                }
+            }
+        }
+
+        if (!success) {
+            LOG.error("Could not render JSP template " + templateContext.getTemplate());
+
+            if (exception != null) {
+                throw exception;
+            } else {
+                return;
+            }
+        }
+
         stack.pop();
     }
 
