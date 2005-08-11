62fa2ae3af7d16316fb922a279ca6c11ea0d1fee
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-08-11 04:28:51 +0000

    passing tests

--- trunk/src/test/com/opensymphony/webwork/views/TemplateEngineManagerTest.java
+++ trunk/src/test/com/opensymphony/webwork/views/TemplateEngineManagerTest.java
@@ -1,10 +1,7 @@
 package com.opensymphony.webwork.views;
 
+import com.opensymphony.webwork.components.template.*;
 import com.opensymphony.webwork.config.Configuration;
-import com.opensymphony.webwork.components.template.JspTemplateEngine;
-import com.opensymphony.webwork.components.template.TemplateEngine;
-import com.opensymphony.webwork.components.template.TemplateEngineManager;
-import com.opensymphony.webwork.components.template.VelocityTemplateEngine;
 import junit.framework.TestCase;
 
 /**
@@ -27,15 +24,16 @@ public class TemplateEngineManagerTest extends TestCase {
                 return null;
             }
         });
-        TemplateEngine engine = TemplateEngineManager.getTemplateEngine("foo");
+        TemplateEngine engine = TemplateEngineManager.getTemplateEngine(new Template("/template", "simple", "foo"));
         assertTrue(engine instanceof JspTemplateEngine);
-        engine = TemplateEngineManager.getTemplateEngine("foo.vm");
+        engine = TemplateEngineManager.getTemplateEngine(new Template("/template", "simple", "foo.vm"));
         assertTrue(engine instanceof VelocityTemplateEngine);
     }
 
     public void testTemplateTypeUsesDefaultWhenNotSetInConfiguration() {
-        TemplateEngine engine = TemplateEngineManager.getTemplateEngine("foo");
-        TemplateEngine defaultTemplateEngine = TemplateEngineManager.getTemplateEngine(TemplateEngineManager.DEFAULT_TEMPLATE_TYPE);
+        TemplateEngine engine = TemplateEngineManager.getTemplateEngine(new Template("/template", "simple", "foo"));
+        Template template = new Template("/template", "simple", "foo." + TemplateEngineManager.DEFAULT_TEMPLATE_TYPE);
+        TemplateEngine defaultTemplateEngine = TemplateEngineManager.getTemplateEngine(template);
         assertTrue(engine.getClass().equals(defaultTemplateEngine.getClass()));
     }
 
