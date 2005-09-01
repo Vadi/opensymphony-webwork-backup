e3d9a36e7d05b1eee3c05b022e73c30275d555df
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-09-01 05:35:27 +0000

    make junit happy

--- src/test/com/opensymphony/webwork/spring/SessionContextSpringProxyableObjectFactoryTest.java
+++ src/test/com/opensymphony/webwork/spring/SessionContextSpringProxyableObjectFactoryTest.java
@@ -3,5 +3,7 @@ package com.opensymphony.webwork.spring;
 import com.opensymphony.webwork.WebWorkTestCase;
 
 public class SessionContextSpringProxyableObjectFactoryTest extends WebWorkTestCase {
-        
+    public void testFoo() {
+        // TODO: jason, you suck
+    }
 }
