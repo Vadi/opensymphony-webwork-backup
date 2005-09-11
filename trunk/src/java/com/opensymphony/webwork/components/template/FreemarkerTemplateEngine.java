ada6a624b85780e70eb27318e93cb9d438d9b2f1
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-09-11 18:32:19 +0000

    safe way to ensure that BodyContent writers aren't flushed (they should never be). This still allows environments that don't even have JSP to work.

--- trunk/src/java/com/opensymphony/webwork/components/template/FreemarkerTemplateEngine.java
+++ trunk/src/java/com/opensymphony/webwork/components/template/FreemarkerTemplateEngine.java
@@ -9,6 +9,7 @@ import com.opensymphony.webwork.views.freemarker.FreemarkerManager;
 import com.opensymphony.xwork.ActionContext;
 import com.opensymphony.xwork.ActionInvocation;
 import com.opensymphony.xwork.util.OgnlValueStack;
+import com.opensymphony.util.ClassLoaderUtil;
 import freemarker.template.Configuration;
 import freemarker.template.SimpleHash;
 import org.apache.commons.logging.Log;
@@ -18,6 +19,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +28,19 @@ import java.util.Map;
  * @author CameronBraid
  */
 public class FreemarkerTemplateEngine extends BaseTemplateEngine {
+    static Class bodyContent = null;
+
+    static {
+        try {
+            bodyContent = ClassLoaderUtil.loadClass("javax.servlet.jsp.tagext.BodyContent",
+                    FreemarkerTemplateEngine.class);
+        } catch (ClassNotFoundException e) {
+            // this is OK -- this just means JSP isn't even being used here, which is perfectly fine.
+            // we need this class in environments that use JSP to know when to wrap the writer
+            // and ignore flush() calls. In JSP, it is illegal for a BodyContent writer to be flushed(),
+            // so we have to take caution here.
+        }
+    }
 
     private static final Log LOG = LogFactory.getLog(FreemarkerTemplateEngine.class);
 
@@ -83,7 +98,26 @@ public class FreemarkerTemplateEngine extends BaseTemplateEngine {
         model.put("tag", templateContext.getTag());
         model.put("parameters", templateContext.getParameters());
 
-        template.process(model, templateContext.getWriter());
+        // the BodyContent JSP writer doesn't like it when FM flushes automatically --
+        // so let's just not do it (it will be flushed eventually anyway)
+        Writer writer = templateContext.getWriter();
+        if (bodyContent != null && bodyContent.isAssignableFrom(writer.getClass())) {
+            final Writer wrapped = writer;
+            writer = new Writer() {
+                public void write(char cbuf[], int off, int len) throws IOException {
+                    wrapped.write(cbuf, off, len);
+                }
+
+                public void flush() throws IOException {
+                    // nothing!
+                }
+
+                public void close() throws IOException {
+                    wrapped.close();
+                }
+            };
+        }
+        template.process(model, writer);
     }
 
     protected String getSuffix() {
