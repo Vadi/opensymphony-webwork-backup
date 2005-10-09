9900a53a9cd00d0e8f9b964e729097cd5f9e3be6
botah <botah@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-10-09 19:00:40 +0000

    Added support for nested param tags to href. Also fixed a problem w/ href where by the altSyntax false was not getting an action name... so the code have a default == the requested action if none was found in the findString method

--- trunk/src/java/com/opensymphony/webwork/components/Href.java
+++ trunk/src/java/com/opensymphony/webwork/components/Href.java
@@ -3,10 +3,15 @@ package com.opensymphony.webwork.components;
 import com.opensymphony.util.TextUtils;
 import com.opensymphony.webwork.config.Configuration;
 import com.opensymphony.webwork.portlet.context.PortletContext;
+import com.opensymphony.webwork.views.util.UrlHelper;
 import com.opensymphony.xwork.util.OgnlValueStack;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.Writer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * A tag that creates a HTML &gt;a href='' /&lt; that when clicked calls a URL remote XMLHttpRequest call
@@ -18,7 +23,9 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Ian Roughley
  */
-public class Href extends ClosingUIBean {
+public class Href extends UIBean{
+    private static final Log LOG = LogFactory.getLog(Href.class);
+
     final public static String OPEN_TEMPLATE = "a";
     final public static String TEMPLATE = "a-close";
     final public static String COMPONENT_NAME = Href.class.getName();
@@ -43,19 +50,26 @@ public class Href extends ClosingUIBean {
 
     protected void evaluateExtraParams() {
         super.evaluateExtraParams();
+        StringBuffer linkString = new StringBuffer();
 
         //Fix for wwportlet Support -- Added by Henry Hu
         if (href != null) {
             String hrefValue = findString(href);
+
+            //if findString could not evalu the string default to the href
+            //basically this is a default thats better than null pointing in this code
+            if (hrefValue == null) {
+                href = removeStringFromStart(href,"'");
+                hrefValue = removeStringFromEnd(href,"'");
+            }
             String contextPath = request.getContextPath();
-            StringBuffer sb = new StringBuffer();
             String actionUrl = PortletContext.getContext().getActionURL();
 
             if (!TextUtils.stringSet(actionUrl)) {
                 if (hrefValue.startsWith("/") && hrefValue.startsWith(contextPath)) {
                     contextPath = "";
                 }
-                sb.append(contextPath).append(hrefValue);
+                linkString.append(contextPath).append(hrefValue);
             } else {
                 String actionExtension = (String) Configuration.get("webwork.action.extension");
 
@@ -68,13 +82,13 @@ public class Href extends ClosingUIBean {
                 boolean isWebWorkAction = hrefValue.indexOf(actionExtension) >= 0;
 
                 if (isWebWorkAction) {
-                    sb.append(actionUrl).append("?wwXAction=.").append(hrefValue);
+                    linkString.append(actionUrl).append("?wwXAction=.").append(hrefValue);
                 } else {
-                    sb.append(actionUrl).append("?wwLink=").append(hrefValue);
+                    linkString.append(actionUrl).append("?wwLink=").append(hrefValue);
                 }
             }
+            addParameter("href", linkString.toString());
 
-            addParameter("href", sb.toString());
         }
 
         if (showErrorTransportText != null) {
@@ -92,6 +106,68 @@ public class Href extends ClosingUIBean {
         if (afterLoading != null) {
             addParameter("afterLoading", findString(afterLoading));
         }
+
+    }
+
+
+
+    public void end(Writer writer, String body) {
+        StringBuffer linkString = new StringBuffer();
+        linkString.append(href);
+
+        UrlHelper.buildParametersString(getParameters(), linkString);
+
+        href = linkString.toString();
+
+        evaluateParams();
+        addParameter("href", linkString.toString());
+        try {
+            mergeTemplate(writer, buildTemplateName(null, getDefaultOpenTemplate()));
+        } catch (Exception e) {
+            LOG.error("Could not open template", e);
+            e.printStackTrace();
+        }
+
+        super.end(writer, body);
+    }
+
+    /**
+     * If the source string starts with the proided stringToRemove removes it and returns
+     * the string without the leading string.
+     * @param source
+     * @param stringToRemove
+     * @return The string without the leading stringToRemove
+     */
+    private String removeStringFromStart(String source, String stringToRemove) {
+        //gate condition
+        if (source == null || stringToRemove == null){
+            return source;
+        }
+
+        if (source.startsWith(stringToRemove)){
+            return source.substring(1, source.length());
+        }
+        return source;
+    }
+
+
+    /**
+     * if the source string ends with the stringToReve returns the source
+     * string without the tailing stringToRemove. Otherwise returns the source
+     * string
+     * @param source
+     * @param stringToRemove
+     * @return the string without the trailing stringToRemove
+     */
+    private String removeStringFromEnd(String source, String stringToRemove) {
+        //gate condition
+        if (source == null || stringToRemove == null){
+            return source;
+        }
+        if (source.endsWith(stringToRemove)){
+            return stringToRemove.substring(0, source.length() -1);
+        }
+        return source;
     }
 
     public void setHref(String href) {
