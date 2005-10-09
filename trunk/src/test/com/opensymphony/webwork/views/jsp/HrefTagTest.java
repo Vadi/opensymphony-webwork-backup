3446fe1260b744be6ee44b65117c87b4c66e2f05
botah <botah@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-10-09 16:55:44 +0000

    Initial creation. Basic test HREF method

---
+++ trunk/src/test/com/opensymphony/webwork/views/jsp/HrefTagTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.webwork.views.jsp;
+
+import com.opensymphony.webwork.views.jsp.ui.HrefTag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import java.io.StringWriter;
+
+
+/**
+ *
+ * @author Mike Porter
+ * @version $Revision$
+ */
+public class HrefTagTest extends AbstractUITagTest {
+    private StringWriter writer = new StringWriter();
+    private HrefTag tag;
+
+    public void testActionURL() {
+        tag.setHref("TestAction.action");
+        try {
+            tag.doStartTag();
+            tag.doEndTag();
+            assertTrue( writer.toString().indexOf("href=\"TestAction.action\"") > -1);
+        } catch (JspException ex) {
+            ex.printStackTrace();
+            fail();
+        }
+    }
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        request.setScheme("http");
+        request.setServerName("localhost");
+        request.setServerPort(80);
+
+        tag = new HrefTag();
+        tag.setPageContext(pageContext);
+        JspWriter jspWriter = new WebWorkMockJspWriter(writer);
+        pageContext.setJspWriter(jspWriter);
+    }
+
+}
