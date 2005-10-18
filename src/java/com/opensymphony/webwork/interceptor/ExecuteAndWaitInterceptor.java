22767f142afa96ff6697483704b3d81b6722c493
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-10-18 05:17:51 +0000

    more javadocs

--- src/java/com/opensymphony/webwork/interceptor/ExecuteAndWaitInterceptor.java
+++ src/java/com/opensymphony/webwork/interceptor/ExecuteAndWaitInterceptor.java
@@ -18,43 +18,88 @@ import java.util.Map;
 
 /**
  * <!-- START SNIPPET: description -->
- * TODO: Give a description of the Interceptor.
+ *
+ * The ExecuteAndWaitInterceptor is great for running long-lived actions in the background while showing the user a nice
+ * progress meter. This also prevents the HTTP request from timing out when the action takes more than 5 or 10 minutes.
+ *
+ * <p/> Using this interceptor is pretty straight forward. Assuming that you are including webwork-default.xml, this
+ * interceptor is already configured but is not part of any of the default stacks. Because of the nature of this
+ * interceptor, it must be the <b>last</b> interceptor in the stack.
+ *
+ * <p/> This interceptor works on a per-session basis. That means that the same action name (myLongRunningAction, in the
+ * above example) cannot be run more than once at a time in a given session. On the initial request or any subsequent
+ * requests (before the action has completed), the <b>wait</b> result will be returned. <b>The wait result is
+ * responsible for issuing a subsequent request back to the action, giving the effect of a self-updating progress
+ * meter</b>.
+ *
+ * <p/> If no "wait" result is found, WebWork will automatically generate a wait result on the fly. This result is
+ * written in FreeMarker and cannot run unless FreeMarker is installed. If you don't wish to deploy with FreeMarker, you
+ * must provide your own wait result. This is generally a good thing to do anyway, as the default wait page is very
+ * plain.
+ *
+ * <p/>Whenever the wait result is returned, the <b>action that is currently running in the background will be placed on
+ * top of the stack</b>. This allows you to display progress data, such as a count, in the wait page. By making the wait
+ * page automatically reload the request to the action (which will be short-circuited by the interceptor), you can give
+ * the appearance of an automatic progress meter.
+ *
+ * <p/><b>Important</b>: Because the action will be running in a seperate thread, you can't use ActionContext because it
+ * is a ThreadLocal. This means if you need to access, for example, session data, you need to implement SessionAware
+ * rather than calling ActionContext.getSesion().
+ *
+ * <p/>The thread kicked off by this interceptor will be named in the form <b><u>actionName</u>BrackgroundProcess</b>.
+ * For example, the <i>search</i> action would run as a thread named <i>searchBackgroundProcess</i>.
+ *
  * <!-- END SNIPPET: description -->
  *
+ * <p/> <u>Interceptor parameters:</u>
+ *
  * <!-- START SNIPPET: parameters -->
- * TODO: Describe the paramters for this Interceptor.
+ *
+ * <ul>
+ *
+ * <li>threadPriority (optional) - the priority to assign the thread</li>
+ *
+ * </ul>
+ *
  * <!-- END SNIPPET: parameters -->
  *
+ * <p/> <u>Extending the interceptor:</u>
+ *
+ * <p/>
+ *
  * <!-- START SNIPPET: extending -->
- * TODO: Discuss some possible extension of the Interceptor.
+ *
+ * If you wish to make special preparations before and/or after the invocation of the background thread, you can extend
+ * the BackgroundProcess class and implement the beforeInvocation() and afterInvocation() methods. This may be useful
+ * for obtaining and releasing resources that the background process will need to execute successfully. To use your
+ * background process extension, extend ExecuteAndWaitInterceptor and implement the getNewBackgroundProcess() method.
+ *
  * <!-- END SNIPPET: extending -->
+ *
+ * <p/> <u>Example code:</u>
  *
  * <pre>
  * <!-- START SNIPPET: example -->
- * &lt;!-- TODO: Describe how the Interceptor reference will effect execution --&gt;
  * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
- *      TODO: fill in the interceptor reference.
- *     &lt;interceptor-ref name=""/&gt;
- *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
+ *     &lt;interceptor-ref name="completeStack"/&gt;
+ *     &lt;interceptor-ref name="execAndWait"/&gt;
+ *     &lt;result name="success"&gt;longRunningAction-wait.jsp&lt;/result&gt;
+ *     &lt;result name="success"&gt;longRunningAction-success.jsp&lt;/result&gt;
  * &lt;/action&gt;
+ *
+ * &lt;%@ taglib prefix="ww" uri="/webwork" %&gt;
+ * &lt;html&gt;
+ *   &lt;head&gt;
+ *     &lt;title&gt;Please wait&lt;/title&gt;
+ *     &lt;meta http-equiv="refresh" content="5;url=&lt;ww:url includeParams="all" /&gt;"/&gt;
+ *   &lt;/head&gt;
+ *   &lt;body&gt;
+ *     Please wait while we process your request.
+ *     Click &lt;a href="&lt;ww:url includeParams="all" /&gt;">&lt;/a&gt; if this page does not reload automatically.
+ *   &lt;/body&gt;
+ * &lt;/html&gt;
  * <!-- END SNIPPET: example -->
  * </pre>
- * 
- * Executes an action in a background thread and then returns
- * to the "wait" result. On the next request, if the action
- * is done executing, the original result is sent back. This allows
- * long running actions to execute while you present the user with a
- * "Please, wait" message. The wait JSP should reload itself by using
- * the following HTML in the HEAD area:
- * <p/>
- * <p/>
- * <pre>
- *  &lt;meta http-equiv="refresh" content="5;url=&lt;ww:url includeParams="all" /&gt;"/&gt;
- * </pre>
- * <p/>
- * <p/>
- * This will cause the request to be reloaded every 5 seconds. <b>You will need
- * to specify a result of "wait" in your action for this to work.</b>
  *
  * @author <a href="plightbo@gmail.com">Pat Lightbody</a>
  */
