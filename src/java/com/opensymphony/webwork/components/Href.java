6ee5491ead5a1caf43248fa83de2561386863408
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-11-16 16:02:57 +0000

    make the Href tag use the UrlHelper to build URLs (for more advanced URL generation, it should use the URL tag)

--- src/java/com/opensymphony/webwork/components/Href.java
+++ src/java/com/opensymphony/webwork/components/Href.java
@@ -1,8 +1,8 @@
 package com.opensymphony.webwork.components;
 
 import com.opensymphony.util.TextUtils;
-import com.opensymphony.webwork.config.Configuration;
 import com.opensymphony.webwork.portlet.context.PortletContext;
+import com.opensymphony.webwork.views.util.UrlHelper;
 import com.opensymphony.xwork.util.OgnlValueStack;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * A tag that creates a HTML &gt;a href='' /&lt; that when clicked calls a URL remote XMLHttpRequest call
  * via the dojo framework.  The result from the URL is executed as JavaScript.
+ *
  * <p/>
  * If a "listenTopics" is supplied, it will publish a 'click' message to that topic when the result is
  * returned.  If utilizing the topic/event elements, then this tag needs to be contained within
@@ -45,25 +46,16 @@ public class Href extends ClosingUIBean {
     protected void evaluateExtraParams() {
         super.evaluateExtraParams();
 
-        //Fix for wwportlet Support -- Added by Henry Hu
         if (href != null) {
             String hrefValue = findString(href);
-            //if findString could not evalu the string default to the href
-            //basically this is a default thats better than null pointing in this code
-            if (hrefValue == null) {
-                href = removeStringFromStart(href,"'");
-                hrefValue = removeStringFromEnd(href,"'");
-            }
-            String contextPath = request.getContextPath();
-            StringBuffer sb = new StringBuffer();
-            String actionUrl = PortletContext.getContext().getActionURL();
 
-            if (!TextUtils.stringSet(actionUrl)) {
-                if (hrefValue.startsWith("/") && hrefValue.startsWith(contextPath)) {
-                    contextPath = "";
-                }
-                sb.append(contextPath).append(hrefValue);
+            if (!TextUtils.stringSet(PortletContext.getContext().getActionURL())) {
+                addParameter("href", UrlHelper.buildUrl(hrefValue, request, response, null));
             } else {
+                /*
+                TODO: This code was added to help with Portlet suppoort, but it is not correct
+                      Instead, it should consult with ActionMapper
+
                 String actionExtension = (String) Configuration.get("webwork.action.extension");
 
                 if (actionExtension == null || "".equals(actionExtension)) {
@@ -79,9 +71,10 @@ public class Href extends ClosingUIBean {
                 } else {
                     sb.append(actionUrl).append("?wwLink=").append(hrefValue);
                 }
-            }
 
-            addParameter("href", sb.toString());
+                addParameter("href", sb.toString());
+                */
+            }
         }
 
         if (showErrorTransportText != null) {
@@ -102,45 +95,6 @@ public class Href extends ClosingUIBean {
         if (preInvokeJS != null) {
             addParameter("preInvokeJS", findString(preInvokeJS));
         }
-    }
-
-    /**
-     * If the source string starts with the proided stringToRemove removes it and returns
-     * the string without the leading string.
-     * @param source
-     * @param stringToRemove
-     * @return The string without the leading stringToRemove
-     */
-    private String removeStringFromStart(String source, String stringToRemove) {
-        //gate condition
-        if (source == null || stringToRemove == null){
-            return source;
-        }
-
-        if (source.startsWith(stringToRemove)){
-            return source.substring(1, source.length());
-        }
-        return source;
-    }
-
-
-    /**
-     * if the source string ends with the stringToReve returns the source
-     * string without the tailing stringToRemove. Otherwise returns the source
-     * string
-     * @param source
-     * @param stringToRemove
-     * @return the string without the trailing stringToRemove
-     */
-    private String removeStringFromEnd(String source, String stringToRemove) {
-        //gate condition
-        if (source == null || stringToRemove == null){
-            return source;
-        }
-        if (source.endsWith(stringToRemove)){
-            return stringToRemove.substring(0, source.length() -1);
-        }
-        return source;
     }
 
     public void setHref(String href) {
