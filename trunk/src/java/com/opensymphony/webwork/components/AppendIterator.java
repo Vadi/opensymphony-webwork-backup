4912e8fa6b5b3a573ded0df34ea53587de38788f
rgielen <rgielen@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-11-27 20:55:01 +0000

    Issue number: WW-971 Now completed components: - added taglib annotations to components to prepare tld and tagdoc generation - added (sometimes simple) description of all attributes - all javadocs now in proposed wiki aware format, including existing wiki docs where existent and corrected references - small fixes

--- trunk/src/java/com/opensymphony/webwork/components/AppendIterator.java
+++ trunk/src/java/com/opensymphony/webwork/components/AppendIterator.java
@@ -22,10 +22,10 @@ import com.opensymphony.xwork.util.OgnlValueStack;
  * <p>Component for AppendIteratorTag, which jobs is to append iterators to form an
  * appended iterator whereby entries goes from one iterator to another after each
  * respective iterator is exhausted of entries.</p>
- * 
- * <p>For example, if there are 3 iterator appended (each iterator has 3 entries), 
+ *
+ * <p>For example, if there are 3 iterator appended (each iterator has 3 entries),
  * the following will be how the appended iterator entries will be arranged:</p>
- * 
+ *
  * <ol>
  * 		<li>First Entry of the First Iterator</li>
  * 		<li>Second Entry of the First Iterator</li>
@@ -38,49 +38,49 @@ import com.opensymphony.xwork.util.OgnlValueStack;
  *      <li>Third Entry of the Third ITerator</li>
  * </ol>
  * <!-- END SNIPPET: javadoc -->
- * 
+ *
  * <!-- START SNIPPET: params -->
  * <ul>
- * 		<li>id (String) - the id of which if supplied will have the resultant 
+ * 		<li>id (String) - the id of which if supplied will have the resultant
  *                        appended iterator stored under in the stack's context</li>
  * </ul>
  * <!-- END SNIPPET: params -->
- * 
- * 
+ *
+ *
  * <!-- START SNIPPET: code -->
  * public class AppendIteratorTagAction extends ActionSupport {
  *
  *	private List myList1;
  *	private List myList2;
  *	private List myList3;
- *	
- *	
+ *
+ *
  *	public String execute() throws Exception {
- *		
+ *
  *		myList1 = new ArrayList();
  *		myList1.add("1");
  *		myList1.add("2");
  *		myList1.add("3");
- *		
+ *
  *		myList2 = new ArrayList();
  *		myList2.add("a");
  *		myList2.add("b");
  *		myList2.add("c");
- *		
+ *
  *		myList3 = new ArrayList();
  *		myList3.add("A");
  *		myList3.add("B");
  *		myList3.add("C");
- *		
+ *
  *		return "done";
  *	}
- *	
+ *
  *	public List getMyList1() { return myList1; }
  *	public List getMyList2() { return myList2; }
  *	public List getMyList3() { return myList3; }
  *}
  * <!-- END SNIPPET: code -->
- * 
+ *
  * <!-- START SNIPPET: example -->
  * &lt;ww:append id="myAppendIterator"&gt;
  *		&lt;ww:param value="%{myList1}" /&gt;
@@ -91,12 +91,15 @@ import com.opensymphony.xwork.util.OgnlValueStack;
  *		&lt;ww:property /&gt;
  * &lt;/ww:iterator&gt;
  * <!-- END SNIPPET: example -->
- * 
- * 
+ *
+ *
  * @author tm_jee ( tm_jee (at) yahoo.co.uk )
  * @version $Date$ $Id$
  * @see com.opensymphony.webwork.util.AppendIteratorFilter
  * @see com.opensymphony.webwork.views.jsp.iterator.AppendIteratorTag
+ *
+ * @jsp.tag name="append" body-content="JSP"
+ * description="Append the values of a list of iterators to one iterator"
  */
 public class AppendIterator extends Component implements UnnamedParametric {
 
