a2353279fe8eebaf66f5a46a249f5672da94beb7
rgielen <rgielen@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-11-27 13:30:47 +0000

    Issue number: WW-971 First cut, more to come: - added taglib annotations to components to prepare tld and tagdoc generation - added (sometimes simple) description of all attributes - all javadocs now in proposed wiki aware format, including existing wiki docs where existent and corrected references

--- src/java/com/opensymphony/webwork/components/Param.java
+++ src/java/com/opensymphony/webwork/components/Param.java
@@ -5,27 +5,43 @@ import com.opensymphony.xwork.util.OgnlValueStack;
 import java.io.Writer;
 
 /**
+ * <!-- START SNIPPET: javadoc -->
  * This tag can be used to parameterize other tags, who implement
- * the ParametricTag interface declared here.
- * <p/>
- * The IncludeTag and BeanTag are examples of such tags.
- * <p/>
+ * the ParametricTag interface declared here.<p/>
+ *
+ * The include tag and bean tag are examples of such tags.<p/>
+ *
  * The inner classes, Parametric and UnnamedParametric, are implemented by tags. They indicate that a
- * particular Tag may have embedded params.  For example, if we were wanted to use the ComponentTag
- * and wanted to provide custom params to assist with the rendering, we could declare something like
- * <p/>
- * <pre>
+ * particular Tag may have embedded params. For example, if we were wanted to use the ComponentTag
+ * and wanted to provide custom params to assist with the rendering, we could declare something like<p/>
+ * <!-- END SNIPPET: javadoc -->
+ *
+ * <p/> <b>Examples</b>
+ * <!-- START SNIPPET: example -->
  * &lt;ui:component&gt;
  *  &lt;ui:param name="key"     value="[0]"/&gt;
  *  &lt;ui:param name="value"   value="[1]"/&gt;
  *  &lt;ui:param name="context" value="[2]"/&gt;
  * &lt;/ui:component&gt;
+ * <!-- END SNIPPET: example -->
  * </pre>
  * <p/>
+ * <!-- START SNIPPET: exampledescription -->
  * where the key will be the identifier and the value the result of an OGNL expression run against the current
- * OgnlValueStack
+ * OgnlValueStack.<p/>
+ * <!-- END SNIPPET: exampledescription -->
+ * <pre>
  *
  * @author Rickard Öberg (rickard@dreambean.com)
+ * @author Rene Gielen
+ * @version $Revision$
+ * @since 2.2
+ *
+ * @see Include
+ * @see Bean
+ *
+ * @jsp.tag name="param" body-content="JSP"
+ * description="Parametrize other tags"
  */
 public class Param extends Component {
     protected String name;
@@ -65,6 +81,10 @@ public class Param extends Component {
         return true;
     }
 
+    /**
+     * @jsp.attribute required="false"  rtexprvalue="true"
+     * description="Name of Parameter to set"
+     */
     public void setName(String name) {
         this.name = name;
     }
@@ -73,6 +93,10 @@ public class Param extends Component {
         this.value = value;
     }
 
+    /**
+     * @jsp.attribute required="false"  rtexprvalue="true"
+     * description="Value expression for Parameter to set"
+     */
     public interface UnnamedParametric {
         public void addParameter(Object value);
     }
