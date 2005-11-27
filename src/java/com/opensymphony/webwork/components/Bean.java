d1f8033cb472350f92d3bf83f0387252d52f2e43
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-11-27 12:58:02 +0000

    added SNIPPETS to javadoc

--- src/java/com/opensymphony/webwork/components/Bean.java
+++ src/java/com/opensymphony/webwork/components/Bean.java
@@ -10,44 +10,67 @@ import org.apache.commons.logging.LogFactory;
 import java.io.Writer;
 
 /**
+ * <!-- START SNIPPET: javadoc -->
  * <p>Instantiates a class that conforms to the JavaBeans specification. This tag has a body which can contain
  * a number of {@link Param} elements to set any mutator methods on that class.</p>
  * <p/>
  * <p>If the id attribute is set on the BeanTag, it will place the instantiated bean into the PageContext and the
  * ActionContext.</p>
  * <p/>
+ * <!-- END SNIPPET: javadoc -->
+ * 
+ * 
+ * <!-- START SNIPPET: params -->
+ * <ul>
+ * 		<li>id - the stack's context id (if supplied) that the created bean will be store under</li>
+ * 		<li>name (*) - the class name of the bean to be instantiated (must respect JavaBean specification)</li>
+ * </ul>
+ * <!-- END SNIPPET: params -->
+ * 
+ * 
  * <p>Examples:</p>
  * <p/>
  * <pre>
+ * <!-- START SNIPPET: examples -->
+ * &lt;-- in freemarker form --&gt;
  * [ww.bean name="com.opensymphony.webwork.example.counter.SimpleCounter" id="counter"]
  *   [ww:param name="foo" value="BAR"/]
  *   The value of foo is : [ww:property value="foo"/], when inside the bean tag.<br />
  * [/ww:bean]
+ * 
+ * &lt;-- in jsp form --&gt;
+ * &lt;ww:bean name="com.opensymphony.webwork.example.counter.SimpleCounter" id="counter"&gt;
+ * 	 &lt;ww:param name="foo" value="BAR" /&gt;
+ *   The value of foot is : &lt;ww:property value="foo"/&gt;, when inside the bean tag &lt;br /&gt;
+ * &lt;/ww:bean&gt;
+ * <!-- END SNIPPET: examples -->
  * </pre>
  * <p/>
+ * 
+ * <!-- START SNIPPET: examplesdescription -->
  * <p>This example instantiates a bean called SimpleCounter and sets the foo property (setFoo('BAR')). The
  * SimpleCounter object is then pushed onto the Valuestack, which means that we can called its accessor methods (getFoo())
  * with the Property tag and get their values.</p>
  * <p/>
  * <p>In the above example, the id has been set to a value of <i>counter</i>. This means that the SimpleCounter class
- * will be placed into the PageContext and ActionContext. You can access the SimpleCounter class using JSTL:</p>
+ * will be placed into the stack's context. You can access the SimpleCounter class using WW's tag:</p>
  * <p/>
  * <pre>
- * [c:out value="${counter.foo}"/]
- * </pre>
- * <p/>
- * <p> or using the webwork Property tag:</p>
- * <p/>
- * <pre>
+ * &lt;-- jsp form --&gt;
+ * &lt;ww:property value="#counter" /&gt;
+ * 
+ * &lt;-- freemarker form --&gt;
  * [ww:property value="#counter.foo"/]
  * </pre>
  * <p/>
  * <p>In the property tag example, the <i>#</i> tells Ognl to search the context for the SimpleCounter class which has
  * an id(key) of <i>counter</i></p>
+ * <!-- END SNIPPET: examplesdescription -->
  *
  * @author $author$
  * @author Rick Salsa (rsal@mb.sympatico.ca)
  * @author Brock Bulger
+ * @author tm_jee ( tm_jee (at) yahoo.co.uk )
  * @version $Revision$
  */
 public class Bean extends Component {
