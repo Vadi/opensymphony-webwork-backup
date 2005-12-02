2cd5e156ce0c7de19afb63fa7725285f83f84bdb
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-12-02 18:01:17 +0000

    update javadoc (use snippets, use altSyntax)

--- src/java/com/opensymphony/webwork/components/Submit.java
+++ src/java/com/opensymphony/webwork/components/Submit.java
@@ -7,15 +7,116 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * <!-- START SNIPPET: javadoc -->
- * Render a submit button.</p>
+ * Render a submit button. The submit tag is used together with the form tag to provide asynchronous form submissions.
  * <!-- END SNIPPET: javadoc -->
  *
  * <p/> <b>Examples</b>
  *
  * <pre>
  * <!-- START SNIPPET: example -->
- * &lt;ww:submit value="'Submit'" /&gt;
+ * &lt;ww:submit value="%{'Submit'}" /&gt;
  * <!-- END SNIPPET: example -->
+ * </pre>
+ * 
+ * 
+ * <!-- START SNIPPET: ajaxJavadoc -->
+ * <B>THE FOLLOWING IS ONLY VALID WHEN AJAX IS CONFIGURED</B>
+ * <ul>
+ * 		<li>resultDivId</li>
+ * 		<li>notifyTopics</li>
+ * 		<li>onLoadJS</li>
+ * 		<li>preInvokeJS</li>
+ * </ul>
+ * The remote form has three basic modes of use, using the resultDivId, 
+ * the notifyTopics, or the onLoadJS. You can mix and match any combination of 
+ * them to get your desired result. All of these examples are contained in the 
+ * Ajax example webapp. Lets go through some scenarios to see how you might use it:
+ * <!-- END SNIPPET: ajaxJavadoc -->
+ * 
+ * <!-- START SNIPPET: ajxExDescription1 -->
+ * Show the results in another div. If you want your results to be shown in 
+ * a div, use the resultDivId where the id is the id of the div you want them 
+ * shown in. This is an inner HTML approah. Your results get jammed into 
+ * the div for you. Here is a sample of this approach:
+ * <!-- END SNIPPET: ajxExDescription1 -->
+ * 
+ * <pre>
+ * <!-- START SNIPPET: ajxExample1 -->
+ * Remote form replacing another div:
+ * &lt;div id='two' style="border: 1px solid yellow;"&gt;Initial content&lt;/div&gt;
+ * &lt;ww:form
+ *       id='theForm2'
+ *       cssStyle="border: 1px solid green;"
+ *       action='/AjaxRemoteForm.action'
+ *       method='post'
+ *       theme="ajax"&gt;
+ *
+ *   &lt;input type='text' name='data' value='WebWork User' /&gt;
+ *   &lt;ww:submit value="GO2" theme="ajax" resultDivId="two" /&gt;
+ *
+ * &lt;/ww:form &gt;
+ * <!-- END SNIPPET: ajxExample1 -->
+ * </pre>
+ * 
+ * 
+ * <!-- START SNIPPET: ajxExDescription2 -->
+ * Notify other controls(divs) of a change. Using an pub-sub model you can 
+ * notify others that your control changed and they can take the appropriate action. 
+ * Most likely they will execute some action to refresh. The notifyTopics does this 
+ * for you. You can have many topic names in a comma delimited list. 
+ * eg: notifyTopics="newPerson, dataChanged" .
+ * Here is an example of this approach:
+ * <!-- END SNIPPET: ajxExDescription2 -->
+ * 
+ * <pre>
+ * <!-- START SNIPPET: ajxExample2 -->
+ * &lt;ww:form id="frm1" action="newPersonWithXMLResult" theme="ajax"  &gt;
+ *     &lt;ww:textfield label="Name" name="person.name" value="person.name" size="20" required="true" /&gt;
+ *     &lt;ww:submit id="submitBtn" value="Save" theme="ajax"  cssClass="primary"  notifyTopics="personUpdated, systemWorking" /&gt;
+ * &lt;/ww:form &gt;
+ * 
+ * &lt;ww:div href="/listPeople.action" theme="ajax" errorText="error opps"
+ *         loadingText="loading..." id="cart-body" &gt;
+ *     &lt;ww:action namespace="" name="listPeople" executeResult="true" /&gt;
+ * &lt;/ww:div&gt;
+ * <!-- END SNIPPET: ajxExample2 -->
+ * </pre>
+ *
+ * <!-- START SNIPPET: ajxExDescription3 -->
+ * Massage the results with JavaScript. Say that your result returns some h
+ * appy XML and you want to parse it and do lots of cool things with it. 
+ * The way to do this is with a onLoadJS handler. Here you provide the name of
+ * a JavaScript function to be called back with the result and the event type.
+ * The only key is that you must use the variable names 'data' and 'type' when 
+ * defining the callback. For example: onLoadJS="myFancyDancyFunction(data, type)".
+ * While I talked about XML in this example, your not limited to XML, the data in 
+ * the callback will be exactly whats returned as your result.
+ * Here is an example of this approach:
+ * <!-- END SNIPPET: ajxExDescription3 -->
+ *
+ * <pre>
+ * <!-- START SNIPPET: ajxExample3 -->
+ * &lt;script language="JavaScript" type="text/javascript"&gt;
+ *     function doGreatThings(data, type) {
+ *         //Do whatever with your returned fragment... 
+ *         //Perhapps.... if xml...
+ *               var xml = dojo.xml.domUtil.createDocumentFromText(data);
+ *               var people = xml.getElementsByTagName("person");
+ *               for(var i = 0;i < people.length; i ++){
+ *                   var person = people[i];
+ *                   var name = person.getAttribute("name")
+ *                   var id = person.getAttribute("id")
+ *                   alert('Thanks dude. Person: ' + name + ' saved great!!!');
+ *               }
+ *
+ *     }
+ * &lt;/script&gt;
+ *
+ * &lt;ww:form id="frm1" action="newPersonWithXMLResult" theme="ajax"  &gt;
+ *     &lt;ww:textfield label="'Name'" name="'person.name'" value="person.name" size="20" required="true" /&gt;
+ *     &lt;ww:submit id="submitBtn" value="Save" theme="ajax"  cssClass="primary"  onLoadJS="doGreatThings(data, type)" /&gt; 
+ * &lt;/ww:form&gt;
+ * <!-- END SNIPPET: ajxExample3 -->
  * </pre>
  *
  * @author Patrick Lightbody
@@ -122,7 +223,7 @@ public class Submit extends UIBean {
 
     /**
      * @ww.tagattribute required="false"
-     * description="Set resultDivId attribute"
+     * description="The id of the HTML element to place the result (this can the the form's id or any id on the page"
      */
     public void setResultDivId(String resultDivId) {
         this.resultDivId = resultDivId;
@@ -130,7 +231,7 @@ public class Submit extends UIBean {
 
     /**
      * @ww.tagattribute required="false"
-     * description="Set onLoadJS attribute"
+     * description="Javascript code that will be executed after the form has been submitted. The format is onLoadJS='yourMethodName(data,type)'. NOTE: the words data and type must be left like that if you want the event type and the returned data."
      */
     public void setOnLoadJS(String onLoadJS) {
         this.onLoadJS = onLoadJS;
@@ -138,7 +239,7 @@ public class Submit extends UIBean {
 
     /**
      * @ww.tagattribute required="false"
-     * description="Set notifyTopics attribute"
+     * description=" Topic names to post an event to after the form has been submitted"
      */
     public void setNotifyTopics(String notifyTopics) {
         this.notifyTopics = notifyTopics;
