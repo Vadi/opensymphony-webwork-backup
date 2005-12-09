fe4db51ed95bdcf098509b86e6475e16e2e7036e
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-12-09 19:31:49 +0000

    include should have parameters as a Map of String->List

--- src/java/com/opensymphony/webwork/components/Include.java
+++ src/java/com/opensymphony/webwork/components/Include.java
@@ -193,6 +193,22 @@ public class Include extends Component {
         return returnValue;
     }
 
+    public void addParameter(String key, Object value) {
+        // don't use the default implementation of addParameter,
+        // instead, include tag requires that each parameter be a list of objects,
+        // just like the HTTP servlet interfaces are (String[]) 
+        if (value != null) {
+            List currentValues = (List) parameters.get(key);
+
+            if (currentValues == null) {
+                currentValues = new ArrayList();
+                parameters.put(key, currentValues);
+            }
+
+            currentValues.add(value);
+        }
+    }
+
     public static void include(String aResult, Writer writer, ServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String resourcePath = getContextRelativePath(request, aResult);
         RequestDispatcher rd = request.getRequestDispatcher(resourcePath);
