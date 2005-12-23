d5e57ef6b335e1870b0496810c3ae81439cea4a6
rgielen <rgielen@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-12-23 10:58:43 +0000

    Issue number: WW-1045 calendarcss, including documentation

--- src/java/com/opensymphony/webwork/components/Head.java
+++ src/java/com/opensymphony/webwork/components/Head.java
@@ -9,19 +9,39 @@ import javax.servlet.http.HttpServletResponse;
  * <!-- START SNIPPET: javadoc -->
  *
  * Renders parts of the HEAD section for an HTML file. This is useful as some themes require certain CSS and JavaScript
- * includes.
+ * includes.<p/>
+ *
+ * If, for example, your page has ajax components integrated, without having the default theme set to ajax, you might
+ * want to use the head tag with <pre>theme="ajax"</pre> so that the typical ajax header setup will be included in the
+ * page.<p/>
+ *
+ * The tag also includes the option to set a custom datepicker theme if needed. See calendarcss parameter for
+ * description for details.<p/>
  *
  * <!-- END SNIPPET: javadoc -->
  *
  * <p/> <b>Examples</b>
  *
  * <pre>
- * <!-- START SNIPPET: example -->
- * &lt;ww:head/&gt;
- * <!-- END SNIPPET: example -->
+ * <!-- START SNIPPET: example1 -->
+ * &lt;head&gt;
+ *   &lt;title&gt;My page&lt;/title&gt;
+ *   &lt;ww:head/&gt;
+ * &lt;/head&gt;
+ * <!-- END SNIPPET: example1 -->
+ * </pre>
+ *
+ * <pre>
+ * <!-- START SNIPPET: example2 -->
+ * &lt;head&gt;
+ *   &lt;title&gt;My page&lt;/title&gt;
+ *   &lt;ww:head theme="ajax" calendarcss="calendar-green"/&gt;
+ * &lt;/head&gt;
+ * <!-- END SNIPPET: example2 -->
  * </pre>
  *
  * @author Patrick Lightbody
+ * @author Rene Gielen
  * @ww.tag name="head" tld-body-content="empty" tld-tag-class="com.opensymphony.webwork.views.jsp.ui.HeadTag"
  * description="Render a chunk of HEAD for your HTML file"
  * @since 2.2
@@ -30,7 +50,7 @@ public class Head extends UIBean {
     final public static String TEMPLATE = "head";
 
     private String calendarcss = "calendar-blue.css";
-    
+
     public Head(OgnlValueStack stack, HttpServletRequest request, HttpServletResponse response) {
         super(stack, request, response);
     }
@@ -44,9 +64,9 @@ public class Head extends UIBean {
 
         if (calendarcss != null) {
             String css = findString(calendarcss);
-            if (css != null && css.trim().length()>0) {
-            	if (css.lastIndexOf(".css")<0) {
-                    addParameter("calendarcss", css+".css");
+            if (css != null && css.trim().length() > 0) {
+                if (css.lastIndexOf(".css") < 0) {
+                    addParameter("calendarcss", css + ".css");
                 } else {
                     addParameter("calendarcss", css);
                 }
@@ -59,9 +79,8 @@ public class Head extends UIBean {
     }
 
     /**
-     * @ww.tagattribute required="false"
-     * description="The jscalendar css theme to use" default="calendar-blue.css"
-      */
+     * @ww.tagattribute required="false" description="The jscalendar css theme to use" default="calendar-blue.css"
+     */
     public void setCalendarcss(String calendarcss) {
         this.calendarcss = calendarcss;
     }
