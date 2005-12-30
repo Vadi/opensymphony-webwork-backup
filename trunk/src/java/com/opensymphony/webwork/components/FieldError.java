0c336af06a3d2f2bc1d06d737e4afb9051620b39
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2005-12-30 22:36:11 +0000

    WW-1038: velocity and FM support for new tags

--- trunk/src/java/com/opensymphony/webwork/components/FieldError.java
+++ trunk/src/java/com/opensymphony/webwork/components/FieldError.java
@@ -4,59 +4,53 @@
  */
 package com.opensymphony.webwork.components;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.opensymphony.webwork.components.Param.UnnamedParametric;
 import com.opensymphony.xwork.util.OgnlValueStack;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <!-- START SNIPPET: javadoc -->
- * 
+ *
  * Create a list ( &lt;ul&gt; and &lt;li&gt; ) of action messages if they exists.
- * 
+ *
  * <!-- END SNIPPET: javadoc -->
- * 
+ *
  * <p/> <b>Examples</b>
- * 
+ *
  * <pre>
  * <!-- START SNIPPET: example -->
- * 
  *    &lt;!-- example 1 --&gt;
  *    &lt;ww:fielderror /&gt;
- *    
+ *
  *    &lt;!-- example 2 --&gt;
  *    &lt;ww:fielderror&gt;
  *         &lt;ww:param&gt;field1&lt;/ww:param&gt;
  *         &lt;ww:param&gt;field2&lt;/ww:param&gt;
  *    &lt;/ww:fielderror&gt;
- *    
+ *
  *    OR
- *    
+ *
  *    &lt;ww:fielderror&gt;
  *    		&lt;ww:param value="%{'field1'}" /&gt;
  *    		&lt;ww:param value="%{'field2'}" /&gt;
  *    &lt;/ww:fielderror&gt;
- *    
- * 
  * <!-- END SNIPPET: example -->
  * </pre>
- * 
- * 
+ *
+ *
  * <p/> <b>Description</b><p/>
- * 
- * 
+ *
+ *
  * <!-- START SNIPPET: description -->
- * 
- * Example 1: display all field errors<p/>
- * Example 2: display field errors only for 'field1' and 'field2'<p/>
- * 
+ *
+ * Example 1: display all field errors<p/> Example 2: display field errors only for 'field1' and 'field2'<p/>
+ *
  * <!-- END SNIPPET: description -->
- * 
- * 
+ *
  * @author tm_jee
  * @version $Date$ $Id$
  * @ww.tag name="fielderror" tld-body-content="JSP" tld-tag-class="com.opensymphony.webwork.views.jsp.ui.FieldErrorTag"
@@ -64,26 +58,26 @@ import com.opensymphony.xwork.util.OgnlValueStack;
  */
 public class FieldError extends UIBean implements UnnamedParametric {
 
-	private List errorFieldNames = new ArrayList();
-	
-	public FieldError(OgnlValueStack stack, HttpServletRequest request, HttpServletResponse response) {
-		super(stack, request, response);
-	}
+    private List errorFieldNames = new ArrayList();
 
-	private static final String TEMPLATE = "fielderror";
-	
-	protected String getDefaultTemplate() {
-		return TEMPLATE;
-	}
+    public FieldError(OgnlValueStack stack, HttpServletRequest request, HttpServletResponse response) {
+        super(stack, request, response);
+    }
 
-	public void addParameter(Object value) {
-		if (value != null) {
-			errorFieldNames.add(value.toString());
-		}
-	}
-	
-	public List getFieldErrorFieldNames() {
-		return errorFieldNames;
-	}
+    private static final String TEMPLATE = "fielderror";
+
+    protected String getDefaultTemplate() {
+        return TEMPLATE;
+    }
+
+    public void addParameter(Object value) {
+        if (value != null) {
+            errorFieldNames.add(value.toString());
+        }
+    }
+
+    public List getFieldErrorFieldNames() {
+        return errorFieldNames;
+    }
 }
 
