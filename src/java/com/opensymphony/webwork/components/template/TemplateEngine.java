083fdc7986f62eb304b0958f7880aa1a15ebafac
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-01-09 01:01:45 +0000

    javadoc tweaks

--- src/java/com/opensymphony/webwork/components/template/TemplateEngine.java
+++ src/java/com/opensymphony/webwork/components/template/TemplateEngine.java
@@ -6,7 +6,6 @@ import java.util.Map;
 /**
  * Any template language which wants to support UI tag templating needs to provide an implementation of this interface
  * to handle rendering the template
- * Date: Sep 28, 2004 11:56:34 AM
  *
  * @author jcarreira
  */
