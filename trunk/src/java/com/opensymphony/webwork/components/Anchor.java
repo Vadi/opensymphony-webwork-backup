11370c2204bd3ae369d852832ae809e05da78aa2
plightbo <plightbo@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-01-10 03:49:02 +0000

    more prep for release

--- trunk/src/java/com/opensymphony/webwork/components/Anchor.java
+++ trunk/src/java/com/opensymphony/webwork/components/Anchor.java
@@ -7,12 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * <!-- START SNIPPET: javadoc -->
- * A tag that creates a HTML &lt;a href='' /&gt; that when clicked calls a URL remote XMLHttpRequest call
- * via the dojo framework. The result from the URL is executed as JavaScript.<p/>
  *
- * If a "listenTopics" is supplied, it will publish a 'click' message to that topic when the result is
- * returned.  If utilizing the topic/event elements, then this tag needs to be contained within
- * a &gt;ww:topicScope /&lt; tag.<p/>
+ * A tag that creates a HTML &lt;a href='' /&gt; that when clicked calls a URL remote XMLHttpRequest call via the dojo
+ * framework. The result from the URL is executed as JavaScript. If a "listenTopics" is supplied, it will publish a
+ * 'click' message to that topic when the result is returned.
+ *
  * <!-- END SNIPPET: javadoc -->
  *
  * <p/> <b>Examples</b>
@@ -25,21 +24,32 @@ import javax.servlet.http.HttpServletResponse;
  * &lt;/ww:a&gt;
  * <!-- END SNIPPET: example1 -->
  * </pre>
+ *
  * </p>
+ *
  * <!-- START SNIPPET: exampledescription1 -->
+ *
  * Results in
+ *
  * <!-- END SNIPPET: exampledescription1 -->
+ *
  * </p>
  *
  * <pre>
  * <!-- START SNIPPET: example2 -->
- * &lt;a dojoType="BindAnchor" evalResult="true" id="link1" href="/DoIt.action?id=1" errorHtml="An error ocurred" showTransportError="true"&gt;&lt;/a&gt;
+ * &lt;a dojoType="BindAnchor" evalResult="true" id="link1" href="/DoIt.action?id=1" errorHtml="An error ocurred"
+ * showTransportError="true"&gt;&lt;/a&gt;
  * <!-- END SNIPPET: example2 -->
  * </pre>
+ *
  * </p>
+ *
  * <!-- START SNIPPET: exampledescription2 -->
+ *
  * Here is an example that uses the postInvokeJS. This example is in altSyntax=true:
+ *
  * <!-- END SNIPPET: exampledescription2 -->
+ *
  * </p>
  *
  * <pre>
@@ -53,11 +63,10 @@ import javax.servlet.http.HttpServletResponse;
  * @author Ian Roughley
  * @author Rene Gielen
  * @version $Revision$
- * @since 2.2
- *
  * @ww.tag name="a" tld-body-content="JSP" tld-tag-class="com.opensymphony.webwork.views.jsp.ui.AnchorTag"
  * description="Render a HTML href element that when clicked calls a URL via remote XMLHttpRequest"
-  */
+ * @since 2.2
+ */
 public class Anchor extends RemoteCallUIBean {
     final public static String OPEN_TEMPLATE = "a";
     final public static String TEMPLATE = "a-close";
@@ -95,24 +104,24 @@ public class Anchor extends RemoteCallUIBean {
     }
 
     /**
-     * @ww.tagattribute required="false" type="String"
-     * description="The id to assign the component"
+     * @ww.tagattribute required="false" type="String" description="The id to assign the component"
      */
     public void setId(String id) {
         super.setId(id);
     }
 
     /**
-     * @ww.tagattribute required="false"
-     * description="Topic names to post an event to after the remote call has been made"
+     * @ww.tagattribute required="false" description="Topic names to post an event to after the remote call has been
+     * made"
      */
     public void setNotifyTopics(String notifyTopics) {
         this.notifyTopics = notifyTopics;
     }
 
     /**
-     * @ww.tagattribute required="false" type="String"
-     * description="A javascript snippet that will be invoked prior to the execution of the target href. If provided must return true or false. True indicates to continue executing target, false says do not execute link target. Possible uses are for confirm dialogs."
+     * @ww.tagattribute required="false" type="String" description="A javascript snippet that will be invoked prior to
+     * the execution of the target href. If provided must return true or false. True indicates to continue executing
+     * target, false says do not execute link target. Possible uses are for confirm dialogs."
      */
     public void setPreInvokeJS(String preInvokeJS) {
         this.preInvokeJS = preInvokeJS;
