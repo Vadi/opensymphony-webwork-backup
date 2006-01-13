79637c4072e8e2184e548ee9396bdf5ce89bc6a2
rainerh <rainerh@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-01-13 10:56:23 +0000

    JSTL support not functioning correctly Issue number:  WW-1080 Submitted by:  Mathias Bogaert Reviewed by:   Rainer Hermanns

--- trunk/src/java/com/opensymphony/webwork/dispatcher/WebWorkRequestWrapper.java
+++ trunk/src/java/com/opensymphony/webwork/dispatcher/WebWorkRequestWrapper.java
@@ -33,7 +33,7 @@ public class WebWorkRequestWrapper extends HttpServletRequestWrapper {
 
         Object attribute = super.getAttribute(s);
 
-        boolean alreadyIn = true;
+        boolean alreadyIn = false;
         Boolean b = (Boolean) ActionContext.getContext().get("__requestWrapper.getAttribute");
         if (b != null) {
             alreadyIn = b.booleanValue();
@@ -41,7 +41,7 @@ public class WebWorkRequestWrapper extends HttpServletRequestWrapper {
 
         // note: we don't let # come through or else a request for
         // #attr.foo or #request.foo could cause an endless loop
-        if (alreadyIn && attribute == null && s.indexOf("#") == -1) {
+        if (!alreadyIn && attribute == null && s.indexOf("#") == -1) {
             try {
                 // If not found, then try the ValueStack
                 ActionContext.getContext().put("__requestWrapper.getAttribute", Boolean.TRUE);
