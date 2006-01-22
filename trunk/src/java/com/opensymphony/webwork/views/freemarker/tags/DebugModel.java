d59127ed287c2918a30db427f1d9cbc03b1d80ae
rainerh <rainerh@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-01-22 15:03:17 +0000

    Debug Tag not working with FTL Issue number:  WW-1110 Obtained from: Submitted by:  Eugene Schava

---
+++ trunk/src/java/com/opensymphony/webwork/views/freemarker/tags/DebugModel.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2005 Opensymphony. All Rights Reserved.
+ */
+package com.opensymphony.webwork.views.freemarker.tags;
+
+import com.opensymphony.xwork.util.OgnlValueStack;
+import com.opensymphony.webwork.components.Component;
+import com.opensymphony.webwork.views.jsp.ui.Debug;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @see Debug
+ *
+ * @author Rainer Hermanns
+ * @version $Id$
+ */
+public class DebugModel extends TagModel {
+
+    public DebugModel(OgnlValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+        super(stack, req, res);
+    }
+
+    protected Component getBean() {
+        return new Debug(stack, req, res);
+    }
+
+}
