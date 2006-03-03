bc08f112cbde0834edd982200e3753dd1875db74
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-03 14:38:13 +0000

    added comment to the code
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- webapps/showcase/src/java/com/opensymphony/webwork/showcase/fileupload/FileUploadAction.java
+++ webapps/showcase/src/java/com/opensymphony/webwork/showcase/fileupload/FileUploadAction.java
@@ -1,15 +1,16 @@
 /*
- * Copyright (c) 2006, Your Corporation. All Rights Reserved.
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
  */
-
 package com.opensymphony.webwork.showcase.fileupload;
 
 import java.io.File;
 
 /**
- * <code>FileUploadAction</code>
+ * Show case File Upload example's action. <code>FileUploadAction</code>
  *
  * @author <a href="mailto:hermanns@aixcept.de">Rainer Hermanns</a>
+ * @author tm_jee
  * @version $Id$
  */
 public class FileUploadAction {
@@ -20,7 +21,8 @@ public class FileUploadAction {
     private String fileName;
     private String caption;
     
-    
+    // since we are using <ww:file name="upload" .../> the file name will be
+    // obtained through getter/setter of <file-tag-name>FileName
     public String getUploadFileName() {
     	return fileName;
     }
@@ -28,26 +30,30 @@ public class FileUploadAction {
     	this.fileName = fileName;
     }
 
+    
+    // since we are using <ww:file name="upload" ... /> the content type will be
+    // obtained through getter/setter of <file-tag-name>ContentType
     public String getUploadContentType() {
         return contentType;
     }
-
     public void setUploadContentType(String contentType) {
         this.contentType = contentType;
     }
 
+    
+    // since we are using <ww:file name="upload" ... /> the File itself will be
+    // obtained through getter/setter of <file-tag-name>
     public File getUpload() {
         return upload;
     }
-
     public void setUpload(File upload) {
         this.upload = upload;
     }
 
+    
     public String getCaption() {
         return caption;
     }
-
     public void setCaption(String caption) {
         this.caption = caption;
     }
