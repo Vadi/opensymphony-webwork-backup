f1204a818bc302656b3782a798f71d16a7a51d57
davsclaus <davsclaus@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-08 22:28:13 +0000

    polished javadoc

--- src/java/com/opensymphony/webwork/WebWorkTestCase.java
+++ src/java/com/opensymphony/webwork/WebWorkTestCase.java
@@ -4,18 +4,19 @@ import com.opensymphony.webwork.config.Configuration;
 import com.opensymphony.xwork.XWorkTestCase;
 
 /**
- * User: plightbo
- * Date: Jun 25, 2005
- * Time: 9:20:27 AM
+ * Base test case for unit testing WebWork.
+ *
+ * @author plightbo
  */
 public abstract class WebWorkTestCase extends XWorkTestCase {
+
     protected void setUp() throws Exception {
         super.setUp();
-
         Configuration.reset();
     }
 
     protected void tearDown() throws Exception {
         super.tearDown();
     }
+
 }
