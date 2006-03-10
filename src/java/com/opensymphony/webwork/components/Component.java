580b6ab24655695f6c1be70144258a8593b16d78
davsclaus <davsclaus@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-10 21:34:04 +0000

    javadoc polished

--- src/java/com/opensymphony/webwork/components/Component.java
+++ src/java/com/opensymphony/webwork/components/Component.java
@@ -23,9 +23,11 @@ import java.util.Map;
 import java.util.Stack;
 
 /**
- * User: plightbo
- * Date: Jul 2, 2005
- * Time: 5:52:26 PM
+ * Base class to extend for UI components.
+ * <p/>
+ * This class is a good extension point when building reuseable UI components.
+ *
+ * @author plightbo
  */
 public class Component {
     private static final Log LOG = LogFactory.getLog(Component.class);
@@ -36,12 +38,21 @@ public class Component {
     protected Map parameters;
     protected String id;
 
+    /**
+     * Constructor.
+     *
+     * @param stack  OGNL value stack.
+     */
     public Component(OgnlValueStack stack) {
         this.stack = stack;
         this.parameters = new HashMap();
         getComponentStack().push(this);
     }
 
+    /**
+     * Get's the name of this component.
+     * @return the name of this component.
+     */
     private String getComponentName() {
         Class c = getClass();
         String name = c.getName();
@@ -50,10 +61,18 @@ public class Component {
         return name.substring(dot + 1).toLowerCase();
     }
 
+    /**
+     * Get's the OGNL value stack assoicated with this component.
+     * @return the OGNL value stack assoicated with this component.
+     */
     public OgnlValueStack getStack() {
         return stack;
     }
 
+    /**
+     * Get's the component stack of this component.
+     * @return the component stack of this component, never <tt>null</tt>.
+     */
     public Stack getComponentStack() {
         Stack componentStack = (Stack) stack.getContext().get(COMPONENT_STACK);
         if (componentStack == null) {
@@ -64,6 +83,10 @@ public class Component {
     }
 
     /**
+     * Callback for the start tag of this component.
+     * Should the body be evaluated?
+     *
+     * @param writer  the output writer.
      * @return true if the body should be evaluated
      */
     public boolean start(Writer writer) {
@@ -71,7 +94,12 @@ public class Component {
     }
 
     /**
+     * Callback for the end tag of this component.
+     * Should the body be evaluated again?
+     * <p/>
      * <b>NOTE:</b> will pop component stack.
+     * @param writer  the output writer.
+     * @param body    the rendered body.
      * @return true if the body should be evaluated again
      */
     public boolean end(Writer writer, String body) {
@@ -79,7 +107,13 @@ public class Component {
     }
     
     /**
-     * <b>NOTE:</b> Will pop component stack or not depends on <code>popComponentStack</code> parameter.
+     * Callback for the start tag of this component.
+     * Should the body be evaluated again?
+     * <p/>
+     * <b>NOTE:</b> has a parameter to determine to pop the component stack.
+     * @param writer  the output writer.
+     * @param body    the rendered body.
+     * @param popComponentStack  should the component stack be popped?
      * @return true if the body should be evaluated again
      */
     protected boolean end(Writer writer, String body, boolean popComponentStack) {
@@ -88,18 +122,26 @@ public class Component {
         try {
             writer.write(body);
         } catch (IOException e) {
-            throw new RuntimeException("IOError: " + e.getMessage(), e);
+            throw new RuntimeException("IOError while writing the body: " + e.getMessage(), e);
         }
         if (popComponentStack) {
         	popComponentStack();
         }
         return false;
     }
-    
+
+    /**
+     * Pops the component stack.
+     */
     protected void popComponentStack() {
     	getComponentStack().pop();
     }
 
+    /**
+     * Finds the nearest ancestor of this component stack.
+     * @param clazz the class to look for, or if assignable from.
+     * @return  the component if found, <tt>null</tt> if not.
+     */
     protected Component findAncestor(Class clazz) {
         Stack componentStack = getComponentStack();
         int currPosition = componentStack.search(this);
@@ -118,10 +160,27 @@ public class Component {
         return null;
     }
 
+    /**
+     * Evaluates the OGNL stack to find a String value.
+     * @param expr  OGNL expression.
+     * @return  the String value found.
+     */
     protected String findString(String expr) {
         return (String) findValue(expr, String.class);
     }
 
+    /**
+     * Evaluates the OGNL stack to find a String value.
+     * <p/>
+     * If the given expression is <tt>null</tt/> a error is logged and a <code>RuntimeException</code> is thrown
+     * constructed with a messaged based on the given field and errorMsg paramter.
+     *
+     * @param expr  OGNL expression.
+     * @param field   field name used when throwing <code>RuntimeException</code>.
+     * @param errorMsg  error message used when throwing <code>RuntimeException</code>.
+     * @return  the String value found.
+     * @throws RuntimeException is thrown in case of expression is <tt>null</tt>.
+     */
     protected String findString(String expr, String field, String errorMsg) {
         if (expr == null) {
             throw fieldError(field, errorMsg, null);
@@ -130,6 +189,16 @@ public class Component {
         }
     }
 
+    /**
+     * Constructs?a <code>RuntimeException</code> based on the given information.
+     * <p/>
+     * A message is constructed and logged at ERROR level before being returned
+     * as a <code>RuntimeException</code>.
+     * @param field   field name used when throwing <code>RuntimeException</code>.
+     * @param errorMsg  error message used when throwing <code>RuntimeException</code>.
+     * @param e  the caused exception, can be <tt>null</tt>.
+     * @return  the constructed <code>RuntimeException</code>.
+     */
     protected RuntimeException fieldError(String field, String errorMsg, Exception e) {
         String msg = "tag " + getComponentName() + ", field " + field + ": " + errorMsg;
         if (e == null) {
@@ -141,6 +210,11 @@ public class Component {
         }
     }
 
+    /**
+     * Finds a value from the OGNL stack based on the given expression.
+     * @param expr  the expression. Returns <tt>null</tt> if expr is null.
+     * @return the value, <tt>null</tt> if not found.
+     */
     protected Object findValue(String expr) {
         if (expr == null) {
             return null;
@@ -156,10 +230,29 @@ public class Component {
         return getStack().findValue(expr);
     }
 
+    /**
+     * Is the altSyntax enabled?
+     * <p/>
+     * See <code>webwork.properties</code> where the altSyntax flag is defined.
+     * <b>Note:</b> Since WebWork 2.17 the altSyntax is default <tt>true</tt>.
+     * @return true if the altSyntax is enabled.
+     */
     public boolean altSyntax() {
         return ContextUtil.isUseAltSyntax(stack.getContext());
     }
 
+    /**
+     * Evaluates the OGNL stack to find an Object value.
+     * <p/>
+     * If the given expression is <tt>null</tt/> a error is logged and a <code>RuntimeException</code> is thrown
+     * constructed with a messaged based on the given field and errorMsg paramter.
+     *
+     * @param expr  OGNL expression.
+     * @param field   field name used when throwing <code>RuntimeException</code>.
+     * @param errorMsg  error message used when throwing <code>RuntimeException</code>.
+     * @return  the Object found, is never <tt>null</tt>.
+     * @throws RuntimeException is thrown in case of not found in the OGNL stack, or expression is <tt>null</tt>.
+     */
     protected Object findValue(String expr, String field, String errorMsg) {
         if (expr == null) {
             throw fieldError(field, errorMsg, null);
@@ -180,6 +273,14 @@ public class Component {
         }
     }
 
+    /**
+     * Evaluates the OGNL stack to find an Object of the given type.
+     * <p/>
+     * This method only supports the altSyntax. So this should be set to true.
+     * @param expr  OGNL expression.
+     * @param toType  the type expected to find.
+     * @return  the Object found, or <tt>null</tt> if not found.
+     */
     protected Object findValue(String expr, Class toType) {
         if (altSyntax() && toType == String.class) {
             return TextParseUtil.translateVariables('%', expr, stack);
@@ -197,6 +298,16 @@ public class Component {
 
     /**
      * Renders an action URL by consulting the {@link com.opensymphony.webwork.dispatcher.mapper.ActionMapper}.
+     * @param action      the action
+     * @param namespace   the namespace
+     * @param method      the method
+     * @param req         HTTP request
+     * @param res         HTTP response
+     * @param parameters  parameters
+     * @param scheme      http or https
+     * @param includeContext  should the context path be included or not
+     * @param encodeResult    should the url be encoded
+     * @return the action url.
      */
     protected String determineActionURL(String action, String namespace, String method,
                                         HttpServletRequest req, HttpServletResponse res, Map parameters, String scheme,
@@ -210,7 +321,11 @@ public class Component {
     }
 
     /**
-     * Determines the namespace of the current page being renderdd. Useful for Form, URL, and Href generations.
+     * Determines the namespace of the current page being renderdd. Useful for Form, URL, and href generations.
+     * @param namespace  the namespace
+     * @param stack      OGNL value stack
+     * @param req        HTTP request
+     * @return  the namepsace of the current page being rendered, is never <tt>null</tt>.
      */
     protected String determineNamespace(String namespace, OgnlValueStack stack, HttpServletRequest req) {
         String result;
@@ -234,7 +349,7 @@ public class Component {
      * pushed before the component itself, any key-value pair that can't be assigned to componet
      * will be set in the parameters Map.
      *
-     * @param params
+     * @param params  the parameters to copy.
      */
     public void copyParams(Map params) {
         stack.push(parameters);
@@ -251,6 +366,11 @@ public class Component {
         }
     }
 
+    /**
+     * Constructs a string representation of the given exception.
+     * @param t  the exception
+     * @return the exception as a string.
+     */
     protected String toString(Throwable t) {
         FastByteArrayOutputStream bout = new FastByteArrayOutputStream();
         PrintWriter wrt = new PrintWriter(bout);
@@ -260,14 +380,31 @@ public class Component {
         return bout.toString();
     }
 
+    /**
+     * Get's the parameters.
+     * @return the parameters. Is never <tt>null</tt>.
+     */
     public Map getParameters() {
         return parameters;
     }
 
+    /**
+     * Add's all the given parameters to this componenets own parameters.
+     * @param params the parameters to add.
+     */
     public void addAllParameters(Map params) {
         parameters.putAll(params);
     }
 
+    /**
+     * Add's the given key and value to this components own parameter.
+     * <p/>
+     * If the provided key is <tt>null</tt> nothing happends.
+     * If the provided value is <tt>null</tt> any existing parameter with
+     * the given key name is removed.
+     * @param key  the key of the new parameter to add.
+     * @param value the value assoicated with the key.
+     */
     public void addParameter(String key, Object value) {
         if (key != null) {
             Map params = getParameters();
@@ -280,6 +417,10 @@ public class Component {
         }
     }
 
+    /**
+     * Get's the id for referencing element.
+     * @return the id for referencing element.
+     */
     public String getId() {
         return id;
     }
@@ -292,7 +433,12 @@ public class Component {
         this.id = id;
     }
 
+    /**
+     * Overwrite to set if body shold be used.
+     * @return always false for this component.
+     */
     public boolean usesBody() {
         return false;
     }
+
 }
