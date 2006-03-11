1f148675abc51b7ab94486e7d6638e616e337f82
davsclaus <davsclaus@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-11 08:33:44 +0000

    improved unit test

--- trunk/src/test/com/opensymphony/webwork/interceptor/FileUploadInterceptorTest.java
+++ trunk/src/test/com/opensymphony/webwork/interceptor/FileUploadInterceptorTest.java
@@ -5,21 +5,29 @@
 package com.opensymphony.webwork.interceptor;
 
 import com.opensymphony.util.ClassLoaderUtil;
+import com.opensymphony.webwork.ServletActionContext;
 import com.opensymphony.webwork.WebWorkTestCase;
-import com.opensymphony.xwork.ActionContext;
-import com.opensymphony.xwork.ValidationAwareSupport;
+import com.opensymphony.webwork.dispatcher.multipart.MultiPartRequestWrapper;
+import com.opensymphony.xwork.*;
 import com.opensymphony.xwork.mock.MockActionInvocation;
+import org.springframework.mock.web.MockHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+
 
 /**
  * Test case for FileUploadInterceptor.
  *
  * @author tmjee
+ * @author Claus Ibsen
  * @version $Date$ $Id$
  */
 public class FileUploadInterceptorTest extends WebWorkTestCase {
@@ -78,7 +86,7 @@ public class FileUploadInterceptorTest extends WebWorkTestCase {
 
         // when file is not of allowed types
         ValidationAwareSupport validation = new ValidationAwareSupport();
-        
+
         URL url = ClassLoaderUtil.getResource("log4j.properties", FileUploadInterceptorTest.class);
         File file = new File(new URI(url.toString()));
         assertTrue("log4j.properties should be in src/test folder", file.exists());
@@ -97,12 +105,108 @@ public class FileUploadInterceptorTest extends WebWorkTestCase {
     }
 
     public void testNoMultipartRequest() throws Exception {
+        MyFileupAction action = new MyFileupAction();
+
         MockActionInvocation mai = new MockActionInvocation();
+        mai.setAction(action);
         mai.setResultCode("NoMultipart");
         mai.setInvocationContext(ActionContext.getContext());
 
         // if no multipart request it will bypass and execute it
         assertEquals("NoMultipart", interceptor.intercept(mai));
+    }
+
+    public void testInvalidContentTypeMultipartRequest() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+
+        req.setCharacterEncoding("text/html");
+        req.setContentType("text/xml"); // not a multipart contentype
+        req.addHeader("Content-type", "multipart/form-data");
+
+        MyFileupAction action = new MyFileupAction();
+        MockActionInvocation mai = new MockActionInvocation();
+        mai.setAction(action);
+        mai.setResultCode("success");
+        mai.setInvocationContext(ActionContext.getContext());
+
+        Map param = new HashMap();
+        ActionContext.getContext().setParameters(param);
+        ActionContext.getContext().put(ServletActionContext.HTTP_REQUEST, createMultipartRequest((HttpServletRequest) req, 2000));
+
+        interceptor.intercept(mai);
+
+        assertTrue(action.hasErrors());
+    }
+
+    public void testNoContentMultipartRequest() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+
+        req.setCharacterEncoding("text/html");
+        req.setContentType("multipart/form-data; boundary=---1234");
+        req.setContent(null); // there is no content
+
+        MyFileupAction action = new MyFileupAction();
+        MockActionInvocation mai = new MockActionInvocation();
+        mai.setAction(action);
+        mai.setResultCode("success");
+        mai.setInvocationContext(ActionContext.getContext());
+
+        Map param = new HashMap();
+        ActionContext.getContext().setParameters(param);
+        ActionContext.getContext().put(ServletActionContext.HTTP_REQUEST, createMultipartRequest((HttpServletRequest) req, 2000));
+
+        interceptor.intercept(mai);
+
+        assertTrue(action.hasErrors());
+    }
+
+    public void testSuccessUploadOfATextFileMultipartRequest() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.setCharacterEncoding("text/html");
+        req.setContentType("multipart/form-data; boundary=---1234");
+        req.addHeader("Content-type", "multipart/form-data");
+
+        // inspired by the unit tests for jakarta commons fileupload
+        String content = ("-----1234\r\n" +
+            "Content-Disposition: form-data; name=\"file\"; filename=\"deleteme.txt\"\r\n" +
+            "Content-Type: text/html\r\n" +
+            "\r\n" +
+            "Unit test of FileUploadInterceptor" +
+            "\r\n" +
+            "-----1234--\r\n");
+        req.setContent(content.getBytes("US-ASCII"));
+
+        MyFileupAction action = new MyFileupAction();
+
+        MockActionInvocation mai = new MockActionInvocation();
+        mai.setAction(action);
+        mai.setResultCode("success");
+        mai.setInvocationContext(ActionContext.getContext());
+        Map param = new HashMap();
+        ActionContext.getContext().setParameters(param);
+        ActionContext.getContext().put(ServletActionContext.HTTP_REQUEST, createMultipartRequest((HttpServletRequest) req, 2000));
+
+        interceptor.intercept(mai);
+
+        assertTrue(! action.hasErrors());
+
+        assertTrue(param.size() == 3);
+        File[] files = (File[]) param.get("file");
+        String[] fileContentTypes = (String[]) param.get("fileContentType");
+        String[] fileRealFilenames = (String[]) param.get("fileFileName");
+
+        assertNotNull(files);
+        assertNotNull(fileContentTypes);
+        assertNotNull(fileRealFilenames);
+        assertTrue(files.length == 1);
+        assertTrue(fileContentTypes.length == 1);
+        assertTrue(fileRealFilenames.length == 1);
+        assertEquals("text/html", fileContentTypes[0]);
+        assertNotNull("deleteme.txt", fileRealFilenames[0]);
+    }
+
+    private MultiPartRequestWrapper createMultipartRequest(HttpServletRequest req, int maxsize) throws IOException {
+       return new MultiPartRequestWrapper(req, "build" + File.separator + "test", maxsize);
     }
 
     protected void setUp() throws Exception {
@@ -114,5 +218,10 @@ public class FileUploadInterceptorTest extends WebWorkTestCase {
         interceptor.destroy();
         super.tearDown();
     }
+
+    private class MyFileupAction extends ActionSupport {
+        // no methods
+    }
+
 
 }
