a66d735c02ff10b12d65f1595a86558c9f7c5e71
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-11 06:43:35 +0000

    added warning log when resource cannot be found through servletContext
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/com/opensymphony/webwork/dispatcher/PlainTextResult.java
+++ src/java/com/opensymphony/webwork/dispatcher/PlainTextResult.java
@@ -10,6 +10,9 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.opensymphony.xwork.ActionInvocation;
 
 /**
@@ -44,32 +47,43 @@ import com.opensymphony.xwork.ActionInvocation;
  * @version $Date$ $Id$
  */
 public class PlainTextResult extends WebWorkResultSupport {
+	
+	private static final Log _log = LogFactory.getLog(PlainTextResult.class);
 
 	private static final long serialVersionUID = 3633371605905583950L;
 	
-	public static final int BUFFER_SIZE = 1000;
+	public static final int BUFFER_SIZE = 1024;
 	
 	protected void doExecute(String finalLocation, ActionInvocation invocation) throws Exception {
 		
 		HttpServletResponse response = (HttpServletResponse) invocation.getInvocationContext().get(HTTP_RESPONSE);
 		ServletContext servletContext = (ServletContext) invocation.getInvocationContext().get(SERVLET_CONTEXT);
 
-		InputStreamReader reader = new InputStreamReader(servletContext.getResourceAsStream(location));
 		response.setContentType("text/plain");
 		response.setHeader("Content-Disposition", "inline");
 		
 		PrintWriter writer = response.getWriter();
+		InputStreamReader reader = null;
 		try {
-			char[] buffer = new char[BUFFER_SIZE];
-			int charRead = 0;
-			while((charRead = reader.read(buffer)) != -1) {
-				writer.write(buffer, 0, charRead);
+			reader = new InputStreamReader(servletContext.getResourceAsStream(location));
+			if (reader == null) {
+				_log.warn("resource at location ["+location+"] cannot be obtained (return null) from ServletContext !!! ");
+			}
+			else {
+				char[] buffer = new char[BUFFER_SIZE];
+				int charRead = 0;
+				while((charRead = reader.read(buffer)) != -1) {
+					writer.write(buffer, 0, charRead);
+				}
 			}
 		}
 		finally {
-			reader.close();
-			writer.flush();
-			writer.close();
+			if (reader != null)
+				reader.close();
+			if (writer != null) {
+				writer.flush();
+				writer.close();
+			}
 		}
 	}
 
