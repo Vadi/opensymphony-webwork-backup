3778ad10aba492a4c709016f7abbded151210932
davsclaus <davsclaus@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-17 15:53:23 +0000

    fixed WW-808 - updated javadoc to document the two situations that WW behaves differently for param tag

--- src/java/com/opensymphony/webwork/components/Param.java
+++ src/java/com/opensymphony/webwork/components/Param.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2002-2006 by OpenSymphony
+ * All rights reserved.
+ */
 package com.opensymphony.webwork.components;
 
 import com.opensymphony.xwork.util.OgnlValueStack;
@@ -6,39 +10,59 @@ import java.io.Writer;
 
 /**
  * <!-- START SNIPPET: javadoc -->
- * <p>This tag can be used to parameterize other tags, who implement
- * the ParametricTag interface declared here.</p>
- *
- * The include tag and bean tag are examples of such tags.<p/>
- *
- * The inner classes, Parametric and UnnamedParametric, are implemented by tags. They indicate that a
- * particular Tag may have embedded params. For example, if we were wanted to use the ComponentTag
- * and wanted to provide custom params to assist with the rendering, we could declare something like<p/>
- * <!-- END SNIPPET: javadoc -->
- *
+ * <p>This tag can be used to parameterize other tags.</p>
+ * The include tag and bean tag are examples of such tags.
+ * <p/>
+ * The parameters can be added with or without a name as key.
+ * If the tag provides a name attribute the parameters are added using the
+ * {@link Component#addParameter(String, Object) addParamter} method.
+ * For unnamed parameters the Tag must implement the {@link UnnamedParametric} interface defined in
+ * this class (e.g. The TextTag does this).
+ * <p/>
+ * This tag has the following two paramters.
  * <!-- START SNIPPET: params -->
  * <ul>
  * 		<li>name (String) - the name of the parameter</li>
  *      <li>value (Object) - the value of the parameter</li>
  * </ul>
  * <!-- END SNIPPET: params -->
- *
+ * <p/>
+ * <b>Note:</b>
+ * When you declare the param tag, the value can be defined in either a <tt>value</tt> attribute or
+ * as text between the start and end tag. WebWork behaves a bit different according to these two situations.
+ * This is best illustrated using an example:
+ * <br/>&lt;param name="color"&gt;blue&lt;/param&gt; &lt;-- (A) --&gt;
+ * <br/>&lt;param name="color" value="blue"/&gt; &lt;-- (B) --&gt;
+ * <br/>In the first situation (A) the value would be evaluated to the stack as a <tt>java.lang.String</tt> object.
+ * And in situation (B) the value would be evaluated to the stack as a <tt>java.lang.Object</tt> object.
+ * <br/>For more information see <a href="http://jira.opensymphony.com/browse/WW-808">WW-808</a>.
+ * <!-- END SNIPPET: javadoc -->
  *
  * <p/> <b>Examples</b>
- * <pre>
  * <!-- START SNIPPET: example -->
+ * <pre>
  * &lt;ui:component&gt;
  *  &lt;ui:param name="key"     value="[0]"/&gt;
  *  &lt;ui:param name="value"   value="[1]"/&gt;
  *  &lt;ui:param name="context" value="[2]"/&gt;
  * &lt;/ui:component&gt;
- * <!-- END SNIPPET: example -->
  * </pre>
+ * <!-- END SNIPPET: example -->
  * <p/>
  * <!-- START SNIPPET: exampledescription -->
  * where the key will be the identifier and the value the result of an OGNL expression run against the current
- * OgnlValueStack.<p/>
+ * OgnlValueStack.
  * <!-- END SNIPPET: exampledescription -->
+ * <p/>
+ * This second example demonstrates how the text tag can use parameters from this param tag.
+ * <!-- START SNIPPET: example2 -->
+ * <pre>
+ * &lt;ww:text name="cart.total.cost"&gt;
+ *     &lt;ww:param value="#session.cartTotal"/&gt;
+ * &lt;/ww:text&gt;
+ * </pre>
+ * <!-- END SNIPPET: example2 -->
+ * <p/>
  *
  * @author Rickard �berg (rickard@dreambean.com)
  * @author Rene Gielen
@@ -48,6 +72,7 @@ import java.io.Writer;
  *
  * @see Include
  * @see Bean
+ * @see Text
  *
  * @ww.tag name="param" tld-body-content="JSP" tld-tag-class="com.opensymphony.webwork.views.jsp.ParamTag"
  * description="Parametrize other tags"
@@ -106,7 +131,20 @@ public class Param extends Component {
         this.value = value;
     }
 
+
+    /**
+     * Tags can implement this to support nested param tags without the <tt>name</tt> attribute.
+     * <p/>
+     * The {@link Text TextTag} uses this approach. For unnamed parameters an example is given in the class
+     * javadoc for {@link Param ParamTag}.
+     */
     public interface UnnamedParametric {
+
+        /**
+         * Adds the given value as a parameter to the outer tag.
+         * @param value  the value
+         */
         public void addParameter(Object value);
     }
+
 }
