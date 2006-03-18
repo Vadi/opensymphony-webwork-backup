45ba4793947e899a611665022cb39afe8f19df71
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-18 04:45:13 +0000

    updated javadoc to show configurable param due to extention off MethodFilterInterceptor and added example as well
    Issue number:  ww-1246
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/com/opensymphony/webwork/interceptor/TokenInterceptor.java
+++ src/java/com/opensymphony/webwork/interceptor/TokenInterceptor.java
@@ -25,10 +25,14 @@ import org.apache.commons.logging.LogFactory;
  * invalid token is found: it returns the result <b>invalid.token</b>, which can be mapped in your action configuration.
  * A more complex implementation, {@link TokenSessionStoreInterceptor}, can provide much better logic for when invalid
  * tokens are found.
+ * 
+ * <p/>
  *
  * <b>Note:</b> To set a token in your form, you should use the <b>token tag</b>. This tag is required and must be used
  * in the forms that submit to actions protected by this interceptor. Any request that does not provide a token (using
  * the token tag) will be processed as a request with an invalid token.
+ * 
+ * <p/>
  * 
  * <b>Internationalization Note:</b> The following key could be used to internationalized the action errors generated
  * by this token interceptor
@@ -36,6 +40,12 @@ import org.apache.commons.logging.LogFactory;
  * <ul>
  *    <li>webwork.messages.invalid.token</li>
  * </ul>
+ * 
+ * <p/>
+ * 
+ * <b>NOTE:</b> As this method extends off MethodFilterInterceptor, it is capable of
+ * deciding if it is applicable only to selective methods in the action class. See
+ * <code>MethodFilterInterceptor</code> for more info.
  *
  * <!-- END SNIPPET: description -->
  *
@@ -67,11 +77,23 @@ import org.apache.commons.logging.LogFactory;
  *
  * <pre>
  * <!-- START SNIPPET: example -->
+ * 
  * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
  *     &lt;interceptor-ref name="token"/&gt;
  *     &lt;interceptor-ref name="basicStack"/&gt;
  *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
  * &lt;/action&gt;
+ * 
+ * &lt;-- In this case, myMethod of the action class will not 
+ *        get checked for invalidity of token --&gt;
+ * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
+ *     &lt;interceptor-ref name="token"&gt;
+ *     	  &lt;param name="excludeMethods"&gt;myMethod&lt;/param&gt;
+ *     &lt;/interceptor-ref name="token"/&gt;
+ *     &lt;interceptor-ref name="basicStack"/&gt;
+ *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
+ * &lt;/action&gt;
+ * 
  * <!-- END SNIPPET: example -->
  * </pre>
  *
@@ -81,6 +103,8 @@ import org.apache.commons.logging.LogFactory;
  * @author <a href='mailto:the_mindstorm[at]evolva[dot]ro'>Alexandru Popescu</a>
  * @see TokenSessionStoreInterceptor
  * @see TokenHelper
+ * 
+ * @version $Date$ $Id$
  */
 public class TokenInterceptor extends MethodFilterInterceptor {
     public static final String INVALID_TOKEN_CODE = "invalid.token";
