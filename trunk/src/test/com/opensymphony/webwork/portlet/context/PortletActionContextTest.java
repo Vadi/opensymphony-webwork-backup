f24c7687f3efa96417a435f6918c529621c30320
nilsga <nilsga@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-19 18:27:23 +0000

    Issue number:  WW-1230 Obtained from: Submitted by: Reviewed by:
    Test for PortletActionContext

--- trunk/src/test/com/opensymphony/webwork/portlet/context/PortletActionContextTest.java
+++ trunk/src/test/com/opensymphony/webwork/portlet/context/PortletActionContextTest.java
@@ -7,7 +7,11 @@ package com.opensymphony.webwork.portlet.context;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
 import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import junit.textui.TestRunner;
 
@@ -25,17 +29,138 @@ import com.opensymphony.xwork.ActionContext;
  */
 public class PortletActionContextTest extends MockObjectTestCase {
     
-    public void testGetRenderRequest() {
-        Mock mockRenderReq = mock(RenderRequest.class);
-        RenderRequest req = (RenderRequest)mockRenderReq.proxy();
+    Mock mockRenderRequest;
+    Mock mockRenderResponse;
+    Mock mockPortletConfig;
+    Mock mockActionRequest;
+    Mock mockActionResponse;
+    
+    RenderRequest renderRequest;
+    RenderResponse renderResponse;
+    
+    ActionRequest actionRequest;
+    ActionResponse actionResponse;
+    
+    PortletConfig portletConfig;
+    
+    Map context = new HashMap();
+    
+    public void setUp() throws Exception {
+        super.setUp();
+        mockRenderRequest = mock(RenderRequest.class);
+        mockRenderResponse = mock(RenderResponse.class);
+        mockActionRequest = mock(ActionRequest.class);
+        mockActionResponse = mock(ActionResponse.class);
+        mockPortletConfig = mock(PortletConfig.class);
         
-        Map extraContext = new HashMap();
-        extraContext.put(PortletActionConstants.REQUEST, req);
-        extraContext.put(PortletActionConstants.PHASE, PortletActionConstants.RENDER_PHASE);
-        ActionContext ctx = new ActionContext(extraContext);
-        ActionContext.setContext(ctx);
+        renderRequest = (RenderRequest)mockRenderRequest.proxy();
+        renderResponse = (RenderResponse)mockRenderResponse.proxy();
+        actionRequest = (ActionRequest)mockActionRequest.proxy();
+        actionResponse = (ActionResponse)mockActionResponse.proxy();
+        portletConfig = (PortletConfig)mockPortletConfig.proxy();
         
-        assertNotNull(PortletActionContext.getRenderRequest());
+        
+        ActionContext.setContext(new ActionContext(context));
+    }
+    
+    public void testGetPhase() {
+        context.put(PortletActionConstants.PHASE, PortletActionConstants.RENDER_PHASE);
+        
+        assertEquals(PortletActionConstants.RENDER_PHASE, PortletActionContext.getPhase());
+    }
+    
+    public void testIsRender() {
+        context.put(PortletActionConstants.PHASE, PortletActionConstants.RENDER_PHASE);
+        
+        assertTrue(PortletActionContext.isRender());
+        assertFalse(PortletActionContext.isEvent());
+    }
+    
+    public void testIsEvent() {
+        context.put(PortletActionConstants.PHASE, PortletActionConstants.EVENT_PHASE);
+        
+        assertTrue(PortletActionContext.isEvent());
+        assertFalse(PortletActionContext.isRender());
+    }
+    
+    public void testGetPortletConfig() {
+        context.put(PortletActionConstants.PORTLET_CONFIG, portletConfig);
+        assertSame(portletConfig, PortletActionContext.getPortletConfig());
+    }
+    
+    public void testGetRenderRequestAndResponse() {
+        context.put(PortletActionConstants.REQUEST, renderRequest);
+        context.put(PortletActionConstants.RESPONSE, renderResponse);
+        context.put(PortletActionConstants.PHASE, PortletActionConstants.RENDER_PHASE);
+        assertSame(renderRequest, PortletActionContext.getRenderRequest());
+        assertSame(renderResponse, PortletActionContext.getRenderResponse());
+        assertSame(renderRequest, PortletActionContext.getRequest());
+        assertSame(renderResponse, PortletActionContext.getResponse());
+    }
+    
+    public void testGetRenderRequestAndResponseInEventPhase() {
+        context.put(PortletActionConstants.REQUEST, renderRequest);
+        context.put(PortletActionConstants.RESPONSE, renderResponse);
+        context.put(PortletActionConstants.PHASE, PortletActionConstants.EVENT_PHASE);
+        try {
+            PortletActionContext.getRenderRequest();
+            fail("Should throw IllegalStateException!");
+        }
+        catch(IllegalStateException e) {
+            assertTrue(true);
+        }
+        try {
+            PortletActionContext.getRenderResponse();
+            fail("Should throw IllegalStateException!");
+        }
+        catch(IllegalStateException e) {
+            assertTrue(true);
+        }
+    }
+    
+    public void testGetActionRequestAndResponse() {
+        context.put(PortletActionConstants.REQUEST, actionRequest);
+        context.put(PortletActionConstants.RESPONSE, actionResponse);
+        context.put(PortletActionConstants.PHASE, PortletActionConstants.EVENT_PHASE);
+        assertSame(actionRequest, PortletActionContext.getActionRequest());
+        assertSame(actionResponse, PortletActionContext.getActionResponse());
+        assertSame(actionRequest, PortletActionContext.getRequest());
+        assertSame(actionResponse, PortletActionContext.getResponse());
+    }
+    
+    public void testGetActionRequestAndResponseInRenderPhase() {
+        context.put(PortletActionConstants.REQUEST, actionRequest);
+        context.put(PortletActionConstants.RESPONSE, actionResponse);
+        context.put(PortletActionConstants.PHASE, PortletActionConstants.RENDER_PHASE);
+        try {
+            PortletActionContext.getActionRequest();
+            fail("Should throw IllegalStateException!");
+        }
+        catch(IllegalStateException e) {
+            assertTrue(true);
+        }
+        try {
+            PortletActionContext.getActionResponse();
+            fail("Should throw IllegalStateException!");
+        }
+        catch(IllegalStateException e) {
+            assertTrue(true);
+        }
+    }
+    
+    public void testGetNamespace() {
+        context.put(PortletActionConstants.PORTLET_NAMESPACE, "testNamespace");
+        assertEquals("testNamespace", PortletActionContext.getPortletNamespace());
+    }
+    
+    public void testGetDefaultActionForMode() {
+        context.put(PortletActionConstants.DEFAULT_ACTION_FOR_MODE, "testAction");
+        assertEquals("testAction", PortletActionContext.getDefaultActionForMode());
+    }
+    
+    public void tearDown() throws Exception {
+        ActionContext.setContext(null);
+        super.tearDown();
     }
     
     public static void main(String[] args) {
