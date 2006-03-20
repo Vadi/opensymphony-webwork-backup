5e6e69529cfe052eaa2967db7dd8ee8b6aaca548
rainerh <rainerh@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-20 18:22:00 +0000

    Add an example for the Date component in showcase app o adding more samples Issue number:  WW-1245 Obtained from: Submitted by:  Philip Luppens Reviewed by:   Rainer Hermanns

--- webapps/showcase/src/java/com/opensymphony/webwork/showcase/DateAction.java
+++ webapps/showcase/src/java/com/opensymphony/webwork/showcase/DateAction.java
@@ -10,6 +10,7 @@ import com.opensymphony.xwork.ActionSupport;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * <code>DateAction</code>
@@ -20,25 +21,73 @@ public class DateAction extends ActionSupport {
 
     private static DateFormat DF = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
 
+    private Date now;
+    private Date past;
+    private Date future;
+    private Date after;
+    private Date before;
+
+
     public String getDate() {
         return DF.format(new Date());
     }
 
+
+    /**
+     * @return Returns the future.
+     */
+    public Date getFuture() {
+        return future;
+    }
+
+    /**
+     * @return Returns the now.
+     */
     public Date getNow() {
-        return new Date();
+        return now;
     }
 
+    /**
+     * @return Returns the past.
+     */
+    public Date getPast() {
+        return past;
+    }
+
+    /**
+     *
+     * @return Returns the before date.
+     */
     public Date getBefore() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.add(Calendar.YEAR, -1);
-        return cal.getTime();
+        return before;
     }
 
+    /**
+     *
+     * @return Returns the after date.
+     */
     public Date getAfter() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.add(Calendar.YEAR, 1);
-        return cal.getTime();
+        return after;
     }
+
+    /**
+     * @see com.opensymphony.xwork.ActionSupport#execute()
+     */
+    public String execute() throws Exception {
+        Calendar cal = GregorianCalendar.getInstance();
+        now = cal.getTime();
+        cal.roll(Calendar.DATE, -1);
+        cal.roll(Calendar.HOUR, -3);
+        past = cal.getTime();
+        cal.roll(Calendar.DATE, 2);
+        future = cal.getTime();
+
+        cal.roll(Calendar.YEAR, -1);
+        before = cal.getTime();
+
+        cal.roll(Calendar.YEAR, 2);
+        after = cal.getTime();
+        return SUCCESS;
+    }
+
 }
