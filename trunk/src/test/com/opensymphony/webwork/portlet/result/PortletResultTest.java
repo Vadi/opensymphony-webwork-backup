e15896286d241b4df3cf99907118873d5bfe2f18
nilsga <nilsga@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-21 20:37:27 +0000

    Issue number:  WW-1230 Obtained from: Submitted by: Reviewed by:
    More tests

--- trunk/src/test/com/opensymphony/webwork/portlet/result/PortletResultTest.java
+++ trunk/src/test/com/opensymphony/webwork/portlet/result/PortletResultTest.java
@@ -187,6 +187,43 @@ public class PortletResultTest extends MockObjectTestCase {
         }
     }
     
+    public void testTitleAndContentType() throws Exception {
+        Mock mockRequest = mock(RenderRequest.class);
+        Mock mockResponse = mock(RenderResponse.class);
+        Mock mockRd = mock(PortletRequestDispatcher.class);
+        Mock mockPrep = mock(PortletRequestDispatcher.class);
+        
+        RenderRequest req = (RenderRequest)mockRequest.proxy();
+        RenderResponse res = (RenderResponse)mockResponse.proxy();
+        PortletRequestDispatcher rd = (PortletRequestDispatcher)mockRd.proxy();
+        PortletConfig cfg = (PortletConfig)mockConfig.proxy();
+        PortletContext ctx = (PortletContext)mockCtx.proxy();
+        ActionInvocation inv = (ActionInvocation)mockInvocation.proxy();
+        
+        Constraint[] params = new Constraint[]{same(req), same(res)};
+        mockRd.expects(once()).method("include").with(params);
+        mockPrep.expects(once()).method("include").with(params);
+        mockCtx.expects(once()).method("getRequestDispatcher").with(eq("/WEB-INF/pages/testPage.jsp")).will(returnValue(rd));
+        mockCtx.expects(once()).method("getNamedDispatcher").with(eq("preparator")).will(returnValue(mockPrep.proxy()));
+        mockConfig.expects(once()).method("getPortletContext").will(returnValue(ctx));
+        
+        mockRequest.stubs().method("getPortletMode").will(returnValue(PortletMode.VIEW));
+        
+        ActionContext ctxMap = ActionContext.getContext();
+        ctxMap.put(PortletActionConstants.RESPONSE, res);
+        ctxMap.put(PortletActionConstants.REQUEST, req);
+        ctxMap.put(PortletActionConstants.PORTLET_CONFIG, cfg);
+        ctxMap.put(PortletActionConstants.PHASE, PortletActionConstants.RENDER_PHASE);
+        
+        mockResponse.expects(atLeastOnce()).method("setTitle").with(eq("testTitle"));
+        mockResponse.expects(atLeastOnce()).method("setContentType").with(eq("testContentType"));
+        
+        PortletResult result = new PortletResult();
+        result.setTitle("testTitle");
+        result.setContentType("testContentType");
+        result.doExecute("/WEB-INF/pages/testPage.jsp", (ActionInvocation)mockInvocation.proxy());
+    }
+    
     public void tearDown() throws Exception {
         super.tearDown();
         ActionContext.setContext(null);
