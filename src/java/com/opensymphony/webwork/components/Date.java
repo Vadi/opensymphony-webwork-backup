c1802b717bc525aec1f80f587707387549f32230
rainerh <rainerh@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-21 17:26:19 +0000

    ww:date tag Issue number: WW-805

--- src/java/com/opensymphony/webwork/components/Date.java
+++ src/java/com/opensymphony/webwork/components/Date.java
@@ -110,6 +110,7 @@ import java.util.List;
  * <pre>
  *  <!-- START SNIPPET: example -->
  *  &lt;ww:date name="person.birthday" format="dd/MM/yyyy" /&gt;
+ *  &lt;ww:date name="person.birthday" format="%{getText('some.i18n.key')}" /&gt;
  *  &lt;ww:date name="person.birthday" nice="true" /&gt;
  *  &lt;ww:date name="person.birthday" /&gt;
  *  <!-- END SNIPPET: example -->
@@ -128,237 +129,246 @@ import java.util.List;
  */
 public class Date extends Component {
 
-	private static final Log LOG = LogFactory.getLog(Date.class);
-	/**
-	 * Property name to fall back when no format is specified
-	 */
-	public static final String DATETAG_PROPERTY = "webwork.date.format";
-	/**
-	 * Property name that defines the past notation (default: {0} ago)
-	 */
-	public static final String DATETAG_PROPERTY_PAST = "webwork.date.format.past";
-	private static final String DATETAG_DEFAULT_PAST = "{0} ago";
-	/**
-	 * Property name that defines the future notation (default: in {0})
-	 */
-	public static final String DATETAG_PROPERTY_FUTURE = "webwork.date.format.future";
-	private static final String DATETAG_DEFAULT_FUTURE = "in {0}";
-	/**
-	 * Property name that defines the seconds notation (default: in instant)
-	 */
-	public static final String DATETAG_PROPERTY_SECONDS = "webwork.date.format.seconds";
-	private static final String DATETAG_DEFAULT_SECONDS = "an instant";
-	/**
-	 * Property name that defines the minutes notation (default: {0,choice,1#one minute|1<{0} minutes})
-	 */
-	public static final String DATETAG_PROPERTY_MINUTES = "webwork.date.format.minutes";
-	private static final String DATETAG_DEFAULT_MINUTES = "{0,choice,1#one minute|1<{0} minutes}";
-	/**
-	 * Property name that defines the hours notation (default: {0,choice,1#one hour|1<{0} hours}{1,choice,0#|1#, one minute|1<, {1} minutes})
-	 */
-	public static final String DATETAG_PROPERTY_HOURS = "webwork.date.format.hours";
-	private static final String DATETAG_DEFAULT_HOURS = "{0,choice,1#one hour|1<{0} hours}{1,choice,0#|1#, one minute|1<, {1} minutes}";
-	/**
-	 * Property name that defines the days notation (default: {0,choice,1#one day|1<{0} days}{1,choice,0#|1#, one hour|1<, {1} hours})
-	 */
-	public static final String DATETAG_PROPERTY_DAYS = "webwork.date.format.days";
-	private static final String DATETAG_DEFAULT_DAYS = "{0,choice,1#one day|1<{0} days}{1,choice,0#|1#, one hour|1<, {1} hours}";
-	/**
-	 * Property name that defines the years notation (default: {0,choice,1#one year|1<{0} years}{1,choice,0#|1#, one day|1<, {1} days})
-	 */
-	public static final String DATETAG_PROPERTY_YEARS = "webwork.date.format.years";
-	private static final String DATETAG_DEFAULT_YEARS = "{0,choice,1#one year|1<{0} years}{1,choice,0#|1#, one day|1<, {1} days}";
+    private static final Log LOG = LogFactory.getLog(Date.class);
+    /**
+     * Property name to fall back when no format is specified
+     */
+    public static final String DATETAG_PROPERTY = "webwork.date.format";
+    /**
+     * Property name that defines the past notation (default: {0} ago)
+     */
+    public static final String DATETAG_PROPERTY_PAST = "webwork.date.format.past";
+    private static final String DATETAG_DEFAULT_PAST = "{0} ago";
+    /**
+     * Property name that defines the future notation (default: in {0})
+     */
+    public static final String DATETAG_PROPERTY_FUTURE = "webwork.date.format.future";
+    private static final String DATETAG_DEFAULT_FUTURE = "in {0}";
+    /**
+     * Property name that defines the seconds notation (default: in instant)
+     */
+    public static final String DATETAG_PROPERTY_SECONDS = "webwork.date.format.seconds";
+    private static final String DATETAG_DEFAULT_SECONDS = "an instant";
+    /**
+     * Property name that defines the minutes notation (default: {0,choice,1#one minute|1<{0} minutes})
+     */
+    public static final String DATETAG_PROPERTY_MINUTES = "webwork.date.format.minutes";
+    private static final String DATETAG_DEFAULT_MINUTES = "{0,choice,1#one minute|1<{0} minutes}";
+    /**
+     * Property name that defines the hours notation (default: {0,choice,1#one hour|1<{0} hours}{1,choice,0#|1#, one
+     * minute|1<, {1} minutes})
+     */
+    public static final String DATETAG_PROPERTY_HOURS = "webwork.date.format.hours";
+    private static final String DATETAG_DEFAULT_HOURS = "{0,choice,1#one hour|1<{0} hours}{1,choice,0#|1#, one minute|1<, {1} minutes}";
+    /**
+     * Property name that defines the days notation (default: {0,choice,1#one day|1<{0} days}{1,choice,0#|1#, one hour|1<,
+     * {1} hours})
+     */
+    public static final String DATETAG_PROPERTY_DAYS = "webwork.date.format.days";
+    private static final String DATETAG_DEFAULT_DAYS = "{0,choice,1#one day|1<{0} days}{1,choice,0#|1#, one hour|1<, {1} hours}";
+    /**
+     * Property name that defines the years notation (default: {0,choice,1#one year|1<{0} years}{1,choice,0#|1#, one
+     * day|1<, {1} days})
+     */
+    public static final String DATETAG_PROPERTY_YEARS = "webwork.date.format.years";
+    private static final String DATETAG_DEFAULT_YEARS = "{0,choice,1#one year|1<{0} years}{1,choice,0#|1#, one day|1<, {1} days}";
 
-	private String name;
+    private String name;
 
-	private String format;
+    private String format;
 
-	private boolean nice;
+    private boolean nice;
 
-	public Date(OgnlValueStack stack) {
-		super(stack);
-	}
+    public Date(OgnlValueStack stack) {
+        super(stack);
+    }
 
-	private TextProvider findProviderInStack() {
-		for (Iterator iterator = getStack().getRoot().iterator(); iterator
-				.hasNext();) {
-			Object o = iterator.next();
+    private TextProvider findProviderInStack() {
+        for (Iterator iterator = getStack().getRoot().iterator(); iterator
+                .hasNext();) {
+            Object o = iterator.next();
 
-			if (o instanceof TextProvider) {
-				return (TextProvider) o;
-			}
-		}
-		return null;
-	}
+            if (o instanceof TextProvider) {
+                return (TextProvider) o;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * Calculates the difference in time from now to the given date, and outputs
-	 * it nicely. <p/> An example: <br/>Now = 2006/03/12 13:38:00, date =
-	 * 2006/03/12 15:50:00 will output "in 1 hour, 12 minutes".
-	 *
-	 * @param tp text provider
-	 * @param date the date
-	 * @return the date nicely
-	 */
-	public String formatTime(TextProvider tp, java.util.Date date) {
-		java.util.Date now = new java.util.Date();
-		StringBuffer sb = new StringBuffer();
-		List args = new ArrayList();
-		long secs = Math.abs((now.getTime() - date.getTime()) / 1000);
-		long mins = secs / 60;
-		long sec = secs % 60;
-		int min = (int) mins % 60;
-		long hours = mins / 60;
-		int hour = (int) hours % 24;
-		int days = (int) hours / 24;
-		int day = days % 365;
-		int years = days / 365;
+    /**
+     * Calculates the difference in time from now to the given date, and outputs it nicely. <p/> An example: <br/>Now =
+     * 2006/03/12 13:38:00, date = 2006/03/12 15:50:00 will output "in 1 hour, 12 minutes".
+     *
+     * @param tp   text provider
+     * @param date the date
+     * @return the date nicely
+     */
+    public String formatTime(TextProvider tp, java.util.Date date) {
+        java.util.Date now = new java.util.Date();
+        StringBuffer sb = new StringBuffer();
+        List args = new ArrayList();
+        long secs = Math.abs((now.getTime() - date.getTime()) / 1000);
+        long mins = secs / 60;
+        long sec = secs % 60;
+        int min = (int) mins % 60;
+        long hours = mins / 60;
+        int hour = (int) hours % 24;
+        int days = (int) hours / 24;
+        int day = days % 365;
+        int years = days / 365;
 
-		if (years > 0) {
-			args.add(new Long(years));
-			args.add(new Long(day));
-			args.add(sb);
-			args.add(null);
-			sb.append(tp.getText(DATETAG_PROPERTY_YEARS, DATETAG_DEFAULT_YEARS, args));
-		} else if (day > 0) {
-			args.add(new Long(day));
-			args.add(new Long(hour));
-			args.add(sb);
-			args.add(null);
-			sb.append(tp.getText(DATETAG_PROPERTY_DAYS, DATETAG_DEFAULT_DAYS, args));
-		} else if (hour > 0) {
-			args.add(new Long(hour));
-			args.add(new Long(min));
-			args.add(sb);
-			args.add(null);
-			sb.append(tp.getText(DATETAG_PROPERTY_HOURS, DATETAG_DEFAULT_HOURS, args));
-		} else if (min > 0) {
-			args.add(new Long(min));
-			args.add(new Long(sec));
-			args.add(sb);
-			args.add(null);
-			sb.append(tp.getText(DATETAG_PROPERTY_MINUTES, DATETAG_DEFAULT_MINUTES, args));
-		} else {
-			args.add(new Long(sec));
-			args.add(sb);
-			args.add(null);
-			sb.append(tp.getText(DATETAG_PROPERTY_SECONDS, DATETAG_DEFAULT_SECONDS, args));
-		}
+        if (years > 0) {
+            args.add(new Long(years));
+            args.add(new Long(day));
+            args.add(sb);
+            args.add(null);
+            sb.append(tp.getText(DATETAG_PROPERTY_YEARS, DATETAG_DEFAULT_YEARS, args));
+        } else if (day > 0) {
+            args.add(new Long(day));
+            args.add(new Long(hour));
+            args.add(sb);
+            args.add(null);
+            sb.append(tp.getText(DATETAG_PROPERTY_DAYS, DATETAG_DEFAULT_DAYS, args));
+        } else if (hour > 0) {
+            args.add(new Long(hour));
+            args.add(new Long(min));
+            args.add(sb);
+            args.add(null);
+            sb.append(tp.getText(DATETAG_PROPERTY_HOURS, DATETAG_DEFAULT_HOURS, args));
+        } else if (min > 0) {
+            args.add(new Long(min));
+            args.add(new Long(sec));
+            args.add(sb);
+            args.add(null);
+            sb.append(tp.getText(DATETAG_PROPERTY_MINUTES, DATETAG_DEFAULT_MINUTES, args));
+        } else {
+            args.add(new Long(sec));
+            args.add(sb);
+            args.add(null);
+            sb.append(tp.getText(DATETAG_PROPERTY_SECONDS, DATETAG_DEFAULT_SECONDS, args));
+        }
 
-		args.clear();
-		args.add(sb.toString());
-		if (date.before(now)) {
-			// looks like this date is passed
-			return tp.getText(DATETAG_PROPERTY_PAST, DATETAG_DEFAULT_PAST, args);
-		} else {
-			return tp.getText(DATETAG_PROPERTY_FUTURE, DATETAG_DEFAULT_FUTURE, args);
-		}
-	}
+        args.clear();
+        args.add(sb.toString());
+        if (date.before(now)) {
+            // looks like this date is passed
+            return tp.getText(DATETAG_PROPERTY_PAST, DATETAG_DEFAULT_PAST, args);
+        } else {
+            return tp.getText(DATETAG_PROPERTY_FUTURE, DATETAG_DEFAULT_FUTURE, args);
+        }
+    }
 
-	public boolean end(Writer writer, String body) {
-		String msg = null;
-		OgnlValueStack stack = getStack();
-		java.util.Date date = null;
-		// find the name on the valueStack, and cast it to a date
-		try {
-			date = (java.util.Date) findValue(name);
-		} catch (Exception e) {
-			LOG.error("Could not convert object with key '" + name
-					+ "' to a java.util.Date instance");
-			// bad date, return a blank instead ?
-			msg = "";
-		}
+    public boolean end(Writer writer, String body) {
+        String msg = null;
+        OgnlValueStack stack = getStack();
+        java.util.Date date = null;
+        // find the name on the valueStack, and cast it to a date
+        try {
+            date = (java.util.Date) findValue(name);
+        } catch (Exception e) {
+            LOG.error("Could not convert object with key '" + name
+                    + "' to a java.util.Date instance");
+            // bad date, return a blank instead ?
+            msg = "";
+        }
 
-		if (date != null) {
-			TextProvider tp = findProviderInStack();
-			if (tp != null) {
-				if (nice) {
-					msg = formatTime(tp, date);
-				} else {
-					if (format == null) {
-						String globalFormat = null;
+        //try to find the format on the stack
+        if (format != null) {
+            format = findString(format);
+        }
+        if (date != null) {
+            TextProvider tp = findProviderInStack();
+            if (tp != null) {
+                if (nice) {
+                    msg = formatTime(tp, date);
+                } else {
+                    if (format == null) {
+                        String globalFormat = null;
 
-						// if the format is not specified, fall back using the
-						// defined property DATETAG_PROPERTY
-						globalFormat = tp.getText(DATETAG_PROPERTY);
+                        // if the format is not specified, fall back using the
+                        // defined property DATETAG_PROPERTY
+                        globalFormat = tp.getText(DATETAG_PROPERTY);
 
-						// if tp.getText can not find the property then the
-						// returned string is the same as input =
-						// DATETAG_PROPERTY
-						if (globalFormat != null
-								&& !DATETAG_PROPERTY.equals(globalFormat)) {
-							msg = new SimpleDateFormat(globalFormat,
-									ActionContext.getContext().getLocale())
-									.format(date);
-						} else {
-							msg = DateFormat.getDateTimeInstance(
-									DateFormat.MEDIUM, DateFormat.MEDIUM,
-									ActionContext.getContext().getLocale())
-									.format(date);
-						}
-					} else {
-						msg = new SimpleDateFormat(format, ActionContext
-								.getContext().getLocale()).format(date);
-					}
-				}
-				if (msg != null) {
-					try {
-						if (getId() == null) {
-							writer.write(msg);
-						} else {
-							stack.getContext().put(getId(), msg);
-						}
-					} catch (IOException e) {
-						LOG.error("Could not write out Date tag", e);
-					}
-				}
-			}
-		}
-		return super.end(writer, "");
-	}
+                        // if tp.getText can not find the property then the
+                        // returned string is the same as input =
+                        // DATETAG_PROPERTY
+                        if (globalFormat != null
+                                && !DATETAG_PROPERTY.equals(globalFormat)) {
+                            msg = new SimpleDateFormat(globalFormat,
+                                    ActionContext.getContext().getLocale())
+                                    .format(date);
+                        } else {
+                            msg = DateFormat.getDateTimeInstance(
+                                    DateFormat.MEDIUM, DateFormat.MEDIUM,
+                                    ActionContext.getContext().getLocale())
+                                    .format(date);
+                        }
+                    } else {
+                        msg = new SimpleDateFormat(format, ActionContext
+                                .getContext().getLocale()).format(date);
+                    }
+                }
+                if (msg != null) {
+                    try {
+                        if (getId() == null) {
+                            writer.write(msg);
+                        } else {
+                            stack.getContext().put(getId(), msg);
+                        }
+                    } catch (IOException e) {
+                        LOG.error("Could not write out Date tag", e);
+                    }
+                }
+            }
+        }
+        return super.end(writer, "");
+    }
 
-	/**
+    /**
      * Date or DateTime format pattern
-	 * @ww.tagattribute required="false" rtexprvalue="false"
-	 */
-	public void setFormat(String format) {
-		this.format = format;
-	}
+     *
+     * @ww.tagattribute required="false" rtexprvalue="false"
+     */
+    public void setFormat(String format) {
+        this.format = format;
+    }
 
-	/**
+    /**
      * Whether to print out the date nicely
-	 * @ww.tagattribute required="false" type="Boolean" default="false"
-	 */
-	public void setNice(boolean nice) {
-		this.nice = nice;
-	}
+     *
+     * @ww.tagattribute required="false" type="Boolean" default="false"
+     */
+    public void setNice(boolean nice) {
+        this.nice = nice;
+    }
 
-	/**
-	 * @return Returns the name.
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * @return Returns the name.
+     */
+    public String getName() {
+        return name;
+    }
 
     /**
      * The date value to format
+     *
      * @ww.tagattribute required="true" type="String"
      */
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * @return Returns the format.
-	 */
-	public String getFormat() {
-		return format;
-	}
+    /**
+     * @return Returns the format.
+     */
+    public String getFormat() {
+        return format;
+    }
 
-	/**
-	 * @return Returns the nice.
-	 */
-	public boolean isNice() {
-		return nice;
+    /**
+     * @return Returns the nice.
+     */
+    public boolean isNice() {
+        return nice;
 	}
 }
