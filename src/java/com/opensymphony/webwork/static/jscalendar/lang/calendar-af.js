b8d1570060e38622f12ea17c1f17e902fd2e8507
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-03-23 15:50:53 +0000

    make al language date picker popup, missing i18n conversion are in english cause i don't know african language
    Issue number:  ww-1265
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/com/opensymphony/webwork/static/jscalendar/lang/calendar-af.js
+++ src/java/com/opensymphony/webwork/static/jscalendar/lang/calendar-af.js
@@ -1,4 +1,9 @@
-// ** I18N Afrikaans
+// ** I18N
+
+// Calendar AF language
+
+
+// full day names
 Calendar._DN = new Array
 ("Sondag",
  "Maandag",
@@ -8,6 +13,12 @@ Calendar._DN = new Array
  "Vrydag",
  "Saterdag",
  "Sondag");
+
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 1;
+
+// full month names
 Calendar._MN = new Array
 ("Januarie",
  "Februarie",
@@ -21,10 +32,6 @@ Calendar._MN = new Array
  "Oktober",
  "November",
  "Desember");
-
-// First day of the week. "0" means display Sunday first, "1" means display
-// Monday first, etc.
-Calendar._FD = 1;
 
 // tooltips
 Calendar._TT = {};
@@ -41,3 +48,17 @@ Calendar._TT["MON_FIRST"] = "Vertoon Maandag eerste";
 Calendar._TT["SUN_FIRST"] = "Display Sunday first";
 Calendar._TT["CLOSE"] = "Close";
 Calendar._TT["TODAY"] = "Today";
+
+
+Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "%m/%d/%Y";
+Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
+
+Calendar._TT["WK"] = "wk";
+Calendar._TT["TIME"] = "Time:";
+
+Calendar._TT["DAY_FIRST"] = "Display %s first";
+Calendar._TT["WEEKEND"] = "0,6";
+
