14dc60b517da6dcb9d0598f8bac568d7c398cdb7
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-04-16 10:14:57 +0000

    updated javadoc
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/com/opensymphony/webwork/components/IteratorComponent.java
+++ src/java/com/opensymphony/webwork/components/IteratorComponent.java
@@ -138,7 +138,7 @@ import java.util.Iterator;
  *
  * <!-- START SNIPPET: example5description -->
  * 
- * To simulate a simple loop with iterator tag, the following could be done. It does the loop 5 times.
+ * </p>To simulate a simple loop with iterator tag, the following could be done. It does the loop 5 times.
  * 
  * <!-- END SNIPPET: example5description -->
  * 
