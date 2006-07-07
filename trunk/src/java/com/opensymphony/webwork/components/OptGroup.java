b31ac03e4dd95425f8417f77c35d8bea0a290282
tmjee <tmjee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2006-07-07 17:56:48 +0000

    - added snippet to javadoc
    Issue number:  WW-1312
    Obtained from:
    Submitted by:
    Reviewed by:

--- trunk/src/java/com/opensymphony/webwork/components/OptGroup.java
+++ trunk/src/java/com/opensymphony/webwork/components/OptGroup.java
@@ -18,9 +18,42 @@ import org.apache.commons.logging.LogFactory;
 import com.opensymphony.xwork.util.OgnlValueStack;
 
 /**
+ * <!-- START SNIPPET: javadoc -->
+ * 
+ * Create a optgroup component which needs to resides within a select tag.
+ * 
+ * <!-- END SNIPPET: javadoc -->
+ * 
+ * <p/>
+ * 
+ * <!-- START SNIPPET: notice -->
+ * 
+ * This component is to be used within a  Select component.
+ * 
+ * <!-- END SNIPPET: notice -->
+ * 
+ * <p/>
+ *
+ * <pre> 
+ * <!-- START SNIPPET: example -->
+ * 
+ * &lt;ww:select label="My Selection" 
+ *            name="mySelection" 
+ *            value="%{'POPEYE'}"
+ *            list="%{#{'SUPERMAN':'Superman', 'SPIDERMAN':'spiderman'}}"&gt;
+ *    &lt;ww:optgroup label="Adult"
+ *                 list="%{#{'SOUTH_PARK':'South Park'}}" /&gt;
+ *    &lt;ww:optgroup label="Japanese"
+ *                 list="%{#{'POKEMON':'pokemon','DIGIMON':'digimon','SAILORMOON':'Sailormoon'}}" /&gt;
+ * &lt;/ww:select&gt;
+ * 
+ * <!-- END SNIPPET: example -->
+ * </pre>
  * 
  * @author tm_jee
  * @version $Date$ $Id$
+ * @ww.tag name="optgroup" tld-body-content="JSP" tld-tag-class="com.opensymphony.webwork.views.jsp.ui.OptGroupTag"
+ * description="Renders a Select Tag's OptGroup Tag"
  */
 public class OptGroup extends Component {
 	
@@ -64,23 +97,42 @@ public class OptGroup extends Component {
 		return false;
 	}
 	
-	
+	/**
+	 * Set the label attribute.
+	 * @ww.tagattribute required="false"
+	 */
 	public void setLabel(String label) {
 		internalUiBean.setLabel(label);
 	}
 	
+	/**
+	 * Set the disable attribute.
+	 * @ww.tagattribute required="false"
+	 */
 	public void setDisabled(String disabled) {
 		internalUiBean.setDisabled(disabled);
 	}
 	
+	/**
+	 * Set the list attribute.
+	 * @ww.tagattribute required="false"
+	 */
 	public void setList(String list) {
 		internalUiBean.setList(list);
 	}
 	
+	/**
+	 * Set the listKey attribute.
+	 * @ww.tagattribute required="false"
+	 */
 	public void setListKey(String listKey) {
 		internalUiBean.setListKey(listKey);
 	}
 	
+	/**
+	 * Set the listValue attribute.
+	 * @ww.tagattribute required="false"
+	 */
 	public void setListValue(String listValue) {
 		internalUiBean.setListValue(listValue);
 	}
