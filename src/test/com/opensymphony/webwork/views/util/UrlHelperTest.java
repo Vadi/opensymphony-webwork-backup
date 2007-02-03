0df2bf06da43eb3821eb9feea0727045de4e5923
tschneider22 <tschneider22@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2007-02-03 06:53:28 +0000

    Fix nullpointer when scheme is null
    Issue number:  WW-1391
    Obtained from: Philip Luppens
    Submitted by: Philip Luppens
    Reviewed by: Tom Schneider

--- src/test/com/opensymphony/webwork/views/util/UrlHelperTest.java
+++ src/test/com/opensymphony/webwork/views/util/UrlHelperTest.java
@@ -45,7 +45,28 @@ public class UrlHelperTest extends WebWorkTestCase {
 		assertEquals(expectedUrl, result);
         mockHttpServletRequest.verify();
     }
-	
+
+    public void testForceAddNullSchemeHostAndPort() throws Exception {
+        String expectedUrl = "http://localhost/contextPath/path1/path2/myAction.action";
+
+        Mock mockHttpServletRequest = new Mock(HttpServletRequest.class);
+        mockHttpServletRequest.expectAndReturn("getScheme", "http");
+        mockHttpServletRequest.expectAndReturn("getServerName", "localhost");
+        mockHttpServletRequest.expectAndReturn("getContextPath",
+            "/contextPath");
+
+        Mock mockHttpServletResponse = new Mock(HttpServletResponse.class);
+        mockHttpServletResponse.expectAndReturn("encodeURL", expectedUrl,
+            expectedUrl);
+
+        String result = UrlHelper.buildUrl("/path1/path2/myAction.action",
+            (HttpServletRequest) mockHttpServletRequest.proxy(),
+            (HttpServletResponse) mockHttpServletResponse.proxy(), null,
+            null, true, true, true);
+        assertEquals(expectedUrl, result);
+        mockHttpServletRequest.verify();
+    }
+
 	public void testDoNotForceAddSchemeHostAndPort() throws Exception {
 		String expectedUrl = "/contextPath/path1/path2/myAction.action";
 		
