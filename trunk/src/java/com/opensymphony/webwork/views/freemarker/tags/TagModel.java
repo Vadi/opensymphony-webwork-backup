cba0588ba071f9a18abb00fe524013cbd9ab468b
tm_jee <tm_jee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2007-03-20 17:52:15 +0000

    WW-1431  - Support non-String attributes to freemarker JSP tag extensions

--- trunk/src/java/com/opensymphony/webwork/views/freemarker/tags/TagModel.java
+++ trunk/src/java/com/opensymphony/webwork/views/freemarker/tags/TagModel.java
@@ -1,11 +1,16 @@
+/*
+ * Copyright (c) 2002-2007 by OpenSymphony
+ * All rights reserved.
+ */
 package com.opensymphony.webwork.views.freemarker.tags;
 
 import com.opensymphony.webwork.components.Component;
 import com.opensymphony.xwork.util.OgnlValueStack;
-import freemarker.template.SimpleNumber;
+
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
 import freemarker.template.TemplateTransformModel;
-import freemarker.template.SimpleSequence;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +23,17 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * An abstract class mean to be implemented by WebWork's Freemarker's
+ * Tag. It abstracts away the logic of parameter conversion exposed by 
+ * Freemarker's {@link TemplateTransformModel#getWriter(Writer, Map)} method
+ * and populating them into WebWork's component exposed by subclass 
+ * through {@link #getBean()} method.
+ * 
+ * @author plightbo
+ * @author tmjee
+ * @version $Date$ $Id$
+ */
 public abstract class TagModel implements TemplateTransformModel {
     private static final Log LOG = LogFactory.getLog(TagModel.class);
 
@@ -31,17 +47,71 @@ public abstract class TagModel implements TemplateTransformModel {
         this.res = res;
     }
 
+    /**
+     * Returns a Writer that will render WebWork Freemarker tag.
+     * 
+     * @see TemplateTransformModel#getWriter(Writer, Map)
+     */
     public Writer getWriter(Writer writer, Map params) throws TemplateModelException, IOException {
         Component bean = getBean();
-        Map basicParams = convertParams(params);
-        bean.copyParams(basicParams);
-        bean.addAllParameters(getComplexParams(params));
+        
+        Map unwrappedParameters = unwrapParameters(params);
+        bean.copyParams(unwrappedParameters);
+        
+        //Map basicParams = convertParams(params);
+        //bean.copyParams(basicParams);
+        //bean.addAllParameters(getComplexParams(params));
+        
         return new CallbackWriter(bean, writer);
     }
+    
+    /**
+     * Unwraped the parameters (Map) passed in by 
+     * {TemplateTransformModel{@link #getWriter(Writer, Map)}. It makes use
+     * of {DefaultObjectWrapper{@link #unwrapParameters(Map)} to do the 
+     * unwrapping if the value of the entry in the <code>params</code> is 
+     * of type {#TemplateModel} (which should ALWAYS be the case) else it
+     * will just use the <code>toString()</code> representation of it.
+     * 
+     * @param params
+     * @return Map
+     */
+    protected Map unwrapParameters(Map params) {
+    	Map map = new HashMap(params.size());
+    	DefaultObjectWrapper objectWrapper = new DefaultObjectWrapper();
+    	for (Iterator iterator = params.entrySet().iterator(); iterator.hasNext(); ) {
+    		Map.Entry entry = (Map.Entry) iterator.next();
+    		
+    		Object value = entry.getValue();
+    		
+    		if (value != null) {
+    				// the value should ALWAYS be a decendant of TemplateModel
+    				if (value instanceof TemplateModel) {
+    					try {
+    						map.put(entry.getKey(), objectWrapper.unwrap((TemplateModel) value));
+    					}catch(TemplateModelException e) {
+    						LOG.error("failed to unwrap ["+value+"] it will be ignored", e);
+    					}
+    				}
+    				// if it doesn't, we'll do it the old way by just returning the toString() representation
+    				else {
+    					map.put(entry.getKey(), value.toString());
+    				}
+    		}
+    	}
+    	return map;
+    }
 
+
+    /**
+     * An abstract method subclass should implement, exposing the WebWork's
+     * underlying {Component} this tag is supposed to delegate to.
+     * 
+     * @return Component
+     */
     protected abstract Component getBean();
 
-    private Map convertParams(Map params) {
+    /*private Map convertParams(Map params) {
         HashMap map = new HashMap(params.size());
         for (Iterator iterator = params.entrySet().iterator(); iterator.hasNext();) {
             Map.Entry entry = (Map.Entry) iterator.next();
@@ -51,9 +121,9 @@ public abstract class TagModel implements TemplateTransformModel {
             }
         }
         return map;
-    }
+    }*/
 
-    private Map getComplexParams(Map params) {
+    /*private Map getComplexParams(Map params) {
         HashMap map = new HashMap(params.size());
         for (Iterator iterator = params.entrySet().iterator(); iterator.hasNext();) {
             Map.Entry entry = (Map.Entry) iterator.next();
@@ -73,11 +143,11 @@ public abstract class TagModel implements TemplateTransformModel {
             }
         }
         return map;
-    }
+    }*/
 
-    private boolean complexType(Object value) {
+    /*private boolean complexType(Object value) {
         return value instanceof freemarker.ext.beans.BeanModel
                 || value instanceof SimpleNumber
                 || value instanceof SimpleSequence;
-    }
+    }*/
 }
