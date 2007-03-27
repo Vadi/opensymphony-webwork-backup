bf99348d501a351460c0f846c0b2570285664567
tm_jee <tm_jee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2007-03-27 08:12:10 +0000

    WW-1437  ShowCase problem with sessionInvalidation feature

--- webapps/showcase/src/java/com/opensymphony/webwork/showcase/sessionInvalidation/SessionInvalidationAction.java
+++ webapps/showcase/src/java/com/opensymphony/webwork/showcase/sessionInvalidation/SessionInvalidationAction.java
@@ -6,6 +6,9 @@ package com.opensymphony.webwork.showcase.sessionInvalidation;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import com.opensymphony.webwork.ServletActionContext;
 import com.opensymphony.webwork.interceptor.SessionAware;
 import com.opensymphony.xwork.ActionSupport;
 
@@ -13,16 +16,29 @@ import com.opensymphony.xwork.ActionSupport;
  * @author tmjee
  * @version $Date$ $Id$
  */
-public class SessionInvalidationAction extends ActionSupport implements SessionAware {
-
+public class SessionInvalidationAction extends ActionSupport /*implements SessionAware*/ {
+// NOTE: SessionAware was not being used due to quickstart not being able to compile, 
+//       complaining that the abstract method setSession(Map) method from SessionAware
+//       interface is not being implemented whereas in fact it is. It seems to be ok 
+//       if the war file was deployed standalone in Tomcat, just that in quickstart
+//       it seems to be an issue. Since quickstart is doing hot deployment, it might
+//       have something to do with that, or how quickstart hot deploy java class file
+//       (not sure). (WW-1437)
+	
 	private Map sessionMap;
 	
 	private static final long serialVersionUID = -2634727929107900849L;
 
 	public String start() throws Exception {
-		// sessionMap.remove("___invalidateSession");
-		sessionMap.put("someKey1", "someValue1");
-		sessionMap.put("someKey2",	"someValue2");
+		
+		HttpSession session = ServletActionContext.getRequest().getSession(true);
+		
+		//sessionMap.put("someKey1", "someValue1");
+		//sessionMap.put("someKey2",	"someValue2");
+		
+		session.setAttribute("someKey1", "someValue1");
+		session.setAttribute("someKey2", "someValue2");
+		
 		return SUCCESS;
 	}
 	
@@ -38,9 +54,9 @@ public class SessionInvalidationAction extends ActionSupport implements SessionA
 		return SUCCESS;
 	}
 
-	public void setSession(Map session) {
+	/*public void setSession(Map session) {
 		this.sessionMap = session;
-	}
+	}*/
 	
 }
 
