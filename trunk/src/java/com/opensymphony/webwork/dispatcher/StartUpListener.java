f6ddf236aa537cb93ea46378c1ea8e14315d42ab
tm_jee <tm_jee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2007-06-11 15:00:38 +0000

    added javadoc snippets

--- trunk/src/java/com/opensymphony/webwork/dispatcher/StartUpListener.java
+++ trunk/src/java/com/opensymphony/webwork/dispatcher/StartUpListener.java
@@ -5,6 +5,8 @@
 package com.opensymphony.webwork.dispatcher;
 
 /**
+ * <!-- START SNIPPET: javadoc -->
+ * 
  * An interface to be implemented if require to get informed when WebWork
  * starts up, It's being hooked up through {@link FilterDispatcher#init(javax.servlet.FilterConfig)} 
  * -> {@link DispatcherUtils#init(javax.servlet.ServletContext)} methods calls.
@@ -21,6 +23,8 @@ package com.opensymphony.webwork.dispatcher;
  * The value of the properties is the FQN (Fully Quantified class name of the start up listeners)
  *  It must implements this interface, else a ClassCastException will be registered in the log.
  *  Multiple class names could be specified, but they must be comma separated.
+ * 
+ * <!-- END SNIPPET: javadoc -->
  * 
  * @author tmjee
  * @version $Date$ $Id$
