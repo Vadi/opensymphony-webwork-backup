bb338b479a717d260d8614cea4a3daf8d846dedc
tm_jee <tm_jee@0edc2cd7-6ee4-a2f5-ee51-5106e7628801>
2007-06-16 13:49:41 +0000

    changed logging from using Log4j to jakarta's commons logging since commons logging is used throughly in all other places except these

--- webapps/showcase/src/java/com/opensymphony/webwork/showcase/action/AbstractCRUDAction.java
+++ webapps/showcase/src/java/com/opensymphony/webwork/showcase/action/AbstractCRUDAction.java
@@ -1,56 +1,9 @@
-/* ====================================================================
- * The OpenSymphony Software License, Version 1.1
- *
- * (this license is derived and fully compatible with the Apache Software
- * License - see http://www.apache.org/LICENSE.txt)
- *
- * Copyright (c) 2001-2005 The OpenSymphony Group. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        OpenSymphony Group (http://www.opensymphony.com/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "OpenSymphony" and "The OpenSymphony Group"
- *    must not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact license@opensymphony.com .
- *
- * 5. Products derived from this software may not be called "OpenSymphony"
- *    or "WebWork", nor may "OpenSymphony" or "WebWork" appear in their
- *    name, without prior written permission of the OpenSymphony Group.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
+/*
+ * Copyright (c) 2002-2007 by OpenSymphony
+ * All rights reserved.
  */
 package com.opensymphony.webwork.showcase.action;
 
-import org.apache.log4j.Logger;
 import com.opensymphony.xwork.ActionSupport;
 import com.opensymphony.webwork.showcase.dao.Dao;
 import com.opensymphony.webwork.showcase.model.IdEntity;
@@ -58,15 +11,19 @@ import com.opensymphony.webwork.showcase.model.IdEntity;
 import java.util.Collection;
 import java.io.Serializable;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * AbstractCRUDAction.
  *
  * @author <a href="mailto:gielen@it-neering.net">Rene Gielen</a>
+ * @author tmjee
  */
 
 public abstract class AbstractCRUDAction extends ActionSupport {
 
-    private static final Logger log = Logger.getLogger(AbstractCRUDAction.class);
+    private static final Log log = LogFactory.getLog(AbstractCRUDAction.class);
 
     private Collection availableItems;
     private String[] toDelete;
